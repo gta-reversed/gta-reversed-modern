e5a47a0f539367e7910e6c708e887064f7195061
saml1er <danishroar@gmail.com>
2019-09-10 07:54:16 +0500

    Fully reverse CTaskSimpleHoldEntity struct

--- source/game_sa/Tasks/TaskTypes/CTaskSimpleHoldEntity.h
+++ source/game_sa/Tasks/TaskTypes/CTaskSimpleHoldEntity.h
@@ -7,24 +7,22 @@ protected:
     CTaskSimpleHoldEntity(plugin::dummy_func_t a) : CTaskSimple(a) {}
 public:
     class CObject* m_pObjectToHold;
-    float m_posX; // posX, posY, and posZ might not be float. We need to confirm it
-    float m_posY;
-    float m_posZ; 
-    bool field_18;
-    bool field_19; 
-    bool field_1A[6];
-    int m_groupID;
-    int m_animID;
+    CVector m_vecPosition;
+    bool m_bBoneFrameId;
+    bool m_bBoneFlags;
+    bool field_1A [2];
+    float m_fRotation;
+    int m_nAnimId;
+    int m_nAnimGroupId;
     int m_animFlags; // m_pAnimBlendAssociation flags
     class CAnimBlock* m_pAnimBlock; 
     class CAnimBlendHierarchy* m_pAnimBlendHierarchy; // If set, m_animID and m_groupID are ignored in StartAnim method
     bool m_bEntityDropped; 
-    bool field_35;
-    bool field_36;
+    bool m_bEntityRequiresProcessing;
+    bool m_bDisallowDroppingOnAnimEnd;
     bool field_37;
     class CAnimBlendAssociation* m_pAnimBlendAssociation; 
-    float m_fPutDownHeightZ;
 };
 
-VALIDATE_SIZE(CTaskSimpleHoldEntity, 0x40);
+VALIDATE_SIZE(CTaskSimpleHoldEntity, 0x3C);
 
