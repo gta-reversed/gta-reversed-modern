63f3a521f031c00ee1694a61ba841d18624a444c
Izzotop <kirill-j57@yandex.ru>
2021-05-30 18:47:58 +0300

    Reverse CCullZones (#63)
    * Reverse CCullZones
    
    * Hook CCullZones
    
    [skip ci]
    
    * Fix according to suggestions CCullZones
    
    Co-authored-by: Code Nulls <codenullsbusiness@gmail.com>
    
    Co-authored-by: Code Nulls <codenullsbusiness@gmail.com>

--- source/game_sa/CCullZones.cpp
+++ source/game_sa/CCullZones.cpp
@@ -1,16 +1,211 @@
 #include "StdInc.h"
 
-void CCullZones::Init()
-{
-    plugin::Call<0x72D6B0>();
+CCullZoneReflection (&CCullZones::aMirrorAttributeZones)[72] = *(CCullZoneReflection(*)[72])0xC815C0;
+CCullZone (&CCullZones::aTunnelAttributeZones)[40] = *(CCullZone(*)[40])0xC81C80;
+CCullZone (&CCullZones::aAttributeZones)[1300] = *(CCullZone(*)[1300])0xC81F50;
+int& CCullZones::CurrentFlags_Player = *(int*)0xC87AB8;
+int& CCullZones::CurrentFlags_Camera = *(int*)0xC87ABC;
+int& CCullZones::NumTunnelAttributeZones = *(int*)0xC87AC0;
+int& CCullZones::NumMirrorAttributeZones = *(int*)0xC87AC4;
+int& CCullZones::NumAttributeZones = *(int*)0xC87AC8;
+bool& CCullZones::bMilitaryZonesDisabled = *(bool*)0xC87ACD;
+
+void CCullZones::InjectHooks() {
+    ReversibleHooks::Install("CCullZones", "Init", 0x72D6B0, &CCullZones::Init);
+    ReversibleHooks::Install("CCullZones", "AddCullZone", 0x72DF70, &CCullZones::AddCullZone);
+    ReversibleHooks::Install("CCullZones", "AddTunnelAttributeZone", 0x72DB50, &CCullZones::AddTunnelAttributeZone);
+    ReversibleHooks::Install("CCullZones", "AddMirrorAttributeZone", 0x72DC10, &CCullZones::AddMirrorAttributeZone);
+    ReversibleHooks::Install("CCullZones", "InRoomForAudio", 0x72DD70, &CCullZones::InRoomForAudio);
+    ReversibleHooks::Install("CCullZones", "CamNoRain", 0x72DDB0, &CCullZones::CamNoRain);
+    ReversibleHooks::Install("CCullZones", "PlayerNoRain", 0x72DDC0, &CCullZones::PlayerNoRain);
+    ReversibleHooks::Install("CCullZones", "FewerPeds", 0x72DD90, &CCullZones::FewerPeds);
+    ReversibleHooks::Install("CCullZones", "NoPolice", 0x72DD50, &CCullZones::NoPolice);
+    ReversibleHooks::Install("CCullZones", "DoExtraAirResistanceForPlayer", 0x72DDD0, &CCullZones::DoExtraAirResistanceForPlayer);
+    ReversibleHooks::Install("CCullZones", "FindTunnelAttributesForCoors", 0x72D9F0, &CCullZones::FindTunnelAttributesForCoors);
+    ReversibleHooks::Install("CCullZones", "FindMirrorAttributesForCoors", 0x72DA70, &CCullZones::FindMirrorAttributesForCoors);
+    ReversibleHooks::Install("CCullZones", "FindZoneWithStairsAttributeForPlayer", 0x72DAD0, &CCullZones::FindZoneWithStairsAttributeForPlayer);
+    ReversibleHooks::Install("CCullZones", "FindAttributesForCoors", 0x72D970, &CCullZones::FindAttributesForCoors);
+    ReversibleHooks::Install("CCullZones", "Update", 0x72DEC0, &CCullZones::Update);
 }
 
-bool CCullZones::DoExtraAirResistanceForPlayer()
-{
-    return plugin::CallAndReturn<bool, 0x72DDD0>();
+// 0x72D6B0
+void CCullZones::Init() {
+    NumAttributeZones = 0;
+    CurrentFlags_Player = 0;
+    CurrentFlags_Camera = 0;
 }
 
-eZoneAttributes CCullZones::FindTunnelAttributesForCoors(CVector point)
-{
-    return plugin::CallAndReturn<eZoneAttributes, 0x72D9F0, CVector>(point);
+// flags: see eZoneAttributes
+// 0x72DF70
+void CCullZones::AddCullZone(const CVector* center, float zero1, float fWidthY, float fBottomZ, float fWidthX, float zero2, float fTopZ, uint16_t flags) {
+    int i = NumAttributeZones;
+
+    if ((flags & (eZoneAttributes::UNKNOWN_1 | eZoneAttributes::UNKNOWN_2))) {
+        AddTunnelAttributeZone(center, zero1, fWidthY, fBottomZ, fWidthX, zero2, fTopZ, flags);
+        flags &= ~(eZoneAttributes::UNKNOWN_2 | eZoneAttributes::UNKNOWN_1);
+    }
+
+    // Will be computed if flags less than 0x880 (UNKNOWN_1 + UNKNOWN_2)
+    if (flags) {
+        aAttributeZones[i].zoneDef.x = center->x - zero1 - fWidthX;
+        aAttributeZones[i].zoneDef.y = center->y - fWidthY - zero2;
+        aAttributeZones[i].zoneDef.field_4 = zero1 + zero1;
+        aAttributeZones[i].zoneDef.widthY = fWidthY + fWidthY;
+        aAttributeZones[i].zoneDef.bottomZ = fBottomZ;
+        aAttributeZones[i].zoneDef.widthX = fWidthX + fWidthX;
+        aAttributeZones[i].zoneDef.field_A = zero2 + zero2;
+        aAttributeZones[i].zoneDef.topZ = fTopZ;
+        aAttributeZones[i].flags = static_cast<eZoneAttributes>(flags);
+
+        NumAttributeZones += 1;
+    }
+}
+
+// flags: see eZoneAttributes
+// 0x72DB50
+void CCullZones::AddTunnelAttributeZone(const CVector* center, float unk1, float fWidthY, float fBottomZ, float fWidthX, float unk2, float fTopZ, uint16_t flags) {
+    int i = NumTunnelAttributeZones;
+
+    aTunnelAttributeZones[i].zoneDef.x = center->x - unk1 - fWidthX;
+    aTunnelAttributeZones[i].zoneDef.y = center->y - fWidthY - unk2;
+    aTunnelAttributeZones[i].zoneDef.field_4 = unk1 + unk1;
+    aTunnelAttributeZones[i].zoneDef.widthY = fWidthY + fWidthY;
+    aTunnelAttributeZones[i].zoneDef.bottomZ = fBottomZ;
+    aTunnelAttributeZones[i].zoneDef.widthX = fWidthX + fWidthX;
+    aTunnelAttributeZones[i].zoneDef.field_A = unk2 + unk2;
+    aTunnelAttributeZones[i].zoneDef.topZ = fTopZ;
+    aTunnelAttributeZones[i].flags = static_cast<eZoneAttributes>(flags);
+
+    NumTunnelAttributeZones += 1;
+}
+
+// flags: see eZoneAttributes
+// 0x72DC10
+void CCullZones::AddMirrorAttributeZone(const CVector* center, float unk1, float fWidthY, float fBottomZ, float fWidthX, float unk2, float fTopZ, uint16_t flags, int cm, float vX, float vY, float vZ) {
+    int i = NumMirrorAttributeZones;
+
+    aMirrorAttributeZones[i].zoneDef.x = center->x - unk1 - fWidthX;
+    aMirrorAttributeZones[i].zoneDef.y = center->y - fWidthY - unk2;
+    aMirrorAttributeZones[i].zoneDef.field_4 = unk1 + unk1;
+    aMirrorAttributeZones[i].zoneDef.widthY = fWidthY + fWidthY;
+    aMirrorAttributeZones[i].zoneDef.bottomZ = fBottomZ;
+    aMirrorAttributeZones[i].zoneDef.widthX = fWidthX + fWidthX;
+    aMirrorAttributeZones[i].zoneDef.field_A = unk2 + unk2;
+    aMirrorAttributeZones[i].zoneDef.topZ = fTopZ;
+    aMirrorAttributeZones[i].flags = static_cast<eZoneAttributes>(flags);
+    aMirrorAttributeZones[i].cm = cm;
+    aMirrorAttributeZones[i].vx = vX * 100.0f;
+    aMirrorAttributeZones[i].vy = vY * 100.0f;
+    aMirrorAttributeZones[i].vz = vZ * 100.0f;
+
+    NumMirrorAttributeZones += 1;
+}
+
+// 0x72DD70
+bool CCullZones::InRoomForAudio() {
+    return (CurrentFlags_Camera & eZoneAttributes::IN_ROOMS_FOR_AUDIO) != 0;
+}
+
+// 0x72DDB0
+bool CCullZones::CamNoRain() {
+    return (CurrentFlags_Camera & eZoneAttributes::CAM_NO_RAIN) != 0;
+}
+
+// 0x72DDC0
+bool CCullZones::PlayerNoRain() {
+    return (CurrentFlags_Player & eZoneAttributes::CAM_NO_RAIN) != 0;
+}
+
+// 0x72DD90
+bool CCullZones::FewerPeds() {
+    return (CurrentFlags_Player & eZoneAttributes::IN_ROOMS_FEWER_PEDS) != 0;
+}
+
+// 0x72DD50
+bool CCullZones::NoPolice() {
+    return (CurrentFlags_Player & eZoneAttributes::NO_POLICE) != 0;
+}
+
+// 0x72DDD0
+bool CCullZones::DoExtraAirResistanceForPlayer() {
+    return (CurrentFlags_Player & eZoneAttributes::EXTRA_AIR_RESISTANCE) != 0;
+}
+
+// 0x72D9F0
+eZoneAttributes CCullZones::FindTunnelAttributesForCoors(CVector point) {
+    if (NumTunnelAttributeZones <= 0)
+        return eZoneAttributes::NONE;
+
+    int out = eZoneAttributes::NONE;
+    for (auto& tunnelAttributeZone : aTunnelAttributeZones) {
+        if (tunnelAttributeZone.IsPointWithin(point)) {
+            out |= tunnelAttributeZone.flags;
+        }
+    }
+
+    return static_cast<eZoneAttributes>(out);
+}
+
+// NOTSA
+CCullZoneReflection* CCullZones::FindMirrorAttributesForCoors_(CVector cameraPosition) {
+    if (NumMirrorAttributeZones <= 0)
+        return nullptr;
+
+    for (auto& pCullZoneReflection: aMirrorAttributeZones) {
+        if (pCullZoneReflection.IsPointWithin(cameraPosition))
+            return &pCullZoneReflection;
+    }
+
+    return nullptr;
+}
+
+// 0x72DA70
+CCullZoneReflection* CCullZones::FindMirrorAttributesForCoors(float x, float y, float z) {
+    CVector cameraPosition{x, y, z};
+    return FindMirrorAttributesForCoors_(cameraPosition);
+}
+
+// 0x72DAD0
+CCullZone* CCullZones::FindZoneWithStairsAttributeForPlayer() {
+    if (NumAttributeZones <= 0)
+        return nullptr;
+
+    for (auto& pCullZone : aAttributeZones) {
+        if ((pCullZone.flags & eZoneAttributes::CAM_STAIRS_FOR_PLAYER) != 0 && pCullZone.IsPointWithin(FindPlayerCoors()))
+            return &pCullZone;
+    }
+
+    return nullptr;
+}
+
+// 0x72D970
+eZoneAttributes CCullZones::FindAttributesForCoors(float x, float y) {
+    if (NumAttributeZones <= 0)
+        return eZoneAttributes::NONE;
+
+    int out = eZoneAttributes::NONE;
+    for (auto& attributeZone : aAttributeZones) {
+        if (attributeZone.IsPointWithin({x, y, 0})) {
+            out |= attributeZone.flags;
+        }
+    }
+
+    return static_cast<eZoneAttributes>(out);
+}
+
+// 0x72DEC0
+void CCullZones::Update() {
+    if ((CTimer::m_FrameCounter & 7) == 2) {
+        auto cameraPosition = TheCamera.GetGameCamPosition();
+        CurrentFlags_Camera = FindAttributesForCoors(cameraPosition->x, cameraPosition->y);
+    } else if ((CTimer::m_FrameCounter & 7) == 6) {
+        CVector posn = FindPlayerCoors();
+        CurrentFlags_Player = FindAttributesForCoors(posn.x, posn.y);
+        if (!bMilitaryZonesDisabled && (CurrentFlags_Player & eZoneAttributes::MILITARY_ZONE) != 0) {
+            auto player = FindPlayerPed();
+            if (player->IsAlive()) {
+                player->SetWantedLevelNoDrop(5);
+            }
+        }
+    }
 }
