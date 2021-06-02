820324fdb5049b448eea14d407e8f71d53727655
Izzotop <kirill-j57@yandex.ru>
2021-06-02 19:29:56 +0300

    Reverse CSpecialPlateHandler (#65)
    * Hook CWindModifiers
    
    * Reverse CSpecialPlateHandler
    
    * Beautify CompressedVector
    
    * Fix CCarGenerator::DoInternalProcessing plate size
    
    Co-authored-by: Code Nulls <codenullsbusiness@gmail.com>

--- source/game_sa/CWindModifiers.h
+++ source/game_sa/CWindModifiers.h
@@ -1,21 +1,21 @@
 #pragma once
-#include "PluginBase.h"
 
-struct CWindModifier
-{
+struct CWindModifier {
     CVector m_vecPos;
-    int m_iActive;
-    float m_fPower;
+    int     m_iActive;
+    float   m_fPower;
 };
-
 
 class CWindModifiers {
 public:
-    static int& snNumActiveModifiers;
     static constexpr int MAX_NUM_MODIFIERS = 16;
-    static CWindModifier(&saModifiers)[MAX_NUM_MODIFIERS];
+
+    static int& Number;
+    static CWindModifier (&Array)[MAX_NUM_MODIFIERS];
 
 public:
+    static void InjectHooks();
+
     static void FindWindModifier(CVector vecPos, float* pOutX, float* pOutY);
     static void RegisterOne(CVector vecPos, int iActive, float fPower);
 };
