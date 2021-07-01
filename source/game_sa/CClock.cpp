baa663061fe50f66e9a5cd0f3af695ba08833763
yukani <65975406+yukani@users.noreply.github.com>
2021-07-01 06:27:48 +0300

    Reverse CClock (#84)
    * Implemented most of the CClock methods
    
    * converted 1440 mins hex to decimal
    
    * rewrote CClock::GetIsTimeInRange()
    
    * removed redundant namespace names
    
    * much more sane identation (thanks izzotop)
    
    * fix copy errors
    
    * Fix double update seconds in CClock::Update
    
    * Update source/game_sa/CClock.cpp
    
    Co-authored-by: Pirulax <patrikjankovics7@gmail.com>
    
    * Update source/game_sa/CClock.cpp
    
    Co-authored-by: Pirulax <patrikjankovics7@gmail.com>
    
    * Update source/game_sa/CClock.cpp
    
    Co-authored-by: Pirulax <patrikjankovics7@gmail.com>
    
    * Rename vars in GetIsTimeInRange
    
    Co-authored-by: Izzotop <kirill-j57@yandex.ru>
    Co-authored-by: Pirulax <patrikjankovics7@gmail.com>

--- source/game_sa/CClock.cpp
+++ source/game_sa/CClock.cpp
@@ -1,6 +1,7 @@
 #include "StdInc.h"
 
-unsigned char (&CClock::daysInMonth)[12] = *reinterpret_cast<unsigned char (*)[12]>(0x8CCF24);
+// 0x8CCF24
+unsigned char CClock::daysInMonth[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 bool& CClock::bClockHasBeenStored = *reinterpret_cast<bool*>(0xB70144);
 unsigned short& CClock::ms_Stored_nGameClockSeconds = *reinterpret_cast<unsigned short*>(0xB70148);
 unsigned char& CClock::ms_Stored_nGameClockMinutes = *reinterpret_cast<unsigned char*>(0xB7014A);
@@ -18,11 +19,11 @@ unsigned int& CClock::ms_nMillisecondsPerGameMinute = *reinterpret_cast<unsigned
 
 void CClock::InjectHooks() {
     ReversibleHooks::Install("CClock", "Initialise", 0x52CD90, &CClock::Initialise);
-//    ReversibleHooks::Install("CClock", "Update", 0x52CF10, &CClock::Update);
-//    ReversibleHooks::Install("CClock", "GetGameClockMinutesUntil", 0x52CEB0, &CClock::GetGameClockMinutesUntil);
-//    ReversibleHooks::Install("CClock", "GetIsTimeInRange", 0x52CEE0, &CClock::GetIsTimeInRange);
+    ReversibleHooks::Install("CClock", "Update", 0x52CF10, &CClock::Update);
+    ReversibleHooks::Install("CClock", "GetGameClockMinutesUntil", 0x52CEB0, &CClock::GetGameClockMinutesUntil);
+    ReversibleHooks::Install("CClock", "GetIsTimeInRange", 0x52CEE0, &CClock::GetIsTimeInRange);
 //    ReversibleHooks::Install("CClock", "NormaliseGameClock", 0x52CDE0, &CClock::NormaliseGameClock);
-//    ReversibleHooks::Install("CClock", "OffsetClockByADay", 0x52D0B0, &CClock::OffsetClockByADay);
+    ReversibleHooks::Install("CClock", "OffsetClockByADay", 0x52D0B0, &CClock::OffsetClockByADay);
     ReversibleHooks::Install("CClock", "SetGameClock", 0x52D150, &CClock::SetGameClock);
     ReversibleHooks::Install("CClock", "StoreClock", 0x52D020, &CClock::StoreClock);
     ReversibleHooks::Install("CClock", "RestoreClock", 0x52D070, &CClock::RestoreClock);
@@ -31,33 +32,89 @@ void CClock::InjectHooks() {
 // Initializes clock
 // 0x52CD90
 void CClock::Initialise(unsigned int millisecondsPerGameMinute) {
-    CClock::ms_nMillisecondsPerGameMinute = millisecondsPerGameMinute;
-    CClock::ms_nGameClockMonth = 1;
-    CClock::ms_nGameClockDays = 1;
-    CClock::ms_nLastClockTick = CTimer::m_snTimeInMilliseconds;
-    CClock::ms_nGameClockHours = 12;
-    CClock::ms_nGameClockMinutes = 0;
-    CClock::ms_nGameClockSeconds = 0;
-    CClock::bClockHasBeenStored = false;
-    CClock::CurrentDay = 4;
+    ms_nMillisecondsPerGameMinute = millisecondsPerGameMinute;
+    ms_nGameClockMonth = 1;
+    ms_nGameClockDays = 1;
+    ms_nLastClockTick = CTimer::m_snTimeInMilliseconds;
+    ms_nGameClockHours = 12;
+    ms_nGameClockMinutes = 0;
+    ms_nGameClockSeconds = 0;
+    bClockHasBeenStored = false;
+    CurrentDay = 4;
 }
 
 // Updates a time
 // 0x52CF10
 void CClock::Update() {
-    plugin::CallDynGlobal<>(0x52CF10);
+    ms_nGameClockSeconds = (CTimer::m_snTimeInMilliseconds - ms_nLastClockTick) * 60 / ms_nMillisecondsPerGameMinute;
+
+    if (ms_nMillisecondsPerGameMinute >= (CTimer::m_snTimeInMilliseconds - ms_nLastClockTick) && !CCheat::m_aCheatsActive[CHEAT_FASTER_CLOCK])
+        return;
+
+    if (CCheat::m_aCheatsActive[CHEAT_ALWAYS_MIDNIGHT] || CCheat::m_aCheatsActive[CHEAT_STOP_GAME_CLOCK_ORANGE_SKY])
+        return;
+
+    // next minute
+    ms_nGameClockMinutes++;
+    ms_nLastClockTick += ms_nMillisecondsPerGameMinute;
+
+    if (CCheat::m_aCheatsActive[CHEAT_FASTER_CLOCK])
+        ms_nLastClockTick = CTimer::m_snTimeInMilliseconds;
+
+    // next hour
+    if (ms_nGameClockMinutes <= 59)
+        return;
+
+    ms_nGameClockMinutes = 0;
+    ms_nGameClockHours++;
+
+    // next day
+    if (ms_nGameClockHours <= 23)
+        return;
+
+    ms_nGameClockHours = 0;
+    ms_nGameClockDays++;
+
+    if (CurrentDay == 7)
+        CurrentDay = 1;
+    else
+        CurrentDay++;
+
+    CStats::IncrementStat(STAT_DAYS_PASSED_IN_GAME, 1.0f);
+
+    // next month
+    if (daysInMonth[ms_nGameClockMonth] > ms_nGameClockDays)
+        return;
+
+    ms_nGameClockDays = 1;
+
+    if (ms_nGameClockMonth == 12)
+        ms_nGameClockMonth = 1;
+    else
+        ms_nGameClockMonth++;
 }
 
 // Returns number of minutes to specified hour & minute.
 // 0x52CEB0
 unsigned short CClock::GetGameClockMinutesUntil(unsigned char hours, unsigned char minutes) {
-    return plugin::CallAndReturnDynGlobal<unsigned short, unsigned char, unsigned char>(0x52CEB0, hours, minutes);
+    unsigned int now = ms_nGameClockHours * 60 + ms_nGameClockMinutes;
+    unsigned int then = hours * 60 + minutes;
+    if (then < now) {
+        then += 24 * 60; // Add one day
+    }
+    return then - now;
 }
 
-// Returns true current hour is in range of two specified hours.
-// 0x52CEE0
-bool CClock::GetIsTimeInRange(unsigned char hourA, unsigned char hourB) {
-    return plugin::CallAndReturnDynGlobal<bool, unsigned char, unsigned char>(0x52CEE0, hourA, hourB);
+/**
+ * Returns true if current hour greater than or equal to *from* and less than *to*.
+ * ms_nGameClockHours ∈ [h1, h2)
+ * @address 0x52CEE0
+ */
+bool CClock::GetIsTimeInRange(unsigned char from, unsigned char to) {
+    if (from > to)
+        return ms_nGameClockHours >= from || ms_nGameClockHours < to;
+    else
+        return ms_nGameClockHours >= from && ms_nGameClockHours < to;
 }
 
 // Normalizes game clock
@@ -71,40 +128,75 @@ void CClock::NormaliseGameClock() {
 // Directions (0 = one day backwards, 1 = one day forwards)
 // 0x52D0B0
 void CClock::OffsetClockByADay(unsigned int timeDirection) {
-    plugin::CallDynGlobal<unsigned int>(0x52D0B0, timeDirection);
+    if (timeDirection == 0) {
+        ms_nGameClockDays--;
+
+        if (ms_nGameClockDays == 0) {
+            ms_nGameClockMonth--;
+
+            if (ms_nGameClockMonth == 0)
+                ms_nGameClockMonth = 12;
+
+            ms_nGameClockDays = daysInMonth[ms_nGameClockMonth];
+        }
+        CurrentDay--;
+
+        if (CurrentDay == 0)
+            CurrentDay = 7;
+    }
+    else {
+        ms_nGameClockDays++;
+
+        if (daysInMonth[ms_nGameClockMonth] < ms_nGameClockDays) {
+            ms_nGameClockMonth++;
+            ms_nGameClockDays = 1;
+
+            if (ms_nGameClockMonth > 12)
+                ms_nGameClockMonth = 1;
+        }
+
+        CurrentDay++;
+
+        if (CurrentDay > 7) {
+            CurrentDay = 1;
+            return;
+        }
+    }
 }
 
 // Sets game clock
 // 0x52D150
 void CClock::SetGameClock(unsigned char hours, unsigned char minutes, unsigned char day) {
-    CClock::ms_nLastClockTick = CTimer::m_snTimeInMilliseconds;
-    CClock::ms_nGameClockHours = hours;
-    CClock::ms_nGameClockMinutes = minutes;
+    ms_nLastClockTick = CTimer::m_snTimeInMilliseconds;
+    ms_nGameClockHours = hours;
+    ms_nGameClockMinutes = minutes;
+
     if (day) {
-        CClock::CurrentDay = day;
-        ++CClock::ms_nGameClockDays;
+        CurrentDay = day;
+        ++ms_nGameClockDays;
     }
-    CClock::ms_nGameClockSeconds = 0;
-    CClock::NormaliseGameClock();
+
+    ms_nGameClockSeconds = 0;
+    NormaliseGameClock();
 }
 
 // Backups a clock time
 // 0x52D020
 void CClock::StoreClock() {
-    CClock::ms_Stored_nGameClockMonths = CClock::ms_nGameClockMonth;
-    CClock::ms_Stored_nGameClockDays = CClock::ms_nGameClockDays;
-    CClock::ms_Stored_nGameClockHours = CClock::ms_nGameClockHours;
-    CClock::ms_Stored_nGameClockMinutes = CClock::ms_nGameClockMinutes;
-    CClock::ms_Stored_nGameClockSeconds = CClock::ms_nGameClockSeconds;
-    CClock::bClockHasBeenStored = true;
+    ms_Stored_nGameClockMonths = ms_nGameClockMonth;
+    ms_Stored_nGameClockDays = ms_nGameClockDays;
+    ms_Stored_nGameClockHours = ms_nGameClockHours;
+    ms_Stored_nGameClockMinutes = ms_nGameClockMinutes;
+    ms_Stored_nGameClockSeconds = ms_nGameClockSeconds;
+    bClockHasBeenStored = true;
 }
 
 // Restores a clock time
 // 0x52D070
 void CClock::RestoreClock() {
-    CClock::ms_nGameClockMonth = CClock::ms_Stored_nGameClockMonths;
-    CClock::ms_nGameClockDays = CClock::ms_Stored_nGameClockDays;
-    CClock::ms_nGameClockHours = CClock::ms_Stored_nGameClockHours;
-    CClock::ms_nGameClockMinutes = CClock::ms_Stored_nGameClockMinutes;
-    CClock::ms_nGameClockSeconds = CClock::ms_Stored_nGameClockSeconds;
+    ms_nGameClockMonth = ms_Stored_nGameClockMonths;
+    ms_nGameClockDays = ms_Stored_nGameClockDays;
+    ms_nGameClockHours = ms_Stored_nGameClockHours;
+    ms_nGameClockMinutes = ms_Stored_nGameClockMinutes;
+    ms_nGameClockSeconds = ms_Stored_nGameClockSeconds;
 }
