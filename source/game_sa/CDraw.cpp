c953c825b2cf0cee0c69f9007e8a61032fd2dd45
Pirulax <patrikjankovics7@gmail.com>
2021-08-05 22:37:34 +0200

    CDraw (#111)
    * CalculateAspectRatio
    
    * Update CDraw.cpp

--- source/game_sa/CDraw.cpp
+++ source/game_sa/CDraw.cpp
@@ -12,7 +12,7 @@ unsigned char &CDraw::FadeValue = *(unsigned char *) 0xC3EFAB;
 
 void CDraw::InjectHooks() {
     ReversibleHooks::Install("CDraw", "SetFOV", 0x6FF410, &CDraw::SetFOV);
-//    ReversibleHooks::Install("CDraw", "CalculateAspectRatio", 0x6FF420, &CDraw::CalculateAspectRatio);
+    ReversibleHooks::Install("CDraw", "CalculateAspectRatio", 0x6FF420, &CDraw::CalculateAspectRatio);
 }
 
 // 0x6FF410
@@ -21,6 +21,11 @@ void CDraw::SetFOV(float fovValue) {
 }
 
 // 0x6FF420
-float CDraw::CalculateAspectRatio() {
-    return plugin::CallAndReturn<float, 0x6FF420>();
+void CDraw::CalculateAspectRatio() {
+    if (FrontEndMenuManager.m_bWidescreenOn)
+        CDraw::ms_fAspectRatio = 16.f / 9.f;
+    else if (TheCamera.m_bWideScreenOn)
+        CDraw::ms_fAspectRatio = 5.f / 4.f;
+    else
+        CDraw::ms_fAspectRatio = 4.f / 3.f;
 }
