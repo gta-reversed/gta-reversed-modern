gta-reversed/gta-reversed-modern

commit c8b0257565b4b20ca0bfe475c67279e5d8fb0554

Reverse CRadar (#88)
* reverse some stuff from CRadar

* reversed 7 methods, added RAD2DEG math constant

* update

* update

* update

* removed retarded trailing whitespaces

* fixed typo and simplified speed calculation

* name render states

Co-authored-by: Izzotop <kirill-j57@yandex.ru>

* simplified rw render state func call

* Use auto in CRadar::StreamRadarSections

* Fix CRadar::DrawRadarMap

* Fix CRadar::DrawMap()

Constants
FindPlayerVehicle(-1, 0)
vehicle->GetPosition()

* Move from class circleTex

* Reverse CRadar::TransformRealWorldToTexCoordSpace

* Fix CRadar::CalculateCachedSinCos

* Change signature CRadar::DrawCoordBlip

* Rename fields in tRadarTrace

* Move to top Initialise and Shutdown

* Fix formatting

* update

* add FEMM field check and refactor some code

* fix scale bug

* remove circleTex

* Update source/game_sa/CRadar.cpp

Co-authored-by: Izzotop <kirill-j57@yandex.ru>

* Update source/game_sa/CRadar.cpp

Co-authored-by: Izzotop <kirill-j57@yandex.ru>

* simplified getting actual blip ids

* fix

* add blipIndex check

* i hate my lif

* Apply suggestions from code review

Co-authored-by: Izzotop <kirill-j57@yandex.ru>

* update

* fix

* Apply suggestions from code review/1

Co-authored-by: Code Nulls <danishroar@gmail.com>

* Apply suggestions from code review/2

Co-authored-by: Code Nulls <danishroar@gmail.com>

* flags update

* indentation

* Remove from StdInc.h

* Add CRadar::RadarBlipFileNames

* unsigned char m_nBlipSprite -> eRadarSprite

* Change signature CRadar::DrawRotatingRadarSprite

CRGBA color -> const CRGBA& color

* Fix CRadar::GetActualBlipArrayIndex

* Change signature CRadar::ShowRadarTraceWithHeight

unsigned char type -> eBlipMode mode

* Change switch order in CRadar::DrawLegend

* camelcase

* use constant for half pi

* fix out of bounds for legendTraceHeight

source/game_sa/CRadar.h
@@ -6,7 +6,6 @@ Do not delete this comment block. Respect others' work!
 */
 #pragma once
 
-#include "PluginBase.h"
 #include "CVector.h"
 #include "CRGBA.h"
 #include "CVector2D.h"
@@ -14,8 +13,8 @@ Do not delete this comment block. Respect others' work!
 
 // Thanks to Wesser for radar-related things
 enum eBlipAppearance {
-    BLIP_FLAG_FRIEND, // It selects BLIP_COLOUR_BLUE. If unset toghether with BLIP_FLAG_THREAT, any color.
-    BLIP_FLAG_THREAT  // It selects BLIP_COLOUR_RED. If unset toghether with BLIP_FLAG_FRIEND, any color.
+    BLIP_FLAG_FRIEND, // It selects BLIP_COLOUR_BLUE. If unset together with BLIP_FLAG_THREAT, any color.
+    BLIP_FLAG_THREAT  // It selects BLIP_COLOUR_RED. If unset together with BLIP_FLAG_FRIEND, any color.
 };
 
 enum eBlipType {
@@ -37,8 +36,8 @@ enum eBlipDisplay {
     BLIP_DISPLAY_BOTH        // 3
 };
 
-// See <https://www.dropbox.com/s/oi3i4f0qsbe7z10/blip_marker_colors.html> to view these colors.
-enum eBlipColour {
+// See <https://www.dropbox.com/s/oi3i4f0qsbe7z10/blip_marker_colors.html> to view these colors. (TODO: dead link, fix it.)
+enum eBlipColour : unsigned int {
     BLIP_COLOUR_RED,        // 0
     BLIP_COLOUR_GREEN,      // 1
     BLIP_COLOUR_BLUE,       // 2
@@ -50,7 +49,8 @@ enum eBlipColour {
     BLIP_COLOUR_DESTINATION // 8 - Default color.
 };
 
-enum eRadarSprite {
+// https://wiki.multitheftauto.com/index.php?title=Radar_Blips
+enum eRadarSprite : char {
     RADAR_SPRITE_NONE,          // 0
     RADAR_SPRITE_WHITE,         // 1
     RADAR_SPRITE_CENTRE,        // 2
@@ -118,10 +118,10 @@ enum eRadarSprite {
     RADAR_SPRITE_TORENO*/       // 64
 };
 
-enum eRadarTraceHeight {
-    RADAR_TRACE_LOW,
-    RADAR_TRACE_HIGH,
-    RADAR_TRACE_NORMAL
+enum eRadarTraceHeight : unsigned char {
+    RADAR_TRACE_LOW,   // 0
+    RADAR_TRACE_HIGH,  // 1
+    RADAR_TRACE_NORMAL // 2
 };
 
 struct tBlipHandle {
@@ -140,35 +140,49 @@ struct airstrip_info {
 
 VALIDATE_SIZE(airstrip_info, 0x10);
 
+class CEntryExit;
 
 struct tRadarTrace {
-    unsigned int   m_dwColour; // see eBlipColour
-    unsigned int   m_dwEntityHandle;
+    eBlipColour    m_nColour;
+    unsigned int   m_nEntityHandle;
     CVector        m_vPosition;
     unsigned short m_nCounter;
     float          m_fSphereRadius;
     unsigned short m_nBlipSize;
-    class CEntryExit* m_pEntryExit;
-    unsigned char  m_nBlipSprite; // see eRadarSprite
-    unsigned char  m_bBright : 1; // It makes use of bright colors. Always set.
+    CEntryExit*    m_pEntryExit;
+    eRadarSprite   m_nBlipSprite;
+    unsigned char  m_bBright : 1;       // It makes use of bright colors. Always set.
     unsigned char  m_bTrackingBlip : 1; // It is available.
-    unsigned char  m_bShortRange : 1; // It doesn't show permanently on the radar.
-    unsigned char  m_bFriendly : 1; // It is affected by BLIP_COLOUR_THREAT.   
-    unsigned char  m_bBlipRemain : 1; // It has the priority over the entity (it will still appear after the entity's deletion).
-    unsigned char  m_bBlipFade : 1; // Possibly a leftover. Always unset (unused).
+    unsigned char  m_bShortRange : 1;   // It doesn't show permanently on the radar.
+    unsigned char  m_bFriendly : 1;     // It is affected by BLIP_COLOUR_THREAT.
+    unsigned char  m_bBlipRemain : 1;   // It has the priority over the entity (it will still appear after the entity's deletion).
+    unsigned char  m_bBlipFade : 1;     // Possibly a leftover. Always unset (unused).
+    unsigned char  pad : 2;
     unsigned char  m_nCoordBlipAppearance : 2; // see eBlipAppearance
-    unsigned char  m_nBlipDisplayFlag : 2; // see eBlipDisplay
-    unsigned char  m_nBlipType : 4; // see eBlipType
+    unsigned char  m_nBlipDisplayFlag : 2;     // see eBlipDisplay
+    unsigned char  m_nBlipType : 4;            // see eBlipType
 };
 
 VALIDATE_SIZE(tRadarTrace, 0x28);
 
-extern unsigned int MAX_RADAR_SPRITES;
-extern unsigned int MAX_RADAR_TRACES;
-
 class CRadar {
 public:
+    static constexpr unsigned int MAX_RADAR_SPRITES = 64;
+    static constexpr unsigned int MAX_RADAR_TRACES = 175;
+    static constexpr unsigned int MAX_AIRSTRIP_INFOS = 4;
+    static constexpr unsigned int MAX_RADAR_WIDTH_TILES = 12;
+    static constexpr unsigned int MAX_RADAR_HEIGHT_TILES = 12;
+
     static float& m_fRadarOrientation;
+
+    // original name unknown
+    static unsigned int& legendTraceTimer;
+    static eRadarTraceHeight& legendTraceHeight;
+
+    static unsigned int mapYouAreHereTimer;
+    static bool mapYouAreHereDisplay;
+
+    static char* RadarBlipFileNames[][2];
 
     // 2990.0 by default
     static float& m_radarRange;
@@ -184,30 +198,40 @@ public:
     static CVector2D& vec2DRadarOrigin;
     // static CSprite2d RadarBlipSprites[64];
     static CSprite2d* RadarBlipSprites;
-    // 
     static CRect& m_radarRect;
     // current airstrip index in airstrip_table
     static unsigned char& airstrip_location;
     // blip handle
     static int& airstrip_blip;
 
+    static float& cachedCos;
+    static float& cachedSin;
+
+public:
+    static void InjectHooks();
+
+    static void Initialise();
+    static void Shutdown();
+
     static void LoadTextures();
-    static int GetNewUniqueBlipIndex(int blipArrId);
+
+    static int GetNewUniqueBlipIndex(int blipIndex);
     static int GetActualBlipArrayIndex(int blipIndex);
-    static void DrawLegend(int x, int y, int blipType);
+
+    static void DrawLegend(int x, int y, eRadarSprite blipType);
     static float LimitRadarPoint(CVector2D& point);
     static void LimitToMap(float* pX, float* pY);
     static unsigned char CalculateBlipAlpha(float distance);
-    static void TransformRadarPointToScreenSpace(CVector2D& out, CVector2D const& in);
-    static void TransformRealWorldPointToRadarSpace(CVector2D& out, CVector2D const& in);
-    static void TransformRadarPointToRealWorldSpace(CVector2D& out, CVector2D const& in);
-    static void TransformRealWorldToTexCoordSpace(CVector2D& out, CVector2D const& in, int arg2, int arg3);
+    static void TransformRadarPointToScreenSpace(CVector2D& out, const CVector2D& in);
+    static void TransformRealWorldPointToRadarSpace(CVector2D& out, const CVector2D& in);
+    static void TransformRadarPointToRealWorldSpace(CVector2D& out, const CVector2D& in);
+    static void TransformRealWorldToTexCoordSpace(CVector2D& out, const CVector2D& in, int arg2, int arg3);
     static void CalculateCachedSinCos();
-    static int SetCoordBlip(eBlipType type, CVector posn, _IGNORED_ unsigned int arg2, eBlipDisplay blipDisplay, _IGNORED_ char* scriptName);
-    static int SetShortRangeCoordBlip(eBlipType type, CVector posn, unsigned int arg2, eBlipDisplay blipDisplay, char* scriptName);
+    static int SetCoordBlip(eBlipType type, CVector posn, _IGNORED_ unsigned int color, eBlipDisplay blipDisplay, _IGNORED_ char* scriptName);
+    static int SetShortRangeCoordBlip(eBlipType type, CVector posn, unsigned int color, eBlipDisplay blipDisplay, char* scriptName);
     static int SetEntityBlip(eBlipType type, int entityHandle, unsigned int arg2, eBlipDisplay blipDisplay);
     static void ChangeBlipColour(int blipIndex, unsigned int color);
-    static bool HasThisBlipBeenRevealed(int blipArrId);
+    static bool HasThisBlipBeenRevealed(int blipIndex);
     static bool DisplayThisBlip(int spriteId, char priority);
     static void ChangeBlipBrightness(int blipIndex, int brightness);
     static void ChangeBlipScale(int blipIndex, int size);
@@ -216,28 +240,25 @@ public:
     static void SetBlipAlwaysDisplayInZoom(int blipIndex, unsigned char display);
     static void SetBlipFade(int blipIndex, unsigned char fade);
     static void SetCoordBlipAppearance(int blipIndex, unsigned char appearance);
-    static void SetBlipFriendly(int blipIndex, unsigned char friendly);
+    static void SetBlipFriendly(int blipIndex, bool friendly);
     static void SetBlipEntryExit(int blipIndex, CEntryExit* enex);
     static void ShowRadarTrace(float x, float y, unsigned int size, unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha);
-    // type 2 - box, 0,1 - triangles
-    static void ShowRadarTraceWithHeight(float x, float y, unsigned int size, unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha, unsigned char type);
-    // show debug line at this position
+    static void ShowRadarTraceWithHeight(float x, float y, unsigned int size, unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha, eRadarTraceHeight height);
     static void ShowRadarMarker(CVector posn, unsigned int color, float radius);
-    static unsigned int GetRadarTraceColour(unsigned int color, unsigned char bright, unsigned char friendly);
-    static void DrawRotatingRadarSprite(CSprite2d* sprite, float x, float y, float angle, unsigned int width, unsigned int height, CRGBA color);
+    static unsigned int GetRadarTraceColour(eBlipColour color, bool bright, bool friendly);
+    static void DrawRotatingRadarSprite(CSprite2d* sprite, float x, float y, float angle, unsigned int width, unsigned int height, const CRGBA& color);
     static void DrawYouAreHereSprite(float x, float y);
     static void SetupRadarRect(int x, int y);
     static void RequestMapSection(int x, int y);
     static void RemoveMapSection(int x, int y);
     static void RemoveRadarSections();
+    static void StreamRadarSections(const CVector& worldPosn);
     static void StreamRadarSections(int x, int y);
-    static int ClipRadarPoly(CVector2D* out, CVector2D const* in);
-    static void DrawAreaOnRadar(CRect const& rect, CRGBA const& color, bool inMenu);
+    static int ClipRadarPoly(CVector2D* out, const CVector2D* in);
+    static void DrawAreaOnRadar(const CRect& rect, const CRGBA& color, bool inMenu);
     static void DrawRadarMask();
-    static void StreamRadarSections(CVector const& worldPosn);
-    static void Shutdown();
     static void InitFrontEndMap();
-    static void AddBlipToLegendList(unsigned char arg0, int blipArrId);
+    static void AddBlipToLegendList(unsigned char arg0, int blipIndex);
     static void SetMapCentreToPlayerCoords();
     static void Draw3dMarkers();
     static void SetRadarMarkerState(int arg0, unsigned char arg1);
@@ -247,31 +268,20 @@ public:
     static void DrawRadarGangOverlay(bool inMenu);
     static void DrawRadarMap();
     static void DrawMap();
-    static void DrawCoordBlip(int blipArrId, unsigned char isSprite);
-    static void DrawEntityBlip(int blipArrId, unsigned char arg1);
-    static void ClearActualBlip(int blipArrId);
+
+    static void DrawCoordBlip(int blipIndex, bool isSprite);
+    static void DrawEntityBlip(int blipIndex, unsigned char arg1);
+    static void ClearActualBlip(int blipIndex);
     static void ClearBlipForEntity(eBlipType blipType, int entityHandle);
     static void ClearBlip(int blipIndex);
     static void SetupAirstripBlips();
-    static void Initialise();
     static void DrawBlips();
-    // Load radar blips from save file
+
     static void Load();
-    // Save radar blips to save file
     static void Save();
 };
 
-extern airstrip_info* airstrip_table; // airstrip_info airstrip_table[4]
-
-extern unsigned int MAX_AIRSTRIP_INFOS;
-
-extern int* gRadarTextures; // int gRadarTextures[12][12]
-
-extern unsigned int MAX_RADAR_WIDTH_TILES;
-extern unsigned int MAX_RADAR_HEIHGT_TILES;
-
 bool ClipRadarTileCoords(int& x, int& y);
-bool IsPointInsideRadar(CVector2D const& point);
+bool IsPointInsideRadar(const CVector2D& point);
 void GetTextureCorners(int x, int y, CVector2D* corners);
-// returns number of intersections
-int LineRadarBoxCollision(CVector2D& result, CVector2D const& lineStart, CVector2D const& lineEnd);
+int LineRadarBoxCollision(CVector2D& result, const CVector2D& lineStart, const CVector2D& lineEnd);

source/InjectHooksMain.cpp
@@ -19,6 +19,7 @@
 
 #include "CCarGenerator.h"
 #include "CTheCarGenerators.h"
+#include "CRadar.h"
 
 void WaitForDebugger() {
     while (!::IsDebuggerPresent()) {
@@ -320,6 +321,7 @@ void InjectHooksMain()
     CIplStore::InjectHooks();
     cHandlingDataMgr::InjectHooks();
     CLoadingScreen::InjectHooks();
+    CRadar::InjectHooks();
     CText::InjectHooks();
     CTaskSimpleUninterruptable::InjectHooks();
     CTaskSimpleSetStayInSamePlace::InjectHooks();

source/StdInc.h
@@ -231,7 +231,6 @@
 #include "game_sa\CPopulation.h"
 #include "game_sa\CProjectileInfo.h"
 #include "game_sa\CQueuedMode.h"
-#include "game_sa\CRadar.h"
 #include "game_sa\CRealTimeShadow.h"
 #include "game_sa\CRealTimeShadowManager.h"
 #include "game_sa\CReference.h"

source/game_sa/CGame.cpp
@@ -3,6 +3,7 @@
 #include "CDebugMenu.h"
 #include "CWaterCannons.h"
 #include "CTheCarGenerators.h"
+#include "CRadar.h"
 
 char (&CGame::aDatFile)[32] = *reinterpret_cast<char (*)[32]>(0xB728EC);
 int& CGame::currLevel = *reinterpret_cast<int*>(0xB7290C);

source/game_sa/CGangWars.h
@@ -96,5 +96,3 @@ public:
     static void Update();
     static void UpdateTerritoryUnderControlPercentage();
 };
-
-//#include "meta/meta.CGangWars.h"

source/game_sa/CPlayerInfo.cpp
@@ -1,6 +1,14 @@
 #include "StdInc.h"
 
-void CPlayerInfo::InjectHooks() {
+void CPlayerInfo::InjectHooks()
+{
+    ReversibleHooks::Install("CPlayerInfo", "IsPlayerInRemoteMode", 0x56DAB0, &CPlayerInfo::IsPlayerInRemoteMode);
+}
+
+bool CPlayerInfo::IsPlayerInRemoteMode()
+{
+    //plugin::CallMethod<0x56DAB0, CPlayerInfo*>(this);
+    return m_pRemoteVehicle != nullptr;
 }
 
 // 0x56F7D0

source/game_sa/CPlayerInfo.h
@@ -93,6 +93,7 @@ public:
 
 public:
     static void InjectHooks();
+    bool IsPlayerInRemoteMode();
     void DeletePlayerSkin();
     void LoadPlayerSkin();
     void Clear();

commit d3049c148e31b778fd1a4feca3cdef1aec1effc4

Fix CRadar crash introduced in #88

source/game_sa/CRadar.h
@@ -157,8 +157,8 @@ struct tRadarTrace {
     unsigned char  m_bFriendly : 1;     // It is affected by BLIP_COLOUR_THREAT.
     unsigned char  m_bBlipRemain : 1;   // It has the priority over the entity (it will still appear after the entity's deletion).
     unsigned char  m_bBlipFade : 1;     // Possibly a leftover. Always unset (unused).
-    unsigned char  pad : 2;
     unsigned char  m_nCoordBlipAppearance : 2; // see eBlipAppearance
+
     unsigned char  m_nBlipDisplayFlag : 2;     // see eBlipDisplay
     unsigned char  m_nBlipType : 4;            // see eBlipType
 };