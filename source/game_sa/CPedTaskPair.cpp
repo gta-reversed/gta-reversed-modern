c3190e8ed96f85ef4f4829a3d1717db5ffc9731f
Pirulax <patrikjankovics7@gmail.com>
2021-08-18 18:30:59 +0200

    CPedTaskPair (#154)
    * Boilerplate
    
    * Flush

--- source/game_sa/CPedTaskPair.cpp
+++ source/game_sa/CPedTaskPair.cpp
@@ -1,1 +1,15 @@
 #include "StdInc.h"
+#include "CPedTaskPair.h"
+
+void CPedTaskPair::InjectHooks() {
+    ReversibleHooks::Install("CPedTaskPair", "Flush", 0x5E95B0, &CPedTaskPair::Flush);
+}
+
+// 0x5E95B0
+void CPedTaskPair::Flush() {
+    m_pPed = nullptr;
+    if (m_pTask)
+        delete m_pTask;
+    m_pTask = nullptr;
+    m_taskSlot = -1;
+}
