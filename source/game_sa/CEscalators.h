3e34f088a40060825a8ec965d04cb2807e1c4f3b
August <lex3a@users.noreply.github.com>
2021-08-19 14:30:12 +0300

    CEscalators done (#158)
    * Boilerplate + reordering
    
    * Implemented functions
    
    * Added addresses
    
    * CReplay::Mode != REPLAY_MODE_1

--- source/game_sa/CEscalators.h
+++ source/game_sa/CEscalators.h
@@ -1,17 +1,17 @@
 #pragma once
 #include "PluginBase.h"
+#define NUM_ESCALATORS 32
 
 class CEscalators {
 public:
-    static constexpr unsigned int NUM_ESCALATORS = 32;
     static CEscalator(&aEscalators)[NUM_ESCALATORS];
 
 public:
     static void InjectHooks();
 
 public:
-    static void Init();
     static void Shutdown();
-    static void Update();
+    static void Init();
     static void AddOne(CVector const& vecStart, CVector const& vecBottom, CVector const& vecEnd, CVector const& vecTop, bool bMoveDown, CEntity* pEntity);
+    static void Update();
 };
