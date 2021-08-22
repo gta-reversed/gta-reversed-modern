56225586c9b6f3b6f297840b0650fdf3ab63591f
Izzotop <kirill-j57@yandex.ru>
2021-08-23 01:56:42 +0300

    Reverse CHudColours (#58)
    80bc247a54319d544471916ec1368a5e761b5ea4

--- source/game_sa/CHudColours.h
+++ source/game_sa/CHudColours.h
@@ -5,62 +5,49 @@
     Do not delete this comment block. Respect others' work!
 */
 #pragma once
-#include "PluginBase.h"
+
 #include "CRGBA.h"
 
-enum eHudColours
-{
-	HUD_COLOUR_RED,
-	HUD_COLOUR_GREEN,
-	HUD_COLOUR_BLUEDARK,
-	HUD_COLOUR_BLUELIGHT,
-	HUD_COLOUR_WHITE,
-	HUD_COLOUR_BLACK,
-	HUD_COLOUR_ORANGE,
-	HUD_COLOUR_PURPLE,
-	HUD_COLOUR_GREY,
-	HUD_COLOUR_REDDARK,
-	HUD_COLOUR_GREENDARK,
-	HUD_COLOUR_ORANGELIGHT,
-	HUD_COLOUR_GREYDARK,
-	HUD_COLOUR_BLUE,
-	HUD_COLOUR_YELLOW,
-	HUD_COLOUR_NUM_COLOURS
+enum eHudColours : unsigned char {
+    HUD_COLOUR_RED,
+    HUD_COLOUR_GREEN,
+    HUD_COLOUR_DARK_BLUE,
+    HUD_COLOUR_LIGHT_BLUE,
+    HUD_COLOUR_LIGHT_GRAY,
+    HUD_COLOUR_BLACK,
+    HUD_COLOUR_GOLD,
+    HUD_COLOUR_PURPLE,
+    HUD_COLOUR_DARK_GRAY,
+    HUD_COLOUR_DARK_RED,
+    HUD_COLOUR_DARK_GREEN,
+    HUD_COLOUR_CREAM,
+    HUD_COLOUR_NIGHT_BLUE,
+    HUD_COLOUR_BLUE,
+    HUD_COLOUR_YELLOW,
+
+    HUD_COLOUR_COUNT = 16,
 };
 
-
-class  CHudColour
-{
+class CHudColours {
 public:
-	unsigned char red, green, blue, alpha;
-};
+    CRGBA m_aColours[15];
 
-
-VALIDATE_SIZE(CHudColour, 4);
-
-
-class  CHudColours
-{
 public:
-	CHudColour m_aColours[15];
+    static void InjectHooks();
 
-	CHudColours();
-	~CHudColours();
-	// setup the color in color table. "color" parameter - index of color in the table, see eHudColours 
-	// enum.
-	void SetRGBAValue(unsigned char color, unsigned char red, unsigned char green, unsigned char blue,
-		unsigned char alpha);
-	// get color from color table as integer value. "color" parameter - index of color in the table, 
-	// see eHudColours enum.
-	unsigned int GetIntColour(unsigned char color);
-	// get color RGB and set alpha manually. "color" parameter - index of color in the table, see 
-	// eHudColours enum.
-	CRGBA GetRGB(unsigned char color, unsigned char alpha);
-	// get color RGBA. "color" parameter - index of color in the table, see eHudColours enum.
-	CRGBA GetRGBA(unsigned char color);
+    CHudColours();
+    CHudColours* Constructor();
+
+    ~CHudColours();
+    CHudColours* Destructor();
+
+    void SetRGBAValue(eHudColours colorIndex, unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha);
+    unsigned int GetIntColour(eHudColours colorIndex);
+
+    CRGBA GetRGB(eHudColours colorIndex);
+    CRGBA GetRGBA(eHudColours index, unsigned char alpha);
 };
-
 
 VALIDATE_SIZE(CHudColours, 0x3C);
 
-extern  CHudColours &HudColour;
+extern CHudColours& HudColour;
