f9cffa06e5dbcdcba8b0b0ae18536f1a62fdcabd
Izzotop <kirill-j57@yandex.ru>
2021-08-23 01:54:12 +0300

    Hook CTxdStore
    80bc247a54319d544471916ec1368a5e761b5ea4

--- source/game_sa/CTxdStore.cpp
+++ source/game_sa/CTxdStore.cpp
@@ -4,141 +4,170 @@ Authors: GTA Community. See more here
 https://github.com/DK22Pac/plugin-sdk
 Do not delete this comment block. Respect others' work!
 */
-
 #include "StdInc.h"
 
-CPool<TxdDef> *&CTxdStore::ms_pTxdPool = *(CPool<TxdDef> **)0xC8800C;
+CPool<TxdDef>*& CTxdStore::ms_pTxdPool = *(CPool<TxdDef>**)0xC8800C;
 
-RwTexDictionary *CTxdStore::ms_pStoredTxd;
+RwTexDictionary* CTxdStore::ms_pStoredTxd;
 // variables list is not finished. Need to make CPools before.
 
-// class functions
-
-// store current txd
-void CTxdStore::PushCurrentTxd()
-{
-    ((void(__cdecl *)())0x7316A0)();
-}
-
-// restore txd
-void CTxdStore::PopCurrentTxd()
-{
-    ((void(__cdecl *)())0x7316B0)();
-}
-
-// find txd by name. Returning value is txd index
-int CTxdStore::FindTxdSlot(char  const* name)
-{
-    return ((int(__cdecl *)(char  const*))0x731850)(name);
-}
-
-// find txd by name hash. Returning value is txd index
-signed int CTxdStore::FindTxdSlot(unsigned int hash)
-{
-    return ((signed int(__cdecl *)(unsigned int))0x7318E0)(hash);
-}
-
-bool CTxdStore::StartLoadTxd(int index, RwStream *stream)
-{
-    return ((bool(__cdecl *)(int, RwStream *))0x731930)(index, stream);
-}
-
-// create rw tex dictionary for txd slot
-void CTxdStore::Create(int index)
-{
-    ((void(__cdecl *)(int))0x731990)(index);
-}
-
-// set the txd to work with
-void CTxdStore::SetCurrentTxd(int index)
-{
-    ((void(__cdecl *)(int))0x7319C0)(index);
-}
-
-// add reference for txd
-TxdDef *CTxdStore::AddRef(int index)
-{
-    return ((TxdDef *(__cdecl *)(int))0x731A00)(index);
-}
-
-// remove reference, if references count is 0, then delete txd
-void CTxdStore::RemoveRef(int index)
-{
-    ((void(__cdecl *)(int))0x731A30)(index);
-}
-
-// remove reference without deleting
-TxdDef *CTxdStore::RemoveRefWithoutDelete(int index)
-{
-    return ((TxdDef *(__cdecl *)(int))0x731A70)(index);
-}
-
-// get references count
-unsigned int CTxdStore::GetNumRefs(int index)
-{
-    return ((unsigned int(__cdecl *)(int))0x731AA0)(index);
-}
-
-// allocate new slot for this txd
-int CTxdStore::AddTxdSlot(char  const* name)
-{
-    return ((int(__cdecl *)(char  const*))0x731C80)(name);
-}
-
-// remove txd slot
-void CTxdStore::RemoveTxdSlot(int index)
-{
-    ((void(__cdecl *)(int))0x731CD0)(index);
-}
-
-// load txd from stream
-bool CTxdStore::LoadTxd(int index, RwStream *stream)
-{
-    return ((bool(__cdecl *)(int, RwStream *))0x731DD0)(index, stream);
-}
-
-bool CTxdStore::FinishLoadTxd(int index, RwStream *stream)
-{
-    return ((bool(__cdecl *)(int, RwStream *))0x731E40)(index, stream);
-}
-
-// remove txd
-void CTxdStore::RemoveTxd(int index)
-{
-    ((void(__cdecl *)(int))0x731E90)(index);
+void CTxdStore::InjectHooks() {
+    // ReversibleHooks::Install("CTxdStore", "PushCurrentTxd", 0x7316A0, &CTxdStore::PushCurrentTxd);
+    // ReversibleHooks::Install("CTxdStore", "PopCurrentTxd", 0x7316B0, &CTxdStore::PopCurrentTxd);
+    // ReversibleHooks::Install("CTxdStore", "FindTxdSlot_name", 0x731850, static_cast<int (*)(char const*)>(&CTxdStore::FindTxdSlot));
+    // ReversibleHooks::Install("CTxdStore", "FindTxdSlot_hash", 0x7318E0, static_cast<int (*)(unsigned int)>(&CTxdStore::FindTxdSlot));
+    // ReversibleHooks::Install("CTxdStore", "StartLoadTxd", 0x731930, &CTxdStore::StartLoadTxd);
+    // ReversibleHooks::Install("CTxdStore", "Create", 0x731990, &CTxdStore::Create);
+    // ReversibleHooks::Install("CTxdStore", "SetCurrentTxd", 0x7319C0, &CTxdStore::SetCurrentTxd);
+    // ReversibleHooks::Install("CTxdStore", "AddRef", 0x731A00, &CTxdStore::AddRef);
+    // ReversibleHooks::Install("CTxdStore", "RemoveRef", 0x731A30, &CTxdStore::RemoveRef);
+    // ReversibleHooks::Install("CTxdStore", "RemoveRefWithoutDelete", 0x731A70, &CTxdStore::RemoveRefWithoutDelete);
+    // ReversibleHooks::Install("CTxdStore", "GetNumRefs", 0x731AA0, &CTxdStore::GetNumRefs);
+    // ReversibleHooks::Install("CTxdStore", "AddTxdSlot", 0x731C80, &CTxdStore::AddTxdSlot);
+    // ReversibleHooks::Install("CTxdStore", "RemoveTxdSlot", 0x731CD0, &CTxdStore::RemoveTxdSlot);
+    // ReversibleHooks::Install("CTxdStore", "LoadTxd_stream", 0x731DD0, static_cast<bool (*)(int, RwStream*)>(&CTxdStore::LoadTxd));
+    ReversibleHooks::Install("CTxdStore", "LoadTxd_filename", 0x7320B0, static_cast<bool (*)(int, char const*)>(&CTxdStore::LoadTxd));
+    // ReversibleHooks::Install("CTxdStore", "FinishLoadTxd", 0x731E40, &CTxdStore::FinishLoadTxd);
+    // ReversibleHooks::Install("CTxdStore", "RemoveTxd", 0x731E90, &CTxdStore::RemoveTxd);
+    // ReversibleHooks::Install("CTxdStore", "Initialise", 0x731F20, &CTxdStore::Initialise);
+    // ReversibleHooks::Install("CTxdStore", "Shutdown", 0x732000, &CTxdStore::Shutdown);
+    // ReversibleHooks::Install("CTxdStore", "GameShutdown", 0x732060, &CTxdStore::GameShutdown);
+    // ReversibleHooks::Install("CTxdStore", "GetParentTxdSlot", 0x0, &CTxdStore::GetParentTxdSlot);
+    // ReversibleHooks::Install("CTxdStore", "GetTxd", 0x408340, &CTxdStore::GetTxd);
 }
 
 // initialise txd store
-void CTxdStore::Initialise()
-{
-    ((void(__cdecl *)())0x731F20)();
+// 0x731F20
+void CTxdStore::Initialise() {
+    ((void(__cdecl*)())0x731F20)();
 }
 
 // shutdown txd store
-void CTxdStore::Shutdown()
-{
-    ((void(__cdecl *)())0x732000)();
+// 0x732000
+void CTxdStore::Shutdown() {
+    ((void(__cdecl*)())0x732000)();
 }
 
-void CTxdStore::GameShutdown()
-{
-    ((void(__cdecl *)())0x732060)();
+// 0x732060
+void CTxdStore::GameShutdown() {
+    ((void(__cdecl*)())0x732060)();
+}
+
+// 0x731930
+bool CTxdStore::StartLoadTxd(int index, RwStream* stream) {
+    return ((bool(__cdecl*)(int, RwStream*))0x731930)(index, stream);
+}
+
+// 0x731E40
+bool CTxdStore::FinishLoadTxd(int index, RwStream* stream) {
+    return ((bool(__cdecl*)(int, RwStream*))0x731E40)(index, stream);
+}
+
+// load txd from stream
+// 0x731DD0
+bool CTxdStore::LoadTxd(int index, RwStream* stream) {
+    return ((bool(__cdecl*)(int, RwStream*))0x731DD0)(index, stream);
 }
 
 // load txd from file
-bool CTxdStore::LoadTxd(int index, char  const* filename)
-{
-    return ((bool(__cdecl *)(int, char  const*))0x7320B0)(index, filename);
+// 0x7320B0
+bool CTxdStore::LoadTxd(int index, char const* filename) {
+    char      data[260];
+    RwStream* stream{};
+    sprintf(data, "%s", filename);
+    do {
+        stream = RwStreamOpen(rwSTREAMFILENAME, rwSTREAMREAD, data);
+    } while (!stream);
+    bool loaded = CTxdStore::LoadTxd(index, stream);
+    RwStreamClose(stream, nullptr);
+    return loaded;
 }
 
-int CTxdStore::GetParentTxdSlot(int index)
-{
-    typedef int(__cdecl* hGetParentTxdSlot)( int index );
-    hGetParentTxdSlot _GetParentTxdSlot = (hGetParentTxdSlot)0x00408370;
-    return _GetParentTxdSlot(index);
+// store current txd
+// 0x7316A0
+void CTxdStore::PushCurrentTxd() {
+    ((void(__cdecl*)())0x7316A0)();
 }
 
-size_t CTxdStore::GetTxd(int index)
-{
+// restore txd
+// 0x7316B0
+void CTxdStore::PopCurrentTxd() {
+    ((void(__cdecl*)())0x7316B0)();
+}
+
+// set the txd to work with
+// 0x7319C0
+void CTxdStore::SetCurrentTxd(int index) {
+    ((void(__cdecl*)(int))0x7319C0)(index);
+}
+
+// find txd by name. Returning value is txd index
+// 0x731850
+int CTxdStore::FindTxdSlot(char const* name) {
+    return ((int(__cdecl*)(char const*))0x731850)(name);
+}
+
+// find txd by name hash. Returning value is txd index
+// 0x7318E0
+int CTxdStore::FindTxdSlot(unsigned int hash) {
+    return ((signed int(__cdecl*)(unsigned int))0x7318E0)(hash);
+}
+
+// plugin-sdk has named it to getTexDictionary
+size_t CTxdStore::GetTxd(int index) {
     return plugin::CallAndReturn<size_t, 0x408340, int>(index);
+}
+
+// plugin-sdk has named it to getTXDEntryParentIndex
+// 0x408370
+int CTxdStore::GetParentTxdSlot(int index) {
+    return plugin::CallAndReturn<int, 0x408370, int>(index);
+}
+
+// create rw tex dictionary for txd slot
+// 0x731990
+void CTxdStore::Create(int index) {
+    ((void(__cdecl*)(int))0x731990)(index);
+}
+
+// allocate new slot for this txd
+// 0x731C80
+int CTxdStore::AddTxdSlot(char const* name) {
+    return ((int(__cdecl*)(char const*))0x731C80)(name);
+}
+
+// remove txd slot
+// 0x731CD0
+void CTxdStore::RemoveTxdSlot(int index) {
+    ((void(__cdecl*)(int))0x731CD0)(index);
+}
+
+// remove txd
+// 0x731E90
+void CTxdStore::RemoveTxd(int index) {
+    ((void(__cdecl*)(int))0x731E90)(index);
+}
+
+// add reference for txd
+// 0x731A00
+TxdDef* CTxdStore::AddRef(int index) {
+    return ((TxdDef * (__cdecl*)(int))0x731A00)(index);
+}
+
+// remove reference, if references count is 0, then delete txd
+// 0x731A30
+void CTxdStore::RemoveRef(int index) {
+    ((void(__cdecl*)(int))0x731A30)(index);
+}
+
+// remove reference without deleting
+// 0x731A70
+TxdDef* CTxdStore::RemoveRefWithoutDelete(int index) {
+    return ((TxdDef * (__cdecl*)(int))0x731A70)(index);
+}
+
+// get references count
+// 0x731AA0
+unsigned int CTxdStore::GetNumRefs(int index) {
+    return ((unsigned int(__cdecl*)(int))0x731AA0)(index);
 }
--- source/game_sa/CTxdStore.h
+++ source/game_sa/CTxdStore.h
@@ -6,62 +6,49 @@
 */
 #pragma once
 
-#include "PluginBase.h"
 #include "RenderWare.h"
 #include "TxdDef.h"
 #include "CPool.h"
 
-class  CTxdStore
-{
+class CTxdStore {
 public:
-	// class variables
+    // class variables
 
-	static CPool<TxdDef> *&ms_pTxdPool;
+    static CPool<TxdDef>*& ms_pTxdPool;
 
-	static RwTexDictionary *ms_pStoredTxd;
-	// variables list is not finished. Need to make CPools before.
+    static RwTexDictionary* ms_pStoredTxd;
+    // variables list is not finished. Need to make CPools before.
 
-	// class functions
+public:
+    static void InjectHooks();
 
-	// store current txd
-	static void PushCurrentTxd();
-	// restore txd
-	static void PopCurrentTxd();
-	// find txd by name. Returning value is txd index
-	static int FindTxdSlot(char  const* name);
-	// find txd by name hash. Returning value is txd index
-	static int FindTxdSlot(unsigned int hash);
-	static bool StartLoadTxd(int index, RwStream *stream);
-	// create rw tex dictionary for txd slot
-	static void Create(int index);
-	// set the txd to work with
-	static void SetCurrentTxd(int index);
-	// add reference for txd
-	static TxdDef *AddRef(int index);
-	// remove reference, if references count is 0, then delete txd
-	static void RemoveRef(int index);
-	// remove reference without deleting
-	static TxdDef *RemoveRefWithoutDelete(int index);
-	// get references count
-	static unsigned int GetNumRefs(int index);
-	// allocate new slot for this txd
-	static int AddTxdSlot(char  const* name);
-	// remove txd slot
-	static void RemoveTxdSlot(int index);
-	// load txd from stream
-	static bool LoadTxd(int index, RwStream *stream);
-	static bool FinishLoadTxd(int index, RwStream *stream);
-	// remove txd
-	static void RemoveTxd(int index);
-	// initialise txd store
-	static void Initialise();
-	// shutdown txd store
-	static void Shutdown();
-	static void GameShutdown();
-	// load txd from file
-	static bool LoadTxd(int index, char  const* filename);
-    // plugin-sdk has named it to getTXDEntryParentIndex
-    static int GetParentTxdSlot(int index);
-    // plugin-sdk has named it to getTexDictionary
+    static void Initialise();
+    static void Shutdown();
+    static void GameShutdown();
+
+    static bool StartLoadTxd(int index, RwStream* stream);
+    static bool FinishLoadTxd(int index, RwStream* stream);
+    static bool LoadTxd(int index, RwStream* stream);
+    static bool LoadTxd(int index, char const* filename);
+
+    static void PushCurrentTxd();
+    static void PopCurrentTxd();
+    static void SetCurrentTxd(int index);
+
+    static int FindTxdSlot(char const* name);
+    static int FindTxdSlot(unsigned int hash);
+
     static size_t GetTxd(int index);
+    static int GetParentTxdSlot(int index);
+
+    static void Create(int index);
+
+    static int AddTxdSlot(char const* name);
+    static void RemoveTxdSlot(int index);
+    static void RemoveTxd(int index);
+
+    static TxdDef* AddRef(int index);
+    static void RemoveRef(int index);
+    static TxdDef* RemoveRefWithoutDelete(int index);
+    static unsigned int GetNumRefs(int index);
 };
