8fbe248b53e51b118cd4680082c9b3be105bc177
Pirulax <patrikjankovics7@gmail.com>
2021-08-28 02:03:48 +0200

    Addendum to Update_OncePerFrame

--- source/game_sa/CFireManager.cpp
+++ source/game_sa/CFireManager.cpp
@@ -6,3 +6,7 @@ CFire* CFireManager::FindNearestFire(CVector const& position, bool notBeingExtin
 {
     return plugin::CallMethodAndReturn<CFire*, 0x538F40, CFireManager*, CVector const&, bool, bool>(this, position, notBeingExtinguished, notScript);
 }
+
+bool CFireManager::ExtinguishPointWithWater(CVector point, float fRadiusSq, float fireSize) {
+	return plugin::CallMethodAndReturn<bool, 0x5394C0, CFireManager*, CVector, float, float>(this, point, fRadiusSq, fireSize);
+}
