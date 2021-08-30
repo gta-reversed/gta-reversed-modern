4a1f21af12d31035814156dc45527d929734be31
Pirulax <patrikjankovics7@gmail.com>
2021-08-30 23:32:28 +0200

    Init

--- source/InjectHooksMain.cpp
+++ source/InjectHooksMain.cpp
@@ -38,6 +38,7 @@ void InjectHooksMain()
 {
     // WaitForDebugger();
 
+    CBrightLights::InjectHooks();
     COnscreenCounterEntry::InjectHooks();
     CPedTaskPair::InjectHooks();
     CDirectory::InjectHooks();
--- source/game_sa/CBrightLights.cpp
+++ source/game_sa/CBrightLights.cpp
@@ -1,11 +1,32 @@
 #include "StdInc.h"
+#include "CBrightLights.h"
 
-void CBrightLights::RenderOutGeometryBuffer()
-{
+int32_t& CBrightLights::NumBrightLights = *(int32_t*)0xC7C6FC;
+tBrightLight (&CBrightLights::aBrightLights)[MAX_NUM_BRIGHTLIGHTS] = *(tBrightLight(*)[MAX_NUM_BRIGHTLIGHTS])0xC7CB58;
+
+void CBrightLights::InjectHooks() {
+    ReversibleHooks::Install("CBrightLights", "Init", 0x722140, &CBrightLights::Init);
+    // ReversibleHooks::Install("CBrightLights", "RenderOutGeometryBuffer", 0x722150, &CBrightLights::RenderOutGeometryBuffer);
+    // ReversibleHooks::Install("CBrightLights", "Render", 0x7241C0, &CBrightLights::Render);
+    // ReversibleHooks::Install("CBrightLights", "RegisterOne", 0x724770, &CBrightLights::RegisterOne);
+}
+
+// 0x722140
+void CBrightLights::Init() {
+    NumBrightLights = 0;
+}
+
+// 0x722150
+void CBrightLights::RenderOutGeometryBuffer() {
     plugin::Call<0x722150>();
 }
 
-void CBrightLights::RegisterOne(CVector posn, CVector top, CVector right, CVector at, unsigned char color, unsigned char arg5, unsigned char arg6, unsigned char arg7)
-{
-    plugin::Call<0x724770, CVector, CVector, CVector, CVector, unsigned char, unsigned char, unsigned char, unsigned char>(posn, top, right, at, color, arg5, arg6, arg7);
+// 0x7241C0
+void CBrightLights::Render() {
+    plugin::Call<0x7241C0>();
+}
+
+// 0x724770
+void CBrightLights::RegisterOne(CVector posn, CVector top, CVector right, CVector at, uint8_t color, uint8_t arg5, uint8_t arg6, uint8_t arg7) {
+    plugin::Call<0x724770, CVector, CVector, CVector, CVector, uint8_t, uint8_t, uint8_t, uint8_t>(posn, top, right, at, color, arg5, arg6, arg7);
 }
