cae65ce073b7c9c94bfa782e7b04c57c4dd07fd6
Izzotop <kirill-j57@yandex.ru>
2021-09-14 00:40:49 +0300

    Reverse CBridge

--- source/game_sa/CBridge.cpp
+++ source/game_sa/CBridge.cpp
@@ -1,33 +1,37 @@
 #include "StdInc.h"
 
 void CBridge::InjectHooks() {
-//    ReversibleHooks::Install("CBridge", "Init", 0x41BC70, &CBridge::Init);
-//    ReversibleHooks::Install("CBridge", "Update", 0x41BC80, &CBridge::Update);
-//    ReversibleHooks::Install("CBridge", "FindBridgeEntities", 0x0, &CBridge::FindBridgeEntities);
-//    ReversibleHooks::Install("CBridge", "ShouldLightsBeFlashing", 0x41BC90, &CBridge::ShouldLightsBeFlashing);
+    using namespace ReversibleHooks;
+    Install("CBridge", "Init", 0x41BC70, &CBridge::Init);
+    Install("CBridge", "Update", 0x41BC80, &CBridge::Update);
+    Install("CBridge", "FindBridgeEntities", 0x41BCA0, &CBridge::FindBridgeEntities);
+    Install("CBridge", "ShouldLightsBeFlashing", 0x41BC90, &CBridge::ShouldLightsBeFlashing);
 }
 
 // 0x41BC70
 void CBridge::Init() {
-    plugin::Call<0x41BC70>();
+    // NOP
 }
 
 // 0x41BC80
 void CBridge::Update() {
-    plugin::Call<0x41BC80>();
+    // NOP
 }
 
-// 0x0
-void CBridge::FindBridgeEntities() {
-    plugin::Call<0x0>();
-}
-
+// used in CEntity::ProcessLightsForEntity
+// always false
 // 0x41BC90
 bool CBridge::ShouldLightsBeFlashing() {
     return false;
 }
 
+// 0x41BCA0
+void CBridge::FindBridgeEntities() {
+    // NOP
+}
+
 // used in CPlayerInfo::Process
+// 0x41BCB0
 bool CBridge::ThisIsABridgeObjectMovingUp(uint32) {
     return false;
 }
