1cfcd891ee06431bc8da4b5ac80a832c19c4e806
Izzotop <kirill-j57@yandex.ru>
2021-09-13 22:32:22 +0300

    Fix CTaskComplexEnterCar ctor
    Parent class didn't called.

--- source/game_sa/Tasks/TaskTypes/CTaskComplexEnterBoatAsDriver.h
+++ source/game_sa/Tasks/TaskTypes/CTaskComplexEnterBoatAsDriver.h
@@ -1,11 +1,10 @@
 /*
-Plugin-SDK (Grand Theft Auto San Andreas) header file
-Authors: GTA Community. See more here
-https://github.com/DK22Pac/plugin-sdk
-Do not delete this comment block. Respect others' work!
+    Plugin-SDK (Grand Theft Auto San Andreas) header file
+    Authors: GTA Community. See more here
+    https://github.com/DK22Pac/plugin-sdk
+    Do not delete this comment block. Respect others' work!
 */
 #pragma once
-
 
 #include "CTaskComplex.h"
 #include "CVehicle.h"
@@ -14,7 +13,12 @@ class CTaskComplexEnterBoatAsDriver : public CTaskComplex {
 public:
     CVehicle* m_pTargetVehicle;
 
-    CTaskComplexEnterBoatAsDriver(CVehicle* pTargetVehicle);
+public:
+    CTaskComplexEnterBoatAsDriver(CVehicle* targetVehicle);
+
+private:
+    friend void InjectHooksMain();
+    static void InjectHooks();
 
 };
 
