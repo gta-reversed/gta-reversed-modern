ea9f1703a232b7e62dadd0b02978cd2252fe1ed1
Izzotop <kirill-j57@yandex.ru>
2021-09-13 21:26:40 +0300

    Fix CTaskComplexArrestPed ctor
    Parent class didn't called.

--- source/game_sa/Tasks/TaskTypes/CTaskComplexArrestPed.h
+++ source/game_sa/Tasks/TaskTypes/CTaskComplexArrestPed.h
@@ -1,22 +1,22 @@
+#pragma once
+
 #include "CTaskComplex.h"
 
 class CVehicle;
+
 class CTaskComplexArrestPed : public CTaskComplex {
 public:
-    bool m_subTaskNeedsToBeCreated;
-    CPed* m_pedToArrest;
-    int32 field_14;
-    int32 field_18;
-    int32 field_1C;
+    bool      m_subTaskNeedsToBeCreated;
+    CPed*     m_pedToArrest;
+    int32     field_14;
+    int32     field_18;
+    int32     field_1C;
     CVehicle* m_vehicle;
 
-    static void InjectHooks();
-
+public:
     CTaskComplexArrestPed(CPed* ped);
     ~CTaskComplexArrestPed();
-private:
-    CTaskComplexArrestPed* Constructor(CPed* ped);
-public:
+
     CTask* Clone() override { return new CTaskComplexArrestPed(m_pedToArrest); }
     eTaskType GetTaskType() override { return TASK_COMPLEX_ARREST_PED; }
     bool MakeAbortable(CPed* ped, eAbortPriority priority, const CEvent* event) override;
@@ -25,6 +25,11 @@ public:
     CTask* ControlSubTask(CPed* ped) override;
     CTask* CreateSubTask(int32 taskId, CPed* ped);
 
+private:
+    friend void InjectHooksMain();
+    static void InjectHooks();
+
+    CTaskComplexArrestPed* Constructor(CPed* ped);
 };
 
 VALIDATE_SIZE(CTaskComplexArrestPed, 0x24);
