bd7b06411b3787a70fb612d456fd83598825e64c
Izzotop <kirill-j57@yandex.ru>
2021-09-13 22:56:33 +0300

    Fix CTaskComplexGoToPointAndStandStill ctor
    Parent class didn't called.

--- source/game_sa/Tasks/TaskTypes/CTaskComplexGoToPointAndStandStill.h
+++ source/game_sa/Tasks/TaskTypes/CTaskComplexGoToPointAndStandStill.h
@@ -1,19 +1,18 @@
 #pragma once
+
 #include "CTaskComplex.h"
 #include "CVector.h"
 
 class CTaskSimpleGoToPoint;
 
-class CTaskComplexGoToPointAndStandStill : public CTaskComplex 
-{
+class CTaskComplexGoToPointAndStandStill : public CTaskComplex {
 public:
-    int32 m_moveState;
+    int32   m_moveState;
     CVector m_vecTargetPoint;
-    float m_fRadius; // ped stops moving after it is within the radius of the point.
-    float m_fMoveStateRadius; // The ped will either sprint, run, or walk depending on far it is from the radius
+    float   m_fRadius;          // ped stops moving after it is within the radius of the point.
+    float   m_fMoveStateRadius; // The ped will either sprint, run, or walk depending on far it is from the radius
     union {
-        struct
-        {
+        struct {
             uint8 m_b01 : 1;
             uint8 m_bGoToPoint : 1;
             uint8 m_bTargetPointUpdated : 1;
@@ -22,17 +21,13 @@ public:
         };
         uint8 m_nFlags;
     };
-private:
-    uint8 field_25[3]; // padding
-public:
-    static void InjectHooks();
-    CTaskComplexGoToPointAndStandStill(int32 moveState, const CVector& targetPoint, float fRadius, float fMoveStateRadius, bool bUnknown, bool bGoToPoint);
-    ~CTaskComplexGoToPointAndStandStill();
+    char _pad_25[3];
 
-private:
-    CTaskComplexGoToPointAndStandStill* Constructor(int32 moveState, const CVector& targetPoint, float fRadius, float fMoveStateRadius, bool bUnknown, bool bGoToPoint);
 public:
-    CTask* Clone() override;
+    CTaskComplexGoToPointAndStandStill(int32 moveState, const CVector& targetPoint, float fRadius, float fMoveStateRadius, bool bUnknown, bool bGoToPoint);
+    ~CTaskComplexGoToPointAndStandStill() override;
+
+    CTask*    Clone() override;
     eTaskType GetTaskType() override { return TASK_COMPLEX_GO_TO_POINT_AND_STAND_STILL; };
     CTask* CreateNextSubTask(CPed* ped) override;
     CTask* CreateFirstSubTask(CPed* ped) override;
@@ -43,10 +38,15 @@ public:
     CTask* CreateFirstSubTask_Reversed(CPed* ped);
     CTask* ControlSubTask_Reversed(CPed* ped);
 
-
-    void GoToPoint(const CVector& targetPoint, float fRadius, float fMoveStateRadius, bool bUpdateTargetEvenIfItsTheSame);
-    void SelectMoveState(CTaskSimpleGoToPoint* pGotoPointTask, CPed* pPed, float fMoveStateRadius, float fRunOrSprintRadius);
+    void   GoToPoint(const CVector& targetPoint, float fRadius, float fMoveStateRadius, bool bUpdateTargetEvenIfItsTheSame);
+    void   SelectMoveState(CTaskSimpleGoToPoint* pGotoPointTask, CPed* pPed, float fMoveStateRadius, float fRunOrSprintRadius);
     CTask* CreateFirstSubTask(int32 taskId, CPed* ped);
+
+private:
+    friend void InjectHooksMain();
+    static void InjectHooks();
+
+    CTaskComplexGoToPointAndStandStill* Constructor(int32 moveState, const CVector& targetPoint, float fRadius, float fMoveStateRadius, bool bUnknown, bool bGoToPoint);
 };
 
 VALIDATE_SIZE(CTaskComplexGoToPointAndStandStill, 0x28);
