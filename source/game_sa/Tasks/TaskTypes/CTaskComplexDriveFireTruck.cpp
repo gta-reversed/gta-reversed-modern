831863ece652de90fe9f9513297b23819cb70910
Izzotop <kirill-j57@yandex.ru>
2021-09-13 22:27:00 +0300

    Fix CTaskComplexDriveFireTruck ctor
    Parent class didn't called.

--- source/game_sa/Tasks/TaskTypes/CTaskComplexDriveFireTruck.cpp
+++ source/game_sa/Tasks/TaskTypes/CTaskComplexDriveFireTruck.cpp
@@ -7,79 +7,69 @@
 #include "CTaskComplexUseWaterCannon.h"
 #include "CTaskSimpleCarDrive.h"
 
-void CTaskComplexDriveFireTruck::InjectHooks()
-{
-    ReversibleHooks::Install("CTaskComplexDriveFireTruck", "Constructor", 0x659310, &CTaskComplexDriveFireTruck::Constructor);
-    ReversibleHooks::Install("CTaskComplexDriveFireTruck", "CreateSubTask", 0x65A240, &CTaskComplexDriveFireTruck::CreateSubTask);
-    //VTABLE
-    ReversibleHooks::Install("CTaskComplexDriveFireTruck", "Clone", 0x659BC0, &CTaskComplexDriveFireTruck::Clone_Reversed);
-    ReversibleHooks::Install("CTaskComplexDriveFireTruck", "CreateFirstSubTask", 0x65B140, &CTaskComplexDriveFireTruck::CreateFirstSubTask_Reversed);
-    ReversibleHooks::Install("CTaskComplexDriveFireTruck", "CreateNextSubTask", 0x65B090, &CTaskComplexDriveFireTruck::CreateNextSubTask_Reversed);
-    ReversibleHooks::Install("CTaskComplexDriveFireTruck", "ControlSubTask", 0x65B1E0, &CTaskComplexDriveFireTruck::ControlSubTask_Reversed);
+void CTaskComplexDriveFireTruck::InjectHooks() {
+    using namespace ReversibleHooks;
+    Install("CTaskComplexDriveFireTruck", "Constructor", 0x659310, &CTaskComplexDriveFireTruck::Constructor);
+    Install("CTaskComplexDriveFireTruck", "CreateSubTask", 0x65A240, &CTaskComplexDriveFireTruck::CreateSubTask);
+    Install("CTaskComplexDriveFireTruck", "Clone", 0x659BC0, &CTaskComplexDriveFireTruck::Clone_Reversed);
+    Install("CTaskComplexDriveFireTruck", "CreateFirstSubTask", 0x65B140, &CTaskComplexDriveFireTruck::CreateFirstSubTask_Reversed);
+    Install("CTaskComplexDriveFireTruck", "CreateNextSubTask", 0x65B090, &CTaskComplexDriveFireTruck::CreateNextSubTask_Reversed);
+    Install("CTaskComplexDriveFireTruck", "ControlSubTask", 0x65B1E0, &CTaskComplexDriveFireTruck::ControlSubTask_Reversed);
 }
 
-CTaskComplexDriveFireTruck* CTaskComplexDriveFireTruck::Constructor(CVehicle* pVehicle, CPed* pPartnerFireman, bool bIsDriver)
-{
-    this->CTaskComplexDriveFireTruck::CTaskComplexDriveFireTruck(pVehicle, pPartnerFireman, bIsDriver);
+CTaskComplexDriveFireTruck* CTaskComplexDriveFireTruck::Constructor(CVehicle* vehicle, CPed* partnerFireman, bool bIsDriver) {
+    this->CTaskComplexDriveFireTruck::CTaskComplexDriveFireTruck(vehicle, partnerFireman, bIsDriver);
     return this;
 }
 
 // 0x659310
-CTaskComplexDriveFireTruck::CTaskComplexDriveFireTruck(CVehicle* pVehicle, CPed* pPartnerFireman, bool bIsDriver)
-{
-    m_pVehicle = pVehicle;
-    m_pPartnerFireman = pPartnerFireman;
-    m_bIsDriver = bIsDriver;
-    m_pFire = nullptr;
+CTaskComplexDriveFireTruck::CTaskComplexDriveFireTruck(CVehicle* vehicle, CPed* partnerFireman, bool bIsDriver) : CTaskComplex() {
+    m_pVehicle        = vehicle;
+    m_pPartnerFireman = partnerFireman;
+    m_bIsDriver       = bIsDriver;
+    m_pFire           = nullptr;
 
     if (m_pVehicle)
-        m_pVehicle->RegisterReference(reinterpret_cast<CEntity * *>(&m_pVehicle));
+        m_pVehicle->RegisterReference(reinterpret_cast<CEntity**>(&m_pVehicle));
 
     if (m_pPartnerFireman)
-        m_pPartnerFireman->RegisterReference(reinterpret_cast<CEntity * *>(&m_pPartnerFireman));
+        m_pPartnerFireman->RegisterReference(reinterpret_cast<CEntity**>(&m_pPartnerFireman));
 }
 
 // 0x6593A0
-CTaskComplexDriveFireTruck::~CTaskComplexDriveFireTruck()
-{
+CTaskComplexDriveFireTruck::~CTaskComplexDriveFireTruck() {
     if (m_pVehicle)
-        m_pVehicle->CleanUpOldReference(reinterpret_cast<CEntity * *>(&m_pVehicle));
+        m_pVehicle->CleanUpOldReference(reinterpret_cast<CEntity**>(&m_pVehicle));
 
     if (m_pPartnerFireman)
-        m_pPartnerFireman->CleanUpOldReference(reinterpret_cast<CEntity * *>(&m_pPartnerFireman));
+        m_pPartnerFireman->CleanUpOldReference(reinterpret_cast<CEntity**>(&m_pPartnerFireman));
 }
 
 // 0x659BC0
-CTask* CTaskComplexDriveFireTruck::Clone()
-{
+CTask* CTaskComplexDriveFireTruck::Clone() {
     return Clone_Reversed();
 }
 
 // 0x65B090
-CTask* CTaskComplexDriveFireTruck::CreateNextSubTask(CPed* ped)
-{
+CTask* CTaskComplexDriveFireTruck::CreateNextSubTask(CPed* ped) {
     return CreateNextSubTask_Reversed(ped);
 }
 
 // 0x65B140
-CTask* CTaskComplexDriveFireTruck::CreateFirstSubTask(CPed* ped)
-{
+CTask* CTaskComplexDriveFireTruck::CreateFirstSubTask(CPed* ped) {
     return CreateFirstSubTask_Reversed(ped);
 }
 
 // 0x65B1E0
-CTask* CTaskComplexDriveFireTruck::ControlSubTask(CPed* ped)
-{
+CTask* CTaskComplexDriveFireTruck::ControlSubTask(CPed* ped) {
     return ControlSubTask_Reversed(ped);
 }
 
-CTask* CTaskComplexDriveFireTruck::Clone_Reversed()
-{
+CTask* CTaskComplexDriveFireTruck::Clone_Reversed() {
     return new CTaskComplexDriveFireTruck(m_pVehicle, m_pPartnerFireman, m_bIsDriver);
 }
 
-CTask* CTaskComplexDriveFireTruck::CreateNextSubTask_Reversed(CPed* ped)
-{
+CTask* CTaskComplexDriveFireTruck::CreateNextSubTask_Reversed(CPed* ped) {
     eTaskType subTaskType = m_pSubTask->GetTaskType();
 
     if (subTaskType == TASK_COMPLEX_CAR_DRIVE_WANDER)
@@ -97,8 +87,7 @@ CTask* CTaskComplexDriveFireTruck::CreateNextSubTask_Reversed(CPed* ped)
     return nullptr;
 }
 
-CTask* CTaskComplexDriveFireTruck::CreateFirstSubTask_Reversed(CPed* ped)
-{
+CTask* CTaskComplexDriveFireTruck::CreateFirstSubTask_Reversed(CPed* ped) {
     if (!m_pVehicle || !ped->bInVehicle)
         return CreateSubTask(TASK_FINISHED, ped);
 
@@ -109,32 +98,27 @@ CTask* CTaskComplexDriveFireTruck::CreateFirstSubTask_Reversed(CPed* ped)
     return CreateSubTask(m_pFire ? TASK_COMPLEX_CAR_DRIVE_TO_POINT : TASK_COMPLEX_CAR_DRIVE_WANDER, ped);
 }
 
-CTask* CTaskComplexDriveFireTruck::ControlSubTask_Reversed(CPed* ped)
-{
+CTask* CTaskComplexDriveFireTruck::ControlSubTask_Reversed(CPed* ped) {
     eTaskType subTaskId = m_pSubTask->GetTaskType();
 
     if (!m_pVehicle || !ped->bInVehicle)
         return CreateFirstSubTask(ped);
 
-    if (subTaskId == TASK_COMPLEX_CAR_DRIVE_TO_POINT)
-    {
-        if (m_bIsDriver && m_pFire->m_nFlags.bBeingExtinguished)
-        {
+    if (subTaskId == TASK_COMPLEX_CAR_DRIVE_TO_POINT) {
+        if (m_bIsDriver && m_pFire->m_nFlags.bBeingExtinguished) {
             CFire* newFire = gFireManager.FindNearestFire(m_pVehicle->GetPosition(), true, true);
 
             if (!newFire)
                 return CreateSubTask(TASK_COMPLEX_CAR_DRIVE_WANDER, ped);
 
-            if (newFire != m_pFire)
-            {
+            if (newFire != m_pFire) {
                 m_pFire = newFire;
                 reinterpret_cast<CTaskComplexDriveToPoint*>(m_pSubTask)->point = m_pFire->m_vecPosition;
             }
         }
     }
 
-    if (subTaskId == TASK_COMPLEX_CAR_DRIVE_WANDER)
-    {
+    if (subTaskId == TASK_COMPLEX_CAR_DRIVE_WANDER) {
         if (m_bIsDriver && gFireManager.FindNearestFire(m_pVehicle->GetPosition(), true, true))
             return CreateFirstSubTask(ped);
     }
@@ -143,10 +127,8 @@ CTask* CTaskComplexDriveFireTruck::ControlSubTask_Reversed(CPed* ped)
 }
 
 // 0x65A240
-CTask* CTaskComplexDriveFireTruck::CreateSubTask(eTaskType taskType, CPed* ped)
-{
-    switch (taskType)
-    {
+CTask* CTaskComplexDriveFireTruck::CreateSubTask(eTaskType taskType, CPed* ped) {
+    switch (taskType) {
     case TASK_COMPLEX_CAR_DRIVE_WANDER:
         return new CTaskComplexCarDriveWander(m_pVehicle, 0, 10.0F);
     case TASK_COMPLEX_CAR_DRIVE_TO_POINT:
@@ -154,7 +136,7 @@ CTask* CTaskComplexDriveFireTruck::CreateSubTask(eTaskType taskType, CPed* ped)
     case TASK_COMPLEX_USE_WATER_CANNON:
         return new CTaskComplexUseWaterCannon(m_pFire);
     case TASK_SIMPLE_CAR_DRIVE:
-        return new CTaskSimpleCarDrive(m_pVehicle, 0, false);
+        return new CTaskSimpleCarDrive(m_pVehicle, nullptr, false);
     default:
         return nullptr;
     }
--- source/game_sa/Tasks/TaskTypes/CTaskComplexDriveFireTruck.h
+++ source/game_sa/Tasks/TaskTypes/CTaskComplexDriveFireTruck.h
@@ -1,24 +1,20 @@
 #pragma once
+
 #include "CTaskComplex.h"
 
-class CTaskComplexDriveFireTruck : public CTaskComplex
-{
+class CTaskComplexDriveFireTruck : public CTaskComplex {
 public:
     CVehicle* m_pVehicle;
-    uint8 m_bIsDriver;
-    uint8 _pad_11[3];
-    CPed* m_pPartnerFireman;
-    CFire* m_pFire;
+    uint8     m_bIsDriver;
+    uint8     _pad_11[3];
+    CPed*     m_pPartnerFireman;
+    CFire*    m_pFire;
 
-private:
-    CTaskComplexDriveFireTruck* Constructor(CVehicle* pVehicle, CPed* pPartnerFireman, bool bIsDriver);
 public:
-    CTaskComplexDriveFireTruck(CVehicle* pVehicle, CPed* pPartnerFireman, bool bIsDriver);
+    CTaskComplexDriveFireTruck(CVehicle* vehicle, CPed* partnerFireman, bool bIsDriver);
     ~CTaskComplexDriveFireTruck() override;
 
-    static void InjectHooks();
-
-    CTask* Clone() override;
+    CTask*    Clone() override;
     eTaskType GetTaskType() override { return TASK_COMPLEX_DRIVE_FIRE_TRUCK; }
     CTask* CreateNextSubTask(CPed* ped) override;
     CTask* CreateFirstSubTask(CPed* ped) override;
@@ -30,6 +26,12 @@ public:
     CTask* ControlSubTask_Reversed(CPed* ped);
 
     CTask* CreateSubTask(eTaskType taskType, CPed* ped);
+
+private:
+    friend void InjectHooksMain();
+    static void InjectHooks();
+
+    CTaskComplexDriveFireTruck* Constructor(CVehicle* vehicle, CPed* partnerFireman, bool bIsDriver);
 };
 
 VALIDATE_SIZE(CTaskComplexDriveFireTruck, 0x1C);
