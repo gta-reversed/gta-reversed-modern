c63eef2f5d9eb1dc6145c7e3bc699d3069ea9e78
Izzotop <kirill-j57@yandex.ru>
2021-09-13 23:23:13 +0300

    Fix CTaskComplexPartner ctor
    Parent class didn't called.

--- source/game_sa/Tasks/TaskTypes/CTaskComplexPartner.h
+++ source/game_sa/Tasks/TaskTypes/CTaskComplexPartner.h
@@ -1,47 +1,50 @@
-#pragma once 
+#pragma once
+
 #include "CTaskComplex.h"
 
 class CTaskComplexSequence;
 
-enum ePartnerState : int8
-{
+enum ePartnerState : int8 {
     PARTNER_STATE_UNK_1 = 1
 };
 
-class CTaskComplexPartner : public CTaskComplex
-{
+class CTaskComplexPartner : public CTaskComplex {
 public:
-    int32 field_C;
-    int32 field_10;
-    char m_commandName[32];
-    int32 m_taskId;
-    CPed* m_partner;
-    float m_distanceMultiplier;
-    CVector m_point;
-    CVector m_targetPoint;
-    bool m_leadSpeaker;
+    int32         field_C;
+    int32         field_10;
+    char          m_commandName[32];
+    int32         m_taskId;
+    CPed*         m_partner;
+    float         m_distanceMultiplier;
+    CVector       m_point;
+    CVector       m_targetPoint;
+    bool          m_leadSpeaker;
     ePartnerState m_partnerState;
-    int8 m_firstToTargetFlag;
-    int8 m_updateDirectionCount;
-    bool m_taskCompleted;
-    bool m_makePedAlwaysFacePartner;
-    char m_animBlockName[16];
-    bool m_requiredAnimsStreamedIn;
-    int8 padding;
+    int8          m_firstToTargetFlag;
+    int8          m_updateDirectionCount;
+    bool          m_taskCompleted;
+    bool          m_makePedAlwaysFacePartner;
+    char          m_animBlockName[16];
+    bool          m_requiredAnimsStreamedIn;
+    char          _pad;
 
-    static void InjectHooks();
-
+public:
     CTaskComplexPartner(const char* commandName, CPed* partner, bool leadSpeaker, float distanceMultiplier, bool makePedAlwaysFacePartner, int8 updateDirectionCount, CVector point);
     ~CTaskComplexPartner();
-private:
-    CTaskComplexPartner* Constructor(const char* commandName, CPed* partner, bool leadSpeaker, float distanceMultiplier, bool makePedAlwaysFacePartner, int8 updateDirectionCount, CVector point);
-public:
+
     eTaskType GetTaskType() override { return TASK_COMPLEX_PARTNER; }
-    CTask* CreateNextSubTask(CPed* ped) override;
-    CTask* CreateFirstSubTask(CPed* ped) override;
-    CTask* ControlSubTask(CPed* ped) override;
+    CTask*       CreateNextSubTask(CPed* ped) override;
+    CTask*       CreateFirstSubTask(CPed* ped) override;
+    CTask*       ControlSubTask(CPed* ped) override;
     virtual void StreamRequiredAnims();
     virtual void RemoveStreamedAnims();
+
+
+private:
+    friend void InjectHooksMain();
+    static void InjectHooks();
+
+    CTaskComplexPartner* Constructor(const char* commandName, CPed* partner, bool leadSpeaker, float distanceMultiplier, bool makePedAlwaysFacePartner, int8 updateDirectionCount, CVector point);
 };
 
 VALIDATE_SIZE(CTaskComplexPartner, 0x70);
