32a1f0e3183c96641c16ede9ac207599a0379cc7
Izzotop <kirill-j57@yandex.ru>
2021-09-14 06:08:54 +0300

    Fix CTaskComplexWalkRoundObject ctor/dtor
    * ctor signature, supercall
    * dtor override

--- source/game_sa/Tasks/TaskTypes/CTaskComplexWalkRoundCar.cpp
+++ source/game_sa/Tasks/TaskTypes/CTaskComplexWalkRoundCar.cpp
@@ -2,7 +2,6 @@
 
 #include "CTaskComplexWalkRoundCar.h"
 
-void CTaskComplexWalkRoundCar::SetNewVehicle(CVehicle* vehicle, uint8 flags)
-{
+void CTaskComplexWalkRoundCar::SetNewVehicle(CVehicle* vehicle, uint8 flags) {
     plugin::CallMethod<0x654290, CTaskComplexWalkRoundCar*, CVehicle*, uint8>(this, vehicle, flags);
 }
