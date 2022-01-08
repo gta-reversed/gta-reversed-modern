3e66554c70c4763231e4394bd82a91b5d49ca5b6
Izzotop <1680033+Izzotop@users.noreply.github.com>
2022-01-08 23:09:26 +0300

    Reverse CCreepingFire (#65)
    * Reverse CCreepingFire
    
    TryToStartFireAtCoors
    Update
    SetReadyToBurn
    Boilerplate
    
    # Conflicts:
    #	source/InjectHooksMain.cpp
    #	source/game_sa/CreepingFire.h
    
    Author: Pirulax <patrikjankovics7@gmail.com>
    Co-authored-by: Izzotop

--- source/game_sa/Game.cpp
+++ source/game_sa/Game.cpp
@@ -20,6 +20,7 @@
 // todo: #include "ShadowManager.h"
 #include "PedType.h"
 #include "IKChainManager_c.h"
+#include "CreepingFire.h"
 
 char (&CGame::aDatFile)[32] = *reinterpret_cast<char (*)[32]>(0xB728EC);
 int32& CGame::currLevel = *reinterpret_cast<int32*>(0xB7290C);
