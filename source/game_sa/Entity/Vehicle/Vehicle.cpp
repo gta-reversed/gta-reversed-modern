ceb52c2560995340ad125c0af351fa7c6965c038
Izzotop <1680033+Izzotop@users.noreply.github.com>
2022-01-09 19:46:43 +0300

    Vehicle

--- source/game_sa/Entity/Vehicle/Vehicle.cpp
+++ source/game_sa/Entity/Vehicle/Vehicle.cpp
@@ -2760,9 +2760,10 @@ void CVehicle::DoDriveByShootings()
     plugin::CallMethod<0x741FD0, CVehicle*>(this);
 }
 
-bool CVehicle::IsAnyOfPassengersFollowerOfGroup(CPedGroup& grp) {
+// NOTSA
+bool CVehicle::IsAnyOfPassengersFollowerOfGroup(CPedGroup& group) {
     const auto end = m_apPassengers + m_nMaxPassengers;
     return std::find_if(m_apPassengers, end, [&](CPed* passenger) {
-        return grp.m_groupMembership.IsFollower(passenger);
+        return group.m_groupMembership.IsFollower(passenger);
     }) != end;
 }
