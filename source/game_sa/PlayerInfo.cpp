150c2461e85e4a47265a779101e1429a43a312ff
Pirulax <patrikjankovics7@gmail.com>
2022-01-12 19:31:32 +0100

    boilerplate

--- source/game_sa/PlayerInfo.cpp
+++ source/game_sa/PlayerInfo.cpp
@@ -1,14 +1,92 @@
 #include "StdInc.h"
+#include "PlayerInfo.h"
 
-void CPlayerInfo::InjectHooks()
-{
-    ReversibleHooks::Install("CPlayerInfo", "IsPlayerInRemoteMode", 0x56DAB0, &CPlayerInfo::IsPlayerInRemoteMode);
+void CPlayerInfo::InjectHooks() {
+    // Constructors (1x)
+    // ReversibleHooks::Install("CPlayerInfo", "CPlayerInfo", 0x571920, &CPlayerInfo::Constructor);
+
+    // Static functions (3x)
+    // ReversibleHooks::Install("CPlayerInfo", "CancelPlayerEnteringCars", 0x56E860, &CPlayerInfo::CancelPlayerEnteringCars);
+    // ReversibleHooks::Install("CPlayerInfo", "FindObjectToSteal", 0x56DBD0, &CPlayerInfo::FindObjectToSteal);
+    // ReversibleHooks::Install("CPlayerInfo", "EvaluateCarPosition", 0x56DAD0, &CPlayerInfo::EvaluateCarPosition);
+
+    // Methods (24x)
+    // ReversibleHooks::Install("CPlayerInfo", "destructor", 0x45B110, &CPlayerInfo::destructor);
+    // ReversibleHooks::Install("CPlayerInfo", "SetPlayerSkin", 0x5717F0, &CPlayerInfo::SetPlayerSkin);
+    // ReversibleHooks::Install("CPlayerInfo", "Process", 0x56F8D0, &CPlayerInfo::Process);
+    // ReversibleHooks::Install("CPlayerInfo", "LoadPlayerSkin", 0x56F7D0, &CPlayerInfo::LoadPlayerSkin);
+    // ReversibleHooks::Install("CPlayerInfo", "FindClosestCarSectorList", 0x56F4E0, &CPlayerInfo::FindClosestCarSectorList);
+    // ReversibleHooks::Install("CPlayerInfo", "Clear", 0x56F330, &CPlayerInfo::Clear);
+    // ReversibleHooks::Install("CPlayerInfo", "GivePlayerParachute", 0x56EC40, &CPlayerInfo::GivePlayerParachute);
+    // ReversibleHooks::Install("CPlayerInfo", "StreamParachuteWeapon", 0x56EB30, &CPlayerInfo::StreamParachuteWeapon);
+    // ReversibleHooks::Install("CPlayerInfo", "AddHealth", 0x56EAB0, &CPlayerInfo::AddHealth);
+    // ReversibleHooks::Install("CPlayerInfo", "DeletePlayerSkin", 0x56EA80, &CPlayerInfo::DeletePlayerSkin);
+    // ReversibleHooks::Install("CPlayerInfo", "BlowUpRCBuggy", 0x56EA30, &CPlayerInfo::BlowUpRCBuggy);
+    // ReversibleHooks::Install("CPlayerInfo", "MakePlayerSafe", 0x56E870, &CPlayerInfo::MakePlayerSafe);
+    // ReversibleHooks::Install("CPlayerInfo", "PlayerFailedCriticalMission", 0x56E830, &CPlayerInfo::PlayerFailedCriticalMission);
+    // ReversibleHooks::Install("CPlayerInfo", "WorkOutEnergyFromHunger", 0x56E610, &CPlayerInfo::WorkOutEnergyFromHunger);
+    // ReversibleHooks::Install("CPlayerInfo", "ArrestPlayer", 0x56E5D0, &CPlayerInfo::ArrestPlayer);
+    // ReversibleHooks::Install("CPlayerInfo", "KillPlayer", 0x56E580, &CPlayerInfo::KillPlayer);
+    // ReversibleHooks::Install("CPlayerInfo", "IsRestartingAfterMissionFailed", 0x56E570, &CPlayerInfo::IsRestartingAfterMissionFailed);
+    // ReversibleHooks::Install("CPlayerInfo", "IsRestartingAfterArrest", 0x56E560, &CPlayerInfo::IsRestartingAfterArrest);
+    // ReversibleHooks::Install("CPlayerInfo", "IsRestartingAfterDeath", 0x56E550, &CPlayerInfo::IsRestartingAfterDeath);
+    // ReversibleHooks::Install("CPlayerInfo", "GetPos", 0x56DFB0, &CPlayerInfo::GetPos);
+    // ReversibleHooks::Install("CPlayerInfo", "GetSpeed", 0x56DF50, &CPlayerInfo::GetSpeed);
+    // ReversibleHooks::Install("CPlayerInfo", "IsPlayerInRemoteMode", 0x56DAB0, &CPlayerInfo::IsPlayerInRemoteMode);
+    // ReversibleHooks::Install("CPlayerInfo", "SetLastTargetVehicle", 0x56DA80, &CPlayerInfo::SetLastTargetVehicle);
+    // ReversibleHooks::Install("CPlayerInfo", "Save", 0x5D3AC0, &CPlayerInfo::Save);
 }
 
-bool CPlayerInfo::IsPlayerInRemoteMode()
+// 0x571920
+CPlayerInfo::CPlayerInfo() {}
+
+CVector* CPlayerInfo::GetSpeed_Hook(CVector* out) {
+    *out = GetSpeed();
+    return out;
+}
+
+CVector* CPlayerInfo::GetPos_Hook(CVector* outPos)
 {
-    //plugin::CallMethod<0x56DAB0, CPlayerInfo*>(this);
-    return m_pRemoteVehicle != nullptr;
+    *outPos = GetPos();
+    return outPos;
+}
+
+// 0x571920
+CPlayerInfo* CPlayerInfo::Constructor() {
+    this->CPlayerInfo::CPlayerInfo();
+    return this;
+}
+
+// Static functions
+// 0x56E860
+void CPlayerInfo::CancelPlayerEnteringCars(CVehicle* a1) {
+    plugin::Call<0x56E860, CVehicle*>(a1);
+}
+
+// 0x56DBD0
+CEntity* CPlayerInfo::FindObjectToSteal(CPed* this_mpPed) {
+    return plugin::CallAndReturn<CEntity*, 0x56DBD0, CPed*>(this_mpPed);
+}
+
+// 0x56DAD0
+void CPlayerInfo::EvaluateCarPosition(CEntity* car, CPed* this_mpPed, float pedToVehDist, float* outDistance, CVehicle** outVehicle) {
+    plugin::Call<0x56DAD0, CEntity*, CPed*, float, float*, CVehicle**>(car, this_mpPed, pedToVehDist, outDistance, outVehicle);
+}
+
+// Methods
+// 0x45B110
+void CPlayerInfo::Destructor() {
+    plugin::CallMethod<0x45B110, CPlayerInfo*>(this);
+}
+
+// 0x5717F0
+void CPlayerInfo::SetPlayerSkin(char const* name) {
+    plugin::CallMethod<0x5717F0, CPlayerInfo*, char const*>(this, name);
+}
+
+// 0x56F8D0
+void CPlayerInfo::Process(uint32 playerIndex) {
+    plugin::CallMethod<0x56F8D0, CPlayerInfo*, uint32>(this, playerIndex);
 }
 
 // 0x56F7D0
@@ -16,11 +94,107 @@ void CPlayerInfo::LoadPlayerSkin() {
     plugin::CallMethod<0x56F7D0, CPlayerInfo*>(this);
 }
 
+// 0x56F4E0
+void CPlayerInfo::FindClosestCarSectorList(CPtrList& ptrList, CPed* this_mp_Ped, float minX, float minY, float maxX, float maxY, float* outVehDist, CVehicle** outVehicle) {
+    plugin::CallMethod<0x56F4E0, CPlayerInfo*, CPtrList&, CPed*, float, float, float, float, float*, CVehicle**>(this, ptrList, this_mp_Ped, minX, minY, maxX, maxY, outVehDist,
+                                                                                                                 outVehicle);
+}
+
 // 0x56F330
 void CPlayerInfo::Clear() {
     plugin::CallMethod<0x56F330, CPlayerInfo*>(this);
 }
 
+// 0x56EC40
+void CPlayerInfo::GivePlayerParachute() {
+    plugin::CallMethod<0x56EC40, CPlayerInfo*>(this);
+}
+
+// 0x56EB30
+void CPlayerInfo::StreamParachuteWeapon(bool a2) {
+    plugin::CallMethod<0x56EB30, CPlayerInfo*, bool>(this, a2);
+}
+
+// 0x56EAB0
+void CPlayerInfo::AddHealth(int32_t amount) {
+    plugin::CallMethod<0x56EAB0, CPlayerInfo*, int32_t>(this, amount);
+}
+
+// 0x56EA80
 void CPlayerInfo::DeletePlayerSkin() {
     plugin::CallMethod<0x56EA80, CPlayerInfo*>(this);
+}
+
+// 0x56EA30
+void CPlayerInfo::BlowUpRCBuggy(bool bExplode) {
+    plugin::CallMethod<0x56EA30, CPlayerInfo*, bool>(this, bExplode);
+}
+
+// 0x56E870
+void CPlayerInfo::MakePlayerSafe(bool canMove, float radius) {
+    plugin::CallMethod<0x56E870, CPlayerInfo*, bool>(this, canMove, radius);
+}
+
+// 0x56E830
+void CPlayerInfo::PlayerFailedCriticalMission() {
+    plugin::CallMethod<0x56E830, CPlayerInfo*>(this);
+}
+
+// 0x56E610
+void CPlayerInfo::WorkOutEnergyFromHunger() {
+    plugin::CallMethod<0x56E610, CPlayerInfo*>(this);
+}
+
+// 0x56E5D0
+void CPlayerInfo::ArrestPlayer() {
+    plugin::CallMethod<0x56E5D0, CPlayerInfo*>(this);
+}
+
+// 0x56E580
+void CPlayerInfo::KillPlayer() {
+    plugin::CallMethod<0x56E580, CPlayerInfo*>(this);
+}
+
+// 0x56E570
+bool CPlayerInfo::IsRestartingAfterMissionFailed() {
+    return plugin::CallMethodAndReturn<bool, 0x56E570, CPlayerInfo*>(this);
+}
+
+// 0x56E560
+bool CPlayerInfo::IsRestartingAfterArrest() {
+    return plugin::CallMethodAndReturn<bool, 0x56E560, CPlayerInfo*>(this);
+}
+
+// 0x56E550
+bool CPlayerInfo::IsRestartingAfterDeath() {
+    return plugin::CallMethodAndReturn<bool, 0x56E550, CPlayerInfo*>(this);
+}
+
+// 0x56DFB0
+CVector CPlayerInfo::GetPos() {
+    CVector result{};
+    plugin::CallMethodAndReturn<CVector*, 0x56DFB0, CPlayerInfo*, CVector*>(this, &result);
+    return result;
+}
+
+// 0x56DF50
+CVector CPlayerInfo::GetSpeed() {
+    CVector result{};
+    plugin::CallMethodAndReturn<CVector*, 0x56DF50, CPlayerInfo*, CVector*>(this, &result);
+    return result;
+}
+
+// 0x56DAB0
+bool CPlayerInfo::IsPlayerInRemoteMode() {
+    return plugin::CallMethodAndReturn<bool, 0x56DAB0, CPlayerInfo*>(this);
+}
+
+// 0x56DA80
+void CPlayerInfo::SetLastTargetVehicle(CVehicle* targetveh) {
+    plugin::CallMethod<0x56DA80, CPlayerInfo*, CVehicle*>(this, targetveh);
+}
+
+// 0x5D3AC0
+int8_t CPlayerInfo::Save() {
+    return plugin::CallMethodAndReturn<int8_t, 0x5D3AC0, CPlayerInfo*>(this);
 }
--- source/game_sa/PlayerInfo.h
+++ source/game_sa/PlayerInfo.h
@@ -21,82 +21,112 @@ enum ePlayerState : uint8 {
 
 class CPed;
 class CVehicle;
+struct RwTexture;
 
 class CPlayerInfo {
 public:
-    CPlayerPed*    m_pPed;                    // Pointer to the player ped (should always be set)
-    CPlayerPedData m_PlayerData;              // instance of player variables
-    CVehicle*      m_pRemoteVehicle;          // Pointer to vehicle player is driving remotely at the moment.(NULL if on foot)
-    CVehicle*      m_pSpecCar;                // which car is using the special collision model
-    int32          m_nMoney;                  // Points for this player
-    int32          m_nDisplayMoney;           // Points as they would be displayed
-    uint32         m_nCollectablesPickedUp;   // How many bags of sugar do we have
-    uint32         m_nTotalNumCollectables;   // How many bags of sugar are there to be had in the game
-    uint32         m_nLastBumpPlayerCarTimer; // Keeps track of when the last ped bumped into the player car
-    uint32         m_nTaxiTimer;              // Keeps track of how long the player has been in a taxi with a passenger (in msecs)
-    uint32         m_nVehicleTimeCounter;     // keeps track of how long player has been in car for driving skill
-    bool           m_bTaxiTimerScore;         // If TRUE then add 1 to score for each second that the player is driving a taxi
-    bool           m_bTryingToExitCar;        // if player holds exit car button, want to trigger getout once car slowed enough with a passenger
-    char           _pad0[2];
-    CVehicle*      m_pLastTargetVehicle; // Last vehicle player tried to enter.
-    ePlayerState   m_nPlayerState;
-    bool           m_bAfterRemoteVehicleExplosion;
-    bool           m_bCreateRemoteVehicleExplosion;
-    bool           m_bFadeAfterRemoteVehicleExplosion;
-    uint32         m_nTimeOfRemoteVehicleExplosion;
-    uint32         m_nLastTimeEnergyLost; // To make numbers flash on the HUD;
-    uint32         m_nLastTimeArmourLost;
-    uint32         m_nLastTimeBigGunFired;   // Tank guns etc
-    uint32         m_nTimesUpsideDownInARow; // Make car blow up if car upside down
-    uint32         m_nTimesStuckInARow;      // Make car blow up if player cannot get out.
-    uint32         m_nCarTwoWheelCounter;    // how long has player's car been on two wheels
-    float          m_fCarTwoWheelDist;       // Make car blow up if player cannot get out.
-    uint32       m_nCarLess3WheelCounter;  // how long has player's car been on less than 3 wheels
-    uint32         m_nBikeRearWheelCounter;  // how long has player's bike been on rear wheel only
-    float          m_fBikeRearWheelDist;
-    uint32         m_nBikeFrontWheelCounter; // how long has player's bike been on front wheel only
-    float          m_fBikeFrontWheelDist;
-    uint32         m_nTempBufferCounter; // so wheels can leave the ground for a few frames without stopping above counters
-    uint32         m_nBestCarTwoWheelsTimeMs;
-    float          m_fBestCarTwoWheelsDistM;
-    uint32         m_nBestBikeWheelieTimeMs;
-    float          m_fBestBikeWheelieDistM;
-    uint32         m_nBestBikeStoppieTimeMs;
-    float          m_fBestBikeStoppieDistM;
-    uint16         m_nCarDensityForCurrentZone;
-    char           _pad1[2];
-    float          m_fRoadDensityAroundPlayer; // 1.0f for an average city.
-    uint32         m_nTimeOfLastCarExplosionCaused;
-    uint32         m_nExplosionMultiplier;
-    uint32         m_nHavocCaused; // A counter going up when the player does bad stuff.
-    uint16         m_nNumHoursDidntEat;
-    char           _pad2[2];
-    float          m_fCurrentChaseValue; // How 'ill' is the chase at the moment
-    bool           m_bDoesNotGetTired;
-    bool           m_bFastReload;
-    bool           m_bFireProof;
-    uint8          m_nMaxHealth;
-    uint8          m_nMaxArmour;
-    bool           m_bGetOutOfJailFree; // Player doesn't lose money/weapons next time arrested
-    bool           m_bFreeHealthCare;   // Player doesn't lose money nexed time patched up at hospital
-    bool           m_bCanDoDriveBy;
-    uint8          m_nBustedAudioStatus;
-    char           _pad3;
-    uint16         m_nLastBustMessageNumber;
-    uint32         m_nCrosshairActivated;
-    CVector2D      m_vecCrosshairTarget; // -1 ... 1 on screen
-    char           m_szSkinName[32];
-    RwTexture*     m_pSkinTexture;
-    bool           m_bParachuteReferenced;
-    char           _pad4[3];
-    uint32         m_nRequireParachuteTimer;
+    CPlayerPed*    m_pPed{};                    // Pointer to the player ped (should always be set)
+    CPlayerPedData m_PlayerData{};              // instance of player variables
+    CVehicle*      m_pRemoteVehicle{};          // Pointer to vehicle player is driving remotely at the moment.(NULL if on foot)
+    CVehicle*      m_pSpecCar{};                // which car is using the special collision model
+    int32          m_nMoney{};                  // Points for this player
+    int32          m_nDisplayMoney{};           // Points as they would be displayed
+    uint32         m_nCollectablesPickedUp{};   // How many bags of sugar do we have
+    uint32         m_nTotalNumCollectables{};   // How many bags of sugar are there to be had in the game
+    uint32         m_nLastBumpPlayerCarTimer{}; // Keeps track of when the last ped bumped into the player car
+    uint32         m_nTaxiTimer{};              // Keeps track of how long the player has been in a taxi with a passenger (in msecs)
+    uint32         m_nVehicleTimeCounter{};     // keeps track of how long player has been in car for driving skill
+    bool           m_bTaxiTimerScore{};         // If TRUE then add 1 to score for each second that the player is driving a taxi
+    bool           m_bTryingToExitCar{};        // if player holds exit car button, want to trigger getout once car slowed enough with a passenger
+    CVehicle*      m_pLastTargetVehicle{};      // Last vehicle player tried to enter.
+    ePlayerState   m_nPlayerState{};
+    bool           m_bAfterRemoteVehicleExplosion{};
+    bool           m_bCreateRemoteVehicleExplosion{};
+    bool           m_bFadeAfterRemoteVehicleExplosion{};
+    uint32         m_nTimeOfRemoteVehicleExplosion{};
+    uint32         m_nLastTimeEnergyLost{};     // To make numbers flash on the HUD
+    uint32         m_nLastTimeArmourLost{};
+    uint32         m_nLastTimeBigGunFired{};   // Tank guns etc
+    uint32         m_nTimesUpsideDownInARow{}; // Make car blow up if car upside down
+    uint32         m_nTimesStuckInARow{};      // Make car blow up if player cannot get out.
+    uint32         m_nCarTwoWheelCounter{};    // how long has player's car been on two wheels
+    float          m_fCarTwoWheelDist{};       // Make car blow up if player cannot get out.
+    uint32         m_nCarLess3WheelCounter{};  // how long has player's car been on less than 3 wheels
+    uint32         m_nBikeRearWheelCounter{};  // how long has player's bike been on rear wheel only
+    float          m_fBikeRearWheelDist{};
+    uint32         m_nBikeFrontWheelCounter{}; // how long has player's bike been on front wheel only
+    float          m_fBikeFrontWheelDist{};
+    uint32         m_nTempBufferCounter{};     // so wheels can leave the ground for a few frames without stopping above counters
+    uint32         m_nBestCarTwoWheelsTimeMs{};
+    float          m_fBestCarTwoWheelsDistM{};
+    uint32         m_nBestBikeWheelieTimeMs{};
+    float          m_fBestBikeWheelieDistM{};
+    uint32         m_nBestBikeStoppieTimeMs{};
+    float          m_fBestBikeStoppieDistM{};
+    uint16         m_nCarDensityForCurrentZone{};
+    float          m_fRoadDensityAroundPlayer{}; // 1.0f for an average city.
+    uint32         m_nTimeOfLastCarExplosionCaused{};
+    uint32         m_nExplosionMultiplier{};
+    uint32         m_nHavocCaused{}; // A counter going up when the player does bad stuff.
+    uint16         m_nNumHoursDidntEat{};
+    float          m_fCurrentChaseValue{}; // How 'ill' is the chase at the moment
+    bool           m_bDoesNotGetTired{};
+    bool           m_bFastReload{};
+    bool           m_bFireProof{};
+    uint8          m_nMaxHealth{};
+    uint8          m_nMaxArmour{};
+    bool           m_bGetOutOfJailFree{}; // Player doesn't lose money/weapons next time arrested
+    bool           m_bFreeHealthCare{};   // Player doesn't lose money nexed time patched up at hospital
+    bool           m_bCanDoDriveBy{};
+    uint8          m_nBustedAudioStatus{};
+    uint16         m_nLastBustMessageNumber{};
+    uint32         m_nCrosshairActivated{};
+    CVector2D      m_vecCrosshairTarget{}; // -1 ... 1 on screen
+    char           m_szSkinName[32]{};
+    RwTexture*     m_pSkinTexture{};
+    bool           m_bParachuteReferenced{};
+    uint32         m_nRequireParachuteTimer{};
 
 public:
     static void InjectHooks();
-    bool IsPlayerInRemoteMode();
-    void DeletePlayerSkin();
+
+    CPlayerInfo();
+
+    static void CancelPlayerEnteringCars(CVehicle* a1);
+    static CEntity* FindObjectToSteal(CPed* this_mpPed);
+    static void EvaluateCarPosition(CEntity* car, CPed* this_mpPed, float pedToVehDist, float* outDistance, CVehicle** outVehicle);
+
+    void Destructor();
+    void SetPlayerSkin(char const* name);
+    void Process(uint32 playerIndex);
     void LoadPlayerSkin();
+    void FindClosestCarSectorList(CPtrList& ptrList, CPed* this_mp_Ped, float minX, float minY, float maxX, float maxY, float* outVehDist, CVehicle** outVehicle);
     void Clear();
+    void GivePlayerParachute();
+    void StreamParachuteWeapon(bool a2);
+    void AddHealth(int32_t amount);
+    void DeletePlayerSkin();
+    void BlowUpRCBuggy(bool bExplode);
+    void MakePlayerSafe(bool canMove, float radius);
+    void PlayerFailedCriticalMission();
+    void WorkOutEnergyFromHunger();
+    void ArrestPlayer();
+    void KillPlayer();
+    bool IsRestartingAfterMissionFailed();
+    bool IsRestartingAfterArrest();
+    bool IsRestartingAfterDeath();
+    CVector GetPos();
+    CVector GetSpeed();
+    bool IsPlayerInRemoteMode();
+    void SetLastTargetVehicle(CVehicle* targetveh);
+    int8_t Save();
+
+private:
+    CVector* GetSpeed_Hook(CVector* out);
+    CVector* GetPos_Hook(CVector* outPos);
+
+
+    CPlayerInfo* Constructor();
 };
 
 VALIDATE_SIZE(CPlayerInfo, 0x190);
