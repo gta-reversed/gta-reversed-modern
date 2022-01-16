2df1798859fbd58a4ada184a93a3de0a5a5f377b
Pirulax <patrikjankovics7@gmail.com>
2022-01-16 13:25:58 +0100

    Fix LoadLevel

--- source/game_sa/FileLoader.cpp
+++ source/game_sa/FileLoader.cpp
@@ -744,7 +744,7 @@ void CFileLoader::LoadLevel(const char* levelFileName) {
 
         // Extract path after identifier like: <ID> <PATH>
         const auto ExtractPathFor = [&](auto id) {
-            assert(LineBeginsWith(weaponType)); // NOTSA - Function should only be called if that's the case. 
+            assert(LineBeginsWith(id)); // NOTSA - Function should only be called if that's the case. 
             return l + strlen(id) + 1;
         };
 
