cc99a9490bebfa33314063d29b384651159eddac
yukani <yukani@zohomail.eu>
2022-01-19 22:13:56 +0300

    check for empty scriptdir path

--- contrib/link_asi.bat
+++ contrib/link_asi.bat
@@ -3,7 +3,10 @@ set pwd=%~dp0
 set scriptdir=%1
 set scriptname=%2
 
+if "%scriptdir%"=="" goto :eof
 if "%scriptname%"=="" set scriptname="gta_reversed"
 
 mklink %scriptdir%\%scriptname%.asi "%pwd%\..\bin\debug\gta_reversed.asi"
 mklink %scriptdir%\%scriptname%.pdb "%pwd%\..\bin\debug\gta_reversed.pdb"
+
+eof:
