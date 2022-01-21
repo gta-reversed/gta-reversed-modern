e4c7421acc6c382da1350d3aaf4cf038b81d2275
Izzotop <1680033+Izzotop@users.noreply.github.com>
2022-01-21 06:53:47 +0300

    Update CMissionTextOffsets::Load
    Co-authored-by: Pirulax <patrikjankovics7@gmail.com>

--- source/game_sa/Text/MissionTextOffsets.cpp
+++ source/game_sa/Text/MissionTextOffsets.cpp
@@ -11,19 +11,16 @@ bool CMissionTextOffsets::Load(uint32 length, FILESTREAM file, uint32* offset, i
     tMissionTextOffset* pTextOffset = m_offsets;
     m_size = 0;
 
-    while (true) {
+    do {
         if (sizeof(tMissionTextOffset) != CFileMgr::Read(file, pTextOffset, sizeof(tMissionTextOffset))) {
             return false;
         }
+        
         *offset += sizeof(tMissionTextOffset);
 
         ++pTextOffset;
         ++m_size;
-
-        if (uint32(pTextOffset - m_offsets) >= length / sizeof(tMissionTextOffset)) {
-            break;
-        }
-    }
+    } while ((uint32)(pTextOffset - m_offsets) >= length / sizeof(tMissionTextOffset));
 
     return true;
 
