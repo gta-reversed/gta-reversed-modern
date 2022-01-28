a0a204a2a6d8d80f761150ae27cd989e5a1ed001
Pirulax <patrikjankovics7@gmail.com>
2022-01-28 19:57:26 +0100

    Fix build error...

--- source/reversiblehooks/RootHookCategory.h
+++ source/reversiblehooks/RootHookCategory.h
@@ -23,9 +23,24 @@ public:
         assert(!category.empty()); // Should never be empty. To add to global category use `RH_ScopedCategoryGlobal()`
 
         HookCategory* cat = this; // Start with us (the root category)
-        for (const auto& catName : category | rng::views::split('/')) {
-            cat = &cat->FindOrCreateSubcategory(std::string_view(catName.begin(), catName.end())); // Find/create all sub-categories - TODO: C++23 - Use string_view range constructor here
+
+        for (auto it = category.begin();;) {
+            const auto next = std::find(it, category.end(), '/');
+            const auto off = (size_t)(it - category.begin());
+            if (next == category.end()) {
+                cat = &cat->FindOrCreateSubcategory(category.substr(off));
+                break;
+            } else {
+                cat = &cat->FindOrCreateSubcategory(category.substr(off, (size_t)(next - category.begin()) - off));
+                it = next + 1; // `+ 1` to skip separator
+            }
         }
+
+        //  TODO: Use this instead, once shit CI is fixed.
+        //for (const auto& catName : category | rng::views::split('/')) {
+        //    cat = &cat->FindOrCreateSubcategory(catName); // Find/create all sub-categories - TODO: C++23 - Use string_view range constructor here
+        //}
+
         assert(cat != this); // Make sure item doesn't get added into us (As the root category should have no items)
         cat->AddItem(std::move(hook)); // The last category is where we add the item to
     }
