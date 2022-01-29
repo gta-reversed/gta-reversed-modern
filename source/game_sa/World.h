d6a18f6b06f7be220d72894a73661e592cf02717
Pirulax <patrikjankovics7@gmail.com>
2022-01-29 13:32:16 +0100

    IsInWorldBounds: change >< to  >= <=

--- source/game_sa/World.h
+++ source/game_sa/World.h
@@ -201,8 +201,8 @@ public:
         return (sector - HalfOfTotalSectorsY) * fTotalMapUnitsY + (fTotalMapUnitsY / 2);
     }
     static bool IsInWorldBounds(CVector2D pos) { // NOTSA
-        return pos.x > -3000.0f && pos.x < 3000.0f
-            && pos.y > -3000.0f && pos.y < 3000.0f;
+        return pos.x >= -3000.0f && pos.x <= 3000.0f
+            && pos.y >= -3000.0f && pos.y <= 3000.0f;
     }
 };
 
