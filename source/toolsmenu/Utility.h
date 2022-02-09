bdd5e8b3e33901593c145f67e5f2bf24f4c447e3
Pirulax <patrikjankovics7@gmail.com>
2022-02-09 21:36:27 +0100

    Add and use `SplitStringView`

--- source/toolsmenu/Utility.h
+++ source/toolsmenu/Utility.h
@@ -6,3 +6,14 @@
 static bool findStringCaseInsensitive(std::string_view haystack, std::string_view needle) {
     return !std::ranges::search(haystack, needle, {}, std::toupper, std::toupper).empty();
 }
+
+// https://stackoverflow.com/a/48403210
+// Maybe get rid of this in C++23 :D
+static auto SplitStringView(std::string_view str, std::string_view delim) {
+    return str
+        | std::ranges::views::split(delim)
+        | std::ranges::views::transform([](auto&& rng) {
+            return std::string_view(&*rng.begin(), std::ranges::distance(rng));
+        });
+}
+
