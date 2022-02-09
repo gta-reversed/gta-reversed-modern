2f59422a2702b34c9417a21ec4dbede85bbae351
Pirulax <patrikjankovics7@gmail.com>
2022-02-09 18:24:41 +0100

    Re-factor state change handling code to be more efficient

--- source/reversiblehooks/HookCategory.h
+++ source/reversiblehooks/HookCategory.h
@@ -49,11 +49,11 @@ public:
         assert(!FindItem(item->Name())); // Make sure there are no duplicate names :D
 
         // Lexographically sorted insert 
-        m_items.emplace(
+        const auto& emplacedItem = *m_items.emplace(
             rng::upper_bound(m_items, item->Name(), {}, [](auto&& v) { return v->Name(); }),
             std::move(item)
         );
-        OnOneItemStateChange(); // Deal with possible state change introduced by item
+        OnOneItemStateChange(emplacedItem); // Deal with possible state change introduced by item
     }
 
     // Enable/disable all items at once (subcategories' items included)
@@ -75,7 +75,7 @@ public:
     void SetItemEnabled(Item& item, bool enabled) {
         if (enabled != item->Hooked()) {
             item->State(enabled);
-            OnOneItemStateChange();
+            OnOneItemStateChange(item);
         }
     }
 
@@ -103,7 +103,7 @@ public:
     // NOTE: Make sure the function doesn't add/remove items/subcategories!
     //       (Underlaying storages are vectors, which don't like being modifies while they're being iterated over)
     template<typename Fn>
-    void ForEachItem(Fn fn) {
+    void ForEachItem(Fn&& fn) {
         for (auto& item : m_items) {
             std::invoke(fn, item);
         }
@@ -163,34 +163,68 @@ private:
         return changed;
     }
 
+    // Helper function to recaulcate new state of collection when one item's state changes
+    template<rng::input_range R, typename Fn>
+    static HooksState CalculateCollectionOverallStateOnEntryChange(R&& collection, HooksState currentCollectiveState, HooksState itemState, Fn&& isHookedPredicate) {
+        assert(!collection.empty()); // Logically impossible for the collection to be empty (It has to contain at least the entry whose state just changed)
+
+        if (itemState == currentCollectiveState) {
+            return currentCollectiveState; // No changes
+
+        }
+
+        // The collection consists of this entry alone,
+        // so overall state is decided by it alone
+        if (collection.size() == 1) {
+            return itemState;
+        }
+
+        // Multiple entries at this point
+        switch (currentCollectiveState) {
+        case HooksState::ALL:
+        case HooksState::NONE: {
+            return HooksState::SOME; // At this point the state of the collection and item differ
+        }
+        case HooksState::SOME: { // Has to calculate it all again - TODO: Check if this is slow - If so, it can be optimized further
+            const auto hookedCount = rng::count_if(collection, isHookedPredicate);
+            if (hookedCount == 0) {
+                return HooksState::NONE;
+            } else if (hookedCount == collection.size()) {
+                return HooksState::ALL;
+            } else {
+                return HooksState::SOME;
+            }
+        }
+        }
+
+        assert(0);
+        return HooksState::SOME; // Theoritically unreachable - Had to put it here to avoid compiler warnings :D
+    }
+
     // Called when a sub-category's overall state changes
     // (Will propagate to parent if it affected this category's overall state)
     void OnSubcategoryStateChanged(HookCategory& cat) {
-        using enum HooksState;
-
-        const auto fullyHookedCount = rng::count_if(m_subCategories, [](const auto& v) -> bool { return v.OverallState() == HooksState::ALL; });
-        m_subcatsState =
-            fullyHookedCount == 0                      ? NONE :
-            fullyHookedCount == m_subCategories.size() ? ALL  :
-                                                         SOME;
+        m_subcatsState = CalculateCollectionOverallStateOnEntryChange(
+            m_subCategories,
+            m_subcatsState,
+            cat.OverallState(),
+            [](const auto& v) { return v.OverallState() == HooksState::ALL; }
+        );
         ReCalculateOverallStateAndMaybeNotify();
     }
 
     // Not always called - Only when an individual item's state changes
     // Also not called if item is modified from the outside
     // (Currently only called by `AddItem` and `SetItemEnabled`, but not from `SetAllItemsEnabled`)
-    void OnOneItemStateChange() {
-        using enum HooksState;
-
-        const auto hookedCount = rng::count_if(m_items, [](const auto& v) -> bool { return v->Hooked(); });
-        m_itemsState =
-            hookedCount == 0              ? HooksState::NONE :
-            hookedCount == m_items.size() ? HooksState::ALL :
-                                            HooksState::SOME;
-
+    void OnOneItemStateChange(const Item& item) {
+        m_itemsState = CalculateCollectionOverallStateOnEntryChange(
+            m_items,
+            m_itemsState,
+            item->Hooked() ? HooksState::ALL : HooksState::NONE,
+            [](const auto& v) { return v->Hooked(); }
+        );
         ReCalculateOverallStateAndMaybeNotify();
     }
-
 public:
     // Stuff required for the Hooks tool
     HooksState                m_itemsState{ HooksState::ALL };    // Collective state of all items (Can be ignored if `m_items.empty()` (In this case it's always NONE))
