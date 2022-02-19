e0141b3a747c6cc535fb40880f3c7115888bc21e
Izzotop <1680033+Izzotop@users.noreply.github.com>
2022-02-20 01:36:05 +0300

    Update

--- source/game_sa/Tasks/TaskTypes/TaskComplexFollowLeaderInFormation.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexFollowLeaderInFormation.cpp
@@ -5,42 +5,24 @@ void CTaskComplexFollowLeaderInFormation::InjectHooks() {
     RH_ScopedClass(CTaskComplexFollowLeaderInFormation);
     RH_ScopedCategory("Tasks/TaskTypes");
 
-    // Constructors (1x)
-    RH_ScopedInstall(Constructor, 0x6949A0);
+    // RH_ScopedInstall(Constructor, 0x6949A0);
+    // RH_ScopedOverloadedInstall(Destructor, "", 0x694A40, CTaskComplexFollowLeaderInFormation * (CTaskComplexFollowLeaderInFormation::*)());
 
-    // Destructors (1x)
-    RH_ScopedOverloadedInstall(Destructor, "", 0x694A40, CTaskComplexFollowLeaderInFormation * (CTaskComplexFollowLeaderInFormation::*)());
-
-    // Virtual methods (5x)
-    RH_ScopedInstall(Clone_Reversed, 0x695740);
-    RH_ScopedInstall(GetTaskType_Reversed, 0x694A30);
-    RH_ScopedInstall(CreateNextSubTask_Reversed, 0x696820);
-    RH_ScopedInstall(CreateFirstSubTask_Reversed, 0x6968E0);
-    RH_ScopedInstall(ControlSubTask_Reversed, 0x696940);
+    // RH_ScopedInstall(Clone_Reversed, 0x695740);
+    // RH_ScopedInstall(GetTaskType_Reversed, 0x694A30);
+    // RH_ScopedInstall(CreateNextSubTask_Reversed, 0x696820);
+    // RH_ScopedInstall(CreateFirstSubTask_Reversed, 0x6968E0);
+    // RH_ScopedInstall(ControlSubTask_Reversed, 0x696940);
 }
 
 // 0x6949A0
-CTaskComplexFollowLeaderInFormation::CTaskComplexFollowLeaderInFormation(CPedGroup* pedGroup, CPed* ped, CVector const& posn, float a5) {}
-
-// 0x6949A0
-CTaskComplexFollowLeaderInFormation* CTaskComplexFollowLeaderInFormation::Constructor(CPedGroup* pedGroup, CPed* ped, CVector const& posn, float a5) {
-    this->CTaskComplexFollowLeaderInFormation::CTaskComplexFollowLeaderInFormation(pedGroup, ped, posn, a5);
-    return this;
+CTaskComplexFollowLeaderInFormation::CTaskComplexFollowLeaderInFormation(CPedGroup* pedGroup, CPed* ped, const CVector& posn, float a5) : CTaskComplex() {
+    plugin::CallMethod<0x6949A0, CTaskComplexFollowLeaderInFormation*, CPedGroup*, CPed*, const CVector&, float>(this, pedGroup, ped, posn, a5);
 }
 
 // 0x694A40
-CTaskComplexFollowLeaderInFormation::~CTaskComplexFollowLeaderInFormation() {}
-
-// 0x694A40
-CTaskComplexFollowLeaderInFormation* CTaskComplexFollowLeaderInFormation::Destructor() {
-    this->CTaskComplexFollowLeaderInFormation::~CTaskComplexFollowLeaderInFormation();
-    return this;
-}
-
-// Virtual methods
-// 0x695740
-CTask* CTaskComplexFollowLeaderInFormation::Clone() {
-    return plugin::CallMethodAndReturn<CTask*, 0x695740, CTaskComplexFollowLeaderInFormation*>(this);
+CTaskComplexFollowLeaderInFormation::~CTaskComplexFollowLeaderInFormation() {
+    plugin::CallMethod<0x694A40, CTaskComplexFollowLeaderInFormation*>(this);
 }
 
 // 0x696820
--- source/game_sa/Tasks/TaskTypes/TaskComplexFollowLeaderInFormation.h
+++ source/game_sa/Tasks/TaskTypes/TaskComplexFollowLeaderInFormation.h
@@ -12,25 +12,28 @@ class CTaskComplexFollowLeaderInFormation : public CTaskComplex {
     CVector    m_pos;       // 0x14
     uint32     m_int;       // 0x20
     float      m_dist;      // 0x24 - Max distance between m_pos and some point, not sure. See `ControlSubTask`. Ignored if < 0
+
 public:
+    CTaskComplexFollowLeaderInFormation(CPedGroup* pedGroup, CPed* ped, const CVector& posn, float dist);
+    ~CTaskComplexFollowLeaderInFormation() override;
+
+    eTaskType GetTaskType() override { return TASK_COMPLEX_FOLLOW_LEADER_IN_FORMATION; }
+    CTask*    Clone() override { return plugin::CallMethodAndReturn<CTask*, 0x695740, CTaskComplexFollowLeaderInFormation*>(this); } // 0x695740
+    CTask*    CreateNextSubTask(CPed* ped) override;
+    CTask*    CreateFirstSubTask(CPed* ped) override;
+    CTask*    ControlSubTask(CPed* ped) override;
+
+private:
+    friend void InjectHooksMain();
     static void InjectHooks();
 
-    ~CTaskComplexFollowLeaderInFormation();
-    CTaskComplexFollowLeaderInFormation(CPedGroup * pedGroup, CPed * ped, CVector const& posn, float dist); 
-
-    CTask*    Clone() override;
-    eTaskType GetTaskType() override { return eTaskType::TASK_COMPLEX_FOLLOW_LEADER_IN_FORMATION; }
-    CTask*    CreateNextSubTask(CPed * ped) override;
-    CTask*    CreateFirstSubTask(CPed * ped) override;
-    CTask*    ControlSubTask(CPed * ped) override;
-private:
-    CTaskComplexFollowLeaderInFormation* Constructor(CPedGroup * pedGroup, CPed * ped, CVector const& posn, float a5);
-    CTaskComplexFollowLeaderInFormation* Destructor();
+    CTaskComplexFollowLeaderInFormation* Constructor(CPedGroup* pedGroup, CPed* ped, CVector const& posn, float a5) { return this; }
+    CTaskComplexFollowLeaderInFormation* Destructor() { this->CTaskComplexFollowLeaderInFormation::~CTaskComplexFollowLeaderInFormation(); return this; }
 
     CTask*    Clone_Reversed() { return CTaskComplexFollowLeaderInFormation::Clone(); }
     eTaskType GetTaskType_Reversed() { return CTaskComplexFollowLeaderInFormation::GetTaskType(); }
-    CTask*    CreateNextSubTask_Reversed(CPed * ped) { return CTaskComplexFollowLeaderInFormation::CreateNextSubTask(ped); }
-    CTask*    CreateFirstSubTask_Reversed(CPed * ped) { return CTaskComplexFollowLeaderInFormation::CreateFirstSubTask(ped); }
-    CTask*    ControlSubTask_Reversed(CPed * ped) { return CTaskComplexFollowLeaderInFormation::ControlSubTask(ped); }
+    CTask*    CreateNextSubTask_Reversed(CPed* ped) { return CTaskComplexFollowLeaderInFormation::CreateNextSubTask(ped); }
+    CTask*    CreateFirstSubTask_Reversed(CPed* ped) { return CTaskComplexFollowLeaderInFormation::CreateFirstSubTask(ped); }
+    CTask*    ControlSubTask_Reversed(CPed* ped) { return CTaskComplexFollowLeaderInFormation::ControlSubTask(ped); }
 };
 VALIDATE_SIZE(CTaskComplexFollowLeaderInFormation, 0x28);
