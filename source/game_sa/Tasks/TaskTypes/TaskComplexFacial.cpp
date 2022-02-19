c6a0de489b51eb44285f30419b140c23d541e60a
Pirulax <patrikjankovics7@gmail.com>
2022-02-19 19:58:46 +0100

    CTaskComplexFacial stub

--- source/game_sa/Tasks/TaskTypes/TaskComplexFacial.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexFacial.cpp
@@ -1,3 +1,74 @@
 #include "StdInc.h"
-
 #include "TaskComplexFacial.h"
+
+void CTaskComplexFacial::InjectHooks() {
+    RH_ScopedClass(CTaskComplexFacial);
+    RH_ScopedCategory("Task/TaskTypes"); // TODO: Change this to the appropriate category!
+
+    // Constructors (1x)
+    // RH_ScopedInstall(Constructor, 0x690D20);
+
+    // Destructors (1x)
+    // RH_ScopedOverloadedInstall(Destructor, "", 0x690D90, CTaskComplexFacial * (CTaskComplexFacial::*)());
+
+    // Methods (1x)
+    // RH_ScopedInstall(StopAll, 0x691250);
+
+    // Virtual methods (6x)
+    // RH_ScopedInstall(Clone_Reversed, 0x6928B0);
+    // RH_ScopedInstall(GetTaskType_Reversed, 0x690D80);
+    // RH_ScopedInstall(MakeAbortable_Reversed, 0x690DA0);
+    // RH_ScopedInstall(CreateNextSubTask_Reversed, 0x690DC0);
+    // RH_ScopedInstall(CreateFirstSubTask_Reversed, 0x690F30);
+    // RH_ScopedInstall(ControlSubTask_Reversed, 0x690FC0);
+}
+
+// 0x690D20
+CTaskComplexFacial::CTaskComplexFacial() {}
+
+// 0x690D20
+CTaskComplexFacial* CTaskComplexFacial::Constructor() {
+    this->CTaskComplexFacial::CTaskComplexFacial();
+    return this;
+}
+
+// 0x690D90
+CTaskComplexFacial::~CTaskComplexFacial() {}
+
+// 0x690D90
+CTaskComplexFacial* CTaskComplexFacial::Destructor() {
+    this->CTaskComplexFacial::~CTaskComplexFacial();
+    return this;
+}
+
+// Methods
+// 0x691250
+void CTaskComplexFacial::StopAll() {
+    plugin::CallMethod<0x691250, CTaskComplexFacial*>(this);
+}
+
+// Virtual methods
+// 0x6928B0
+CTask* CTaskComplexFacial::Clone() {
+    return plugin::CallMethodAndReturn<CTask*, 0x6928B0, CTaskComplexFacial*>(this);
+}
+
+// 0x690DA0
+bool CTaskComplexFacial::MakeAbortable(CPed* ped, eAbortPriority priority, CEvent const* event) {
+    return plugin::CallMethodAndReturn<bool, 0x690DA0, CTaskComplexFacial*, CPed*, int32, CEvent const*>(this, ped, priority, event);
+}
+
+// 0x690DC0
+CTask* CTaskComplexFacial::CreateNextSubTask(CPed* ped) {
+    return plugin::CallMethodAndReturn<CTask*, 0x690DC0, CTaskComplexFacial*, CPed*>(this, ped);
+}
+
+// 0x690F30
+CTask* CTaskComplexFacial::CreateFirstSubTask(CPed* ped) {
+    return plugin::CallMethodAndReturn<CTask*, 0x690F30, CTaskComplexFacial*, CPed*>(this, ped);
+}
+
+// 0x690FC0
+CTask* CTaskComplexFacial::ControlSubTask(CPed* ped) {
+    return plugin::CallMethodAndReturn<CTask*, 0x690FC0, CTaskComplexFacial*, CPed*>(this, ped);
+}
--- source/game_sa/Tasks/TaskTypes/TaskComplexFacial.h
+++ source/game_sa/Tasks/TaskTypes/TaskComplexFacial.h
@@ -1,26 +1,48 @@
-/*
-    Plugin-SDK file
-    Authors: GTA Community. See more here
-    https://github.com/DK22Pac/plugin-sdk
-    Do not delete this comment block. Respect others' work!
-*/
 #pragma once
 
 #include "TaskComplex.h"
 #include "TaskSimpleFacial.h"
+
+class CPed;
 
 class CTaskComplexFacial : public CTaskComplex {
 public:
     bool              m_bNotPlayer;
     bool              m_bStarted;
     bool              m_bIsAborting;
+
+    // TODO: Should probably make a struct of this then an array of it.
     eFacialExpression m_nFacialExpression1;
     int32             m_nDuration1;
+
     eFacialExpression m_nFacialExpression2;
     int32             m_nDuration2;
+    
+public:
+    static void InjectHooks();
 
+    ~CTaskComplexFacial() override;
     CTaskComplexFacial();
+
+    void StopAll();
+
+    CTask* Clone() override;
+    eTaskType GetTaskType() override { return TASK_COMPLEX_FACIAL; }
+    bool MakeAbortable(CPed* ped, eAbortPriority priority, CEvent const* event) override;
+    CTask* CreateNextSubTask(CPed* ped) override;
+    CTask* CreateFirstSubTask(CPed* ped) override;
+    CTask* ControlSubTask(CPed* ped) override;
+
+private:
+    CTaskComplexFacial* Constructor();
+    CTaskComplexFacial* Destructor();
+
+    // Virtual wrappers for hooks
+    CTask* Clone_Reversed() { return CTaskComplexFacial::Clone(); }
+    int32  GetTaskType_Reversed() { return CTaskComplexFacial::GetTaskType(); }
+    bool   MakeAbortable_Reversed(CPed* ped, eAbortPriority priority, CEvent const* event) { return CTaskComplexFacial::MakeAbortable(ped, priority, event); }
+    CTask* CreateNextSubTask_Reversed(CPed* ped) { return CTaskComplexFacial::CreateNextSubTask(ped); }
+    CTask* CreateFirstSubTask_Reversed(CPed* ped) { return CTaskComplexFacial::CreateFirstSubTask(ped); }
+    CTask* ControlSubTask_Reversed(CPed* ped) { return CTaskComplexFacial::ControlSubTask(ped); }
 };
-
 VALIDATE_SIZE(CTaskComplexFacial, 0x20);
-
