05dc5ac7ff1185bf5ca3fd0a2a42bb27516da7de
Pirulax <patrikjankovics7@gmail.com>
2022-02-20 01:20:33 +0100

    fix bugs

--- source/extensions/random.hpp
+++ source/extensions/random.hpp
@@ -6,12 +6,16 @@
 namespace notsa {
 namespace rng = std::ranges;
 
-// Returns random iterator from range
+// Returns random iterator from range - If empty the end iterator is returned.
 template<rng::forward_range R>
 auto random_iter(R&& range) {
+    if (rng::empty(range)) { // If empty just return end iterator
+        return rng::end(range);
+    }
+
     static std::default_random_engine reng{ std::random_device{}() };
-    std::uniform_int_distribution<size_t> unidist{ 0, rng::size(range) };
-    return std::next(range.begin(), unidist(reng));
+    std::uniform_int_distribution<size_t> unidist{ 0, rng::size(range) - 1 };
+    return std::next(rng::begin(range), unidist(reng));
 }
 
 // Returns random item(value) from range
