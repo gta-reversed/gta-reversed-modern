ffa847b8a544f59068953931143a4625ffda9120
Pirulax <patrikjankovics7@gmail.com>
2022-02-26 12:58:34 +0100

    tBoneInfo

---
+++ source/game_sa/tBoneInfo.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Base.h"
+
+struct tBoneInfo {
+    int16 field_0;
+    int16 field_2;
+    int32 field_4;
+    int32 field_8;
+    int32 field_C;
+    int32 field_10;
+    int32 field_14;
+    int32 field_18;
+    int32 field_1C;
+    int32 field_20;
+    int32 field_24;
+};
