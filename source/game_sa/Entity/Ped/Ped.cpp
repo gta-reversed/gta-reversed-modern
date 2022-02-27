c178e39f6cb8b769b21f5d10a949168dde17e11d
Pirulax <patrikjankovics7@gmail.com>
2022-02-27 22:06:03 +0100

    Fix `CPed::CanSetPedState`

--- source/game_sa/Entity/Ped/Ped.cpp
+++ source/game_sa/Entity/Ped/Ped.cpp
@@ -858,9 +858,9 @@ bool CPed::CanSetPedState() {
     case PEDSTATE_ENTER_CAR:
     case PEDSTATE_CARJACK:
     case PEDSTATE_STEAL_CAR:
-        return true;
+        return false;
     }
-    return false;
+    return true;
 }
 
 /*!
