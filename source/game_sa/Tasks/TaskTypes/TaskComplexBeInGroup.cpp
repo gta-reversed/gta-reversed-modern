c2d5ebb2a7b85e83584738f56dad20dbecd493fb
Pirulax <patrikjankovics7@gmail.com>
2022-03-01 22:10:46 +0100

    Add boilerplate

--- source/game_sa/Tasks/TaskTypes/TaskComplexBeInGroup.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexBeInGroup.cpp
@@ -2,15 +2,37 @@
 
 #include "TaskComplexBeInGroup.h"
 
-CTaskComplexBeInGroup::CTaskComplexBeInGroup(int32 groupId, bool isLeader)
+void CTaskComplexBeInGroup::InjectHooks() {
+    RH_ScopedClass(CTaskComplexBeInGroup);
+    RH_ScopedCategory("Tasks/TaskTypes");
+
+    RH_ScopedInstall(Constructor, 0x632E50);
+    RH_ScopedInstall(Destructor, 0x632EA0);
+
+    //RH_ScopedInstall(MonitorMainGroupTask, 0x633010);
+    //RH_ScopedInstall(MonitorSecondaryGroupTask, 0x6330B0);
+    //RH_ScopedInstall(Clone_Reversed, 0x636BE0);
+    RH_ScopedInstall(GetTaskType_Reversed, 0x632E90);
+    //RH_ScopedInstall(MakeAbortable_Reversed, 0x632EB0);
+    //RH_ScopedInstall(CreateNextSubTask_Reversed, 0x632F40);
+    //RH_ScopedInstall(CreateFirstSubTask_Reversed, 0x632FB0);
+    //RH_ScopedInstall(ControlSubTask_Reversed, 0x638AA0);
+}
+
+CTaskComplexBeInGroup::CTaskComplexBeInGroup(int32 groupId, bool isLeader) :
+    m_groupId{groupId},
+    m_isLeader{isLeader}
 {
-    m_groupId           = groupId;
-    m_ped               = 0;
-    m_mainTask          = nullptr;
-    m_secondaryTask     = nullptr;
-    m_isLeader          = isLeader;
-    m_mainTaskId        = TASK_NONE;
-    m_secondaryTaskSlot = -1;
+}
+
+// 0x633010
+void CTaskComplexBeInGroup::MonitorMainGroupTask(CPed* ped) {
+    return plugin::CallMethodAndReturn<void, 0x633010, CTaskComplexBeInGroup*, CPed*>(this, ped);
+}
+
+// 0x6330B0
+void CTaskComplexBeInGroup::MonitorSecondaryGroupTask(CPed* ped) {
+    plugin::CallMethod<0x6330B0, CTaskComplexBeInGroup*, CPed*>(this, ped);
 }
 
 CTask* CTaskComplexBeInGroup::Clone()
--- source/game_sa/Tasks/TaskTypes/TaskComplexBeInGroup.h
+++ source/game_sa/Tasks/TaskTypes/TaskComplexBeInGroup.h
@@ -3,27 +3,51 @@
 #include "TaskComplex.h"
 
 class CTaskComplexBeInGroup : public CTaskComplex {
-    int32  m_groupId;
-    bool   m_isLeader;
-    char   _pad[3];
-    CPed*  m_ped;
-    CTask* m_mainTask;
-    int32  m_mainTaskId;
-    CTask* m_secondaryTask;
-    int32  m_secondaryTaskSlot; // used as index in CTaskManager::m_aSecondaryTasks
+    int32  m_groupId{};
+    bool   m_isLeader{};
+    CPed*  m_ped{};
+    CTask* m_mainTask{};
+    int32  m_mainTaskId{TASK_NONE};
+    CTask* m_secondaryTask{};
+    int32  m_secondaryTaskSlot{-1}; // used as index in `CTaskManager::m_aSecondaryTasks`
 
 public:
     static constexpr auto Type = TASK_COMPLEX_BE_IN_GROUP;
 
+    static void InjectHooks();
+
     CTaskComplexBeInGroup(int32 groupId, bool isLeader);
     ~CTaskComplexBeInGroup() {}
 
+    void MonitorMainGroupTask(CPed* ped);
+    void MonitorSecondaryGroupTask(CPed* ped);
+
     CTask*    Clone() override;
     eTaskType GetTaskType() override { return Type; }
-    bool   MakeAbortable(CPed* ped, eAbortPriority priority, const CEvent* event) override;
-    CTask* CreateNextSubTask(CPed* ped) override;
-    CTask* CreateFirstSubTask(CPed* ped) override;
-    CTask* ControlSubTask(CPed* ped) override;
+    bool      MakeAbortable(CPed* ped, eAbortPriority priority, const CEvent* event) override;
+    CTask*    CreateNextSubTask(CPed* ped) override;
+    CTask*    CreateFirstSubTask(CPed* ped) override;
+    CTask*    ControlSubTask(CPed* ped) override;
+
+private:
+    // 0x632E50
+    CTaskComplexBeInGroup* Constructor(int32 groupId, bool isLeader) {
+        this->CTaskComplexBeInGroup::CTaskComplexBeInGroup(groupId, isLeader);
+        return this;
+    }
+
+    // 0x632EA0
+    CTaskComplexBeInGroup* Destructor() {
+        this->CTaskComplexBeInGroup::~CTaskComplexBeInGroup();
+        return this;
+    }
+
+    CTask*    Clone_Reversed() { return CTaskComplexBeInGroup::Clone(); }
+    eTaskType GetTaskType_Reversed() { return CTaskComplexBeInGroup::GetTaskType(); }
+    bool      MakeAbortable_Reversed(CPed* ped, eAbortPriority priority, CEvent const* event) { return CTaskComplexBeInGroup::MakeAbortable(ped, priority, event); }
+    CTask*    CreateNextSubTask_Reversed(CPed* ped) { return CTaskComplexBeInGroup::CreateNextSubTask(ped); }
+    CTask*    CreateFirstSubTask_Reversed(CPed* ped) { return CTaskComplexBeInGroup::CreateFirstSubTask(ped); }
+    CTask*    ControlSubTask_Reversed(CPed* ped) { return CTaskComplexBeInGroup::ControlSubTask(ped); }
 };
 
 VALIDATE_SIZE(CTaskComplexBeInGroup, 0x28);
