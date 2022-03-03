d265edbbef42d99ac75427f5f716b25d52ede7ea
Pirulax <patrikjankovics7@gmail.com>
2022-03-03 11:25:54 +0100

    Boilerplate + Dtor + Ctor

---
+++ source/game_sa/Tasks/TaskTypes/TaskComplexLeaveAnyCar.cpp
@@ -0,0 +1,45 @@
+#include "StdInc.h"
+#include "TaskComplexLeaveAnyCar.h"
+
+void CTaskComplexLeaveAnyCar::InjectHooks() {
+    RH_ScopedClass(CTaskComplexLeaveAnyCar);
+    RH_ScopedCategory("Tasks/TaskTypes");
+
+    // Constructors (1x)
+    RH_ScopedInstall(Constructor, 0x421150);
+    RH_ScopedInstall(Destructor, 0x421270);
+
+    //RH_ScopedInstall(Clone_Reversed, 0x4211C0);
+    //RH_ScopedInstall(GetTaskType_Reversed, 0x421240);
+    //RH_ScopedInstall(CreateNextSubTask_Reversed, 0x63BCD0);
+    //RH_ScopedInstall(CreateFirstSubTask_Reversed, 0x63BCE0);
+    //RH_ScopedInstall(ControlSubTask_Reversed, 0x63BDC0);
+}
+
+// 0x421150
+CTaskComplexLeaveAnyCar::CTaskComplexLeaveAnyCar(int32 delayTime, bool sensibleLeaveCar, bool forceGetOut) :
+    m_sensibleLeaveCar{sensibleLeaveCar},
+    m_delayTime{delayTime},
+    m_forceGetOut{forceGetOut}
+{
+}
+
+// 0x4211C0
+CTask* CTaskComplexLeaveAnyCar::Clone() {
+    return plugin::CallMethodAndReturn<CTask*, 0x4211C0, CTaskComplexLeaveAnyCar*>(this);
+}
+
+// 0x63BCD0
+CTask* CTaskComplexLeaveAnyCar::CreateNextSubTask(CPed* ped) {
+    return plugin::CallMethodAndReturn<CTask*, 0x63BCD0, CTaskComplexLeaveAnyCar*, CPed*>(this, ped);
+}
+
+// 0x63BCE0
+CTask* CTaskComplexLeaveAnyCar::CreateFirstSubTask(CPed* ped) {
+    return plugin::CallMethodAndReturn<CTask*, 0x63BCE0, CTaskComplexLeaveAnyCar*, CPed*>(this, ped);
+}
+
+// 0x63BDC0
+CTask* CTaskComplexLeaveAnyCar::ControlSubTask(CPed* ped) {
+    return plugin::CallMethodAndReturn<CTask*, 0x63BDC0, CTaskComplexLeaveAnyCar*, CPed*>(this, ped);
+}
---
+++ source/game_sa/Tasks/TaskTypes/TaskComplexLeaveAnyCar.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "TaskComplex.h"
+
+class CTaskComplexLeaveAnyCar : CTaskComplex {
+public:
+    int32 m_delayTime{};        // 0xC
+    bool  m_sensibleLeaveCar{}; // 0x10
+    bool  m_forceGetOut{};      // 0x11
+
+public:
+    constexpr static auto Type = TASK_COMPLEX_LEAVE_ANY_CAR;
+
+    static void InjectHooks();
+
+    CTaskComplexLeaveAnyCar(int32 delayTime, bool sensibleLeaveCar, bool forceGetOut);
+    ~CTaskComplexLeaveAnyCar() = default;
+
+    CTask * Clone() override;
+    eTaskType GetTaskType() override { return Type; }
+    CTask * CreateNextSubTask(CPed * ped) override;
+    CTask * CreateFirstSubTask(CPed * ped) override;
+    CTask * ControlSubTask(CPed * ped) override;
+private:
+    CTaskComplexLeaveAnyCar* Constructor(int32 delayTime, bool sensibleLeaveCar, bool forceGetOut) {
+        this->CTaskComplexLeaveAnyCar::CTaskComplexLeaveAnyCar(delayTime, sensibleLeaveCar, forceGetOut);
+        return this;
+    }
+
+    CTaskComplexLeaveAnyCar* Destructor() {
+        this->CTaskComplexLeaveAnyCar::~CTaskComplexLeaveAnyCar();
+        return this;
+    }
+
+     CTask * Clone_Reversed() { return CTaskComplexLeaveAnyCar::Clone(); }
+     eTaskType GetTaskType_Reversed() { return CTaskComplexLeaveAnyCar::GetTaskType(); }
+     CTask * CreateNextSubTask_Reversed(CPed * ped) { return CTaskComplexLeaveAnyCar::CreateNextSubTask(ped); }
+     CTask * CreateFirstSubTask_Reversed(CPed * ped) { return CTaskComplexLeaveAnyCar::CreateFirstSubTask(ped); }
+     CTask * ControlSubTask_Reversed(CPed * ped) { return CTaskComplexLeaveAnyCar::ControlSubTask(ped); }
+};
+VALIDATE_SIZE(CTaskComplexLeaveAnyCar, 0x14);
