7e8d1976fc6faf55d2bbc5e87f6031f2636c137d
yukani <yukani@zohomail.eu>
2022-03-04 21:46:14 +0300

    Update source/game_sa/GangWars.cpp
    Co-authored-by: Izzotop <1680033+Izzotop@users.noreply.github.com>

--- source/game_sa/GangWars.cpp
+++ source/game_sa/GangWars.cpp
@@ -275,7 +275,7 @@ bool CGangWars::PedStreamedInForThisGang(int32 gangId) {
         return false;
 
     for (auto i = 0u; i < numPeds; i++) {
-        if (CStreaming::GetInfo(*CPopulation::m_PedGroups[i]).m_nLoadState != LOADSTATE_LOADED)
+        if (CStreaming::GetInfo(*CPopulation::m_PedGroups[i]).IsLoaded())
             return true;
     }
 
