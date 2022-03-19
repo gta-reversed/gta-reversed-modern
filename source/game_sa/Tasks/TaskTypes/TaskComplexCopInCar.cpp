2daeb0a4363d636bd0a89f6cf177d64e159b873f
Pirulax <patrikjankovics7@gmail.com>
2022-03-19 02:23:55 +0100

    Add boilerplate

--- source/game_sa/Tasks/TaskTypes/TaskComplexCopInCar.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexCopInCar.cpp
@@ -1,3 +1,68 @@
 #include "StdInc.h"
-
 #include "TaskComplexCopInCar.h"
+
+void CTaskComplexCopInCar::InjectHooks() {
+    RH_ScopedClass(CTaskComplexCopInCar);
+    RH_ScopedCategory("Tasks/TaskTypes");
+
+    RH_ScopedInstall(Constructor, 0x68C7F0);
+    RH_ScopedInstall(Destructor, 0x68C8C0);
+
+    // RH_ScopedInstall(CreateSubTask, 0x68C9E0);
+
+    // RH_ScopedInstall(Clone_Reversed, 0x68CEC0);
+    // RH_ScopedInstall(GetTaskType_Reversed, 0x68C8B0);
+    // RH_ScopedInstall(MakeAbortable_Reversed, 0x68C940);
+    // RH_ScopedInstall(CreateNextSubTask_Reversed, 0x68FA50);
+    // RH_ScopedInstall(CreateFirstSubTask_Reversed, 0x68FA10);
+    // RH_ScopedInstall(ControlSubTask_Reversed, 0x68FD50);
+}
+
+// 0x68C7F0
+CTaskComplexCopInCar::CTaskComplexCopInCar(CVehicle* vehicle, CPed* cop1, CPed* cop2, bool flag_0x1) :
+    m_pVehicle{vehicle},
+    m_pCop1{cop1},
+    m_pCop2{cop2},
+    m_flag0x1{flag_0x1}
+{
+    CEntity::SafeRegisterRef(m_pVehicle);
+    CEntity::SafeRegisterRef(m_pCop1);
+    CEntity::SafeRegisterRef(m_pCop2);
+}
+
+// 0x68C8C0
+CTaskComplexCopInCar::~CTaskComplexCopInCar() {
+    CEntity::SafeCleanUpRef(m_pVehicle);
+    CEntity::SafeCleanUpRef(m_pCop1);
+    CEntity::SafeCleanUpRef(m_pCop2);
+}
+ 
+// 0x68C9E0
+void CTaskComplexCopInCar::CreateSubTask(eTaskType taskType, CPed* copPed) {
+    plugin::CallMethod<0x68C9E0, CTaskComplexCopInCar*, eTaskType, CPed*>(this, taskType, copPed);
+}
+
+// 0x68CEC0
+CTask* CTaskComplexCopInCar::Clone() {
+    return plugin::CallMethodAndReturn<CTask*, 0x68CEC0, CTaskComplexCopInCar*>(this);
+}
+
+// 0x68C940
+bool CTaskComplexCopInCar::MakeAbortable(CPed* ped, eAbortPriority priority, CEvent const* event) {
+    return plugin::CallMethodAndReturn<bool, 0x68C940, CTaskComplexCopInCar*, CPed*, eAbortPriority, CEvent const*>(this, ped, priority, event);
+}
+
+// 0x68FA50
+CTask* CTaskComplexCopInCar::CreateNextSubTask(CPed* ped) {
+    return plugin::CallMethodAndReturn<CTask*, 0x68FA50, CTaskComplexCopInCar*, CPed*>(this, ped);
+}
+
+// 0x68FA10
+CTask* CTaskComplexCopInCar::CreateFirstSubTask(CPed* ped) {
+    return plugin::CallMethodAndReturn<CTask*, 0x68FA10, CTaskComplexCopInCar*, CPed*>(this, ped);
+}
+
+// 0x68FD50
+CTask* CTaskComplexCopInCar::ControlSubTask(CPed* ped) {
+    return plugin::CallMethodAndReturn<CTask*, 0x68FD50, CTaskComplexCopInCar*, CPed*>(this, ped);
+}
--- source/game_sa/Tasks/TaskTypes/TaskComplexCopInCar.h
+++ source/game_sa/Tasks/TaskTypes/TaskComplexCopInCar.h
@@ -1,29 +1,68 @@
-/*
-    Plugin-SDK file
-    Authors: GTA Community. See more here
-    https://github.com/DK22Pac/plugin-sdk
-    Do not delete this comment block. Respect others' work!
-*/
 #pragma once
 
 #include "TaskComplex.h"
-#include "Vehicle.h"
-#include "Ped.h"
+#include "TaskTimer.h"
+
+class CPed;
+class CVehicle;
 
 class CTaskComplexCopInCar : public CTaskComplex {
 public:
-    CVehicle*  m_pVehicle;
-    CPed*      m_pCop1;
-    CPed*      m_pCop2;
-    CTaskTimer m_timer1;
-    CTaskTimer m_timer2;
-    uint8      m_nFlags;
-    char       _flags[3];
+    CVehicle* m_pVehicle{};    // 0xC
+    CPed* m_pCop1{};           // 0x10
+    CPed* m_pCop2{};           // 0x14
+    CTaskTimer m_timer1{};     // 0x18
+    CTaskTimer m_timer2{};     // 0x24
+    uint8 m_flag0x1 : 1{};     // 0x30 - 0st bit
+    uint8 m_flag0x2 : 1{};     // 0x30 - 1nd bit
+    uint8 m_flag0x4 : 1{true}; // 0x30 - 2rd bit
+    uint8 m_flag0x8 : 1{};     // 0x30 - 3th bit
 
 public:
     static constexpr auto Type = TASK_COMPLEX_COP_IN_CAR;
 
-    CTaskComplexCopInCar(CVehicle* vehicle, CPed* cop1, CPed* cop2, bool arg3);
-};
+    static void InjectHooks();
 
+    ~CTaskComplexCopInCar() override;
+    CTaskComplexCopInCar(CVehicle* vehicle, CPed* cop1, CPed* cop2, bool flag_0x1);
+
+    void CreateSubTask(eTaskType taskType, CPed* copPed);
+
+    CTask* Clone() override;
+    eTaskType GetTaskType() override { return Type; }
+    bool MakeAbortable(CPed* ped, eAbortPriority priority, CEvent const* event) override;
+    CTask* CreateNextSubTask(CPed* ped) override;
+    CTask* CreateFirstSubTask(CPed* ped) override;
+    CTask* ControlSubTask(CPed* ped) override;
+
+private: // Wrappers for hooks
+    CTaskComplexCopInCar* Constructor(CVehicle* vehicle, CPed* cop1, CPed* cop2, bool flag_0x1) {
+        this->CTaskComplexCopInCar::CTaskComplexCopInCar(vehicle, cop1, cop2, flag_0x1);
+        return this;
+    }
+
+    CTaskComplexCopInCar* Destructor() {
+        this->CTaskComplexCopInCar::~CTaskComplexCopInCar();
+        return this;
+    }
+
+    CTask* Clone_Reversed() {
+        return CTaskComplexCopInCar::Clone();
+    }
+    eTaskType GetTaskType_Reversed() {
+        return CTaskComplexCopInCar::GetTaskType();
+    }
+    bool MakeAbortable_Reversed(CPed* ped, eAbortPriority priority, CEvent const* event) {
+        return CTaskComplexCopInCar::MakeAbortable(ped, priority, event);
+    }
+    CTask* CreateNextSubTask_Reversed(CPed* ped) {
+        return CTaskComplexCopInCar::CreateNextSubTask(ped);
+    }
+    CTask* CreateFirstSubTask_Reversed(CPed* ped) {
+        return CTaskComplexCopInCar::CreateFirstSubTask(ped);
+    }
+    CTask* ControlSubTask_Reversed(CPed* ped) {
+        return CTaskComplexCopInCar::ControlSubTask(ped);
+    }
+};
 VALIDATE_SIZE(CTaskComplexCopInCar, 0x34);
