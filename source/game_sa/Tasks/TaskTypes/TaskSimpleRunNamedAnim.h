397ec27f1e8195784258a19c2d0dd3b70b488e75
Pirulax <patrikjankovics7@gmail.com>
2022-03-21 22:50:07 +0100

    `CTaskSimpleRunNamedAnim`

--- source/game_sa/Tasks/TaskTypes/TaskSimpleRunNamedAnim.h
+++ source/game_sa/Tasks/TaskTypes/TaskSimpleRunNamedAnim.h
@@ -6,7 +6,6 @@
 */
 #pragma once
 
-
 #include "TaskSimpleAnim.h"
 #include "AnimBlendHierarchy.h"
 #include "TaskTimer.h"
@@ -14,19 +13,55 @@
 
 class CTaskSimpleRunNamedAnim : public CTaskSimpleAnim {
 public:
-    char                 m_animName[24];
-    char                 m_animGroupName[16];
-    float                m_fBlendDelta;
-    CAnimBlendHierarchy* m_pAnimHierarchy;
-    int32                m_nTime;
-    CTaskTimer           m_timer;
-    CVector              m_vecOffsetAtEnd;
-    uint32               m_nFlags;
-    int16                m_nAnimId;
+    char                 m_animName[24]{};
+    char                 m_animGroupName[16]{};
+    float                m_blendDelta{};
+    CAnimBlendHierarchy* m_pAnimHierarchy{};
+    uint32               m_endTime{};
+    CTaskTimer           m_timer{};
+    CVector              m_vecOffsetAtEnd{};
+    uint32               m_animFlags{};
+    int16                m_nAnimId{};
 
 public:
-    CTaskSimpleRunNamedAnim(const char* pAnimName, const char* pAnimGroupName, int32 flags, float fBlendDelta,
-        int32 nTime, bool bDontInterrupt, bool bRunInSequence, bool bOffsetPed, bool bHoldLastFrame);
-};
+    static constexpr auto Type = TASK_SIMPLE_NAMED_ANIM;
 
+    friend void InjectHooksMain();
+    static void InjectHooks();
+
+    CTaskSimpleRunNamedAnim();
+    CTaskSimpleRunNamedAnim(const char* pAnimName, const char* pAnimGroupName, uint32 animFlags, float blendDelta,
+        uint32 endTime, bool bDontInterrupt, bool bRunInSequence, bool bOffsetPed, bool bHoldLastFrame);
+
+    bool ProcessPed(CPed* ped) override;
+    eTaskType GetTaskType() override { return Type; }
+
+private: // Wrappers for hooks
+    CTaskSimpleRunNamedAnim* Constructor(char const* pAnimName, char const* pAnimGroupName, int32 animFlags, float blendDelta, int32 endTime, bool bDontInterrupt,
+                                         bool bRunInSequence, bool bOffsetPed, bool bHoldLastFrame) {
+        this->CTaskSimpleRunNamedAnim::CTaskSimpleRunNamedAnim(pAnimName, pAnimGroupName, animFlags, blendDelta, endTime, bDontInterrupt, bRunInSequence, bOffsetPed,
+                                                               bHoldLastFrame);
+        return this;
+    }
+    
+    CTaskSimpleRunNamedAnim* Constructor() {
+        this->CTaskSimpleRunNamedAnim::CTaskSimpleRunNamedAnim();
+        return this;
+    }
+
+    CTaskSimpleRunNamedAnim* Destructor() {
+        this->CTaskSimpleRunNamedAnim::~CTaskSimpleRunNamedAnim();
+        return this;
+    }
+
+    //CTask* Clone_Reversed() {
+    //    return CTaskSimpleRunNamedAnim::Clone();
+    //}
+    eTaskType GetTaskType_Reversed() {
+        return CTaskSimpleRunNamedAnim::GetTaskType();
+    }
+    //bool ProcessPed_Reversed(CPed* ped) {
+    //    return CTaskSimpleRunNamedAnim::ProcessPed(ped);
+    //}
+};
 VALIDATE_SIZE(CTaskSimpleRunNamedAnim, 0x64);
