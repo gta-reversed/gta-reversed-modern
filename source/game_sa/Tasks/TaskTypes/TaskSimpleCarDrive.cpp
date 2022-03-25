de3d6486feac7c4e6a4caa44969a35ad48ad3deb
Pirulax <patrikjankovics7@gmail.com>
2022-03-25 20:51:12 +0100

    `TriggerIK`

--- source/game_sa/Tasks/TaskTypes/TaskSimpleCarDrive.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskSimpleCarDrive.cpp
@@ -1,6 +1,7 @@
 #include "StdInc.h"
 #include "TaskSimpleCarDrive.h"
 #include "TaskUtilityLineUpPedWithCar.h"
+#include "IKChainManager_c.h"
 
 void CTaskSimpleCarDrive::InjectHooks() {
     RH_ScopedClass(CTaskSimpleCarDrive);
@@ -9,7 +10,7 @@ void CTaskSimpleCarDrive::InjectHooks() {
     RH_ScopedInstall(Constructor, 0x63C340);
     RH_ScopedInstall(Destructor, 0x63C460);
 
-    //RH_ScopedInstall(TriggerIK, 0x63C500);
+    RH_ScopedInstall(TriggerIK, 0x63C500);
     //RH_ScopedInstall(UpdateBopping, 0x63C900);
     //RH_ScopedInstall(StartBopping, 0x642760);
     //RH_ScopedInstall(ProcessHeadBopping, 0x6428C0);
@@ -33,7 +34,38 @@ CTaskSimpleCarDrive::CTaskSimpleCarDrive(CVehicle* vehicle, CTaskUtilityLineUpPe
 
 // 0x63C500
 void CTaskSimpleCarDrive::TriggerIK(CPed* ped) {
-    plugin::CallMethod<0x63C500, CTaskSimpleCarDrive*, CPed*>(this, ped);
+    if (!m_pVehicle) {
+        return;
+    }
+
+    // Made an early-out of this, as this same code is used in both possible cases.
+    if (g_ikChainMan.IsLooking(ped) || CGeneral::GetRandomNumberInRange(0, 100) >= 5) {
+        return;
+    }
+
+    switch (m_pVehicle->m_autoPilot.m_nCarMission) {
+    case MISSION_RAMPLAYER_FARAWAY:
+    case MISSION_RAMPLAYER_CLOSE:
+    case MISSION_BLOCKPLAYER_FARAWAY:
+    case MISSION_BLOCKPLAYER_CLOSE:
+    case MISSION_BLOCKPLAYER_HANDBRAKESTOP: { // Make ped look at player ped 
+        g_ikChainMan.LookAt("DriveCar", ped, FindPlayerPed(), 3000, BONE_HEAD, nullptr, false, 0.25f, 500, 3, false);
+        break;
+    }
+    case MISSION_RAMCAR_FARAWAY:
+    case MISSION_RAMCAR_CLOSE: {
+        if (const auto vehTargetCar = m_pVehicle->m_autoPilot.m_pTargetCar) {
+            if (vehTargetCar->IsVehicle()) {
+                if (const auto driver = vehTargetCar->m_pDriver) { // Make ped look at target car or it's driver (if any)
+                    g_ikChainMan.LookAt("DriveCar", ped, driver, 3000, BONE_HEAD, nullptr, false, 0.25f, 500, 3, false);
+                } else {
+                    g_ikChainMan.LookAt("DriveCar", ped, vehTargetCar, 3000, BONE_UNKNOWN, nullptr, false, 0.25f, 500, 3, false);
+                }
+            }
+        }
+        break;
+    }
+    }
 }
 
 // 0x63C900
