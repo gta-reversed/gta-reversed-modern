3e4d21ee80d3464a85f3a9bec733e2d05319a64e
Izzotop <1680033+Izzotop@users.noreply.github.com>
2022-04-27 22:48:46 +0300

    Reverse 9/10 CCarFXRenderer funcs

--- source/game_sa/CustomCarEnvMapPipeline.cpp
+++ source/game_sa/CustomCarEnvMapPipeline.cpp
@@ -6,3 +6,23 @@
 void CCustomCarEnvMapPipeline::PreRenderUpdate() {
     plugin::Call<0x5D8870>();
 }
+
+// 0x5DA020
+bool CCustomCarEnvMapPipeline::CreatePipe() {
+    return plugin::CallAndReturn<bool, 0x5DA020>();
+}
+
+// 0x5DA130
+void CCustomCarEnvMapPipeline::DestroyPipe() {
+    plugin::Call<0x5DA130>();
+}
+
+// 0x5DA450
+bool CCustomCarEnvMapPipeline::RegisterPlugin() {
+    return plugin::CallAndReturn<bool, 0x5DA450>();
+}
+
+// 0x5DA610
+RpAtomic* CCustomCarEnvMapPipeline::CustomPipeAtomicSetup(RpAtomic* atomic) {
+    return plugin::CallAndReturn<RpAtomic*, 0x5DA610, RpAtomic*>(atomic);
+}
