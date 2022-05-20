d68ab259e2ecf01e708dec178db2d2e4fa6f8160
yukani <yukani@zohomail.eu>
2022-05-20 21:19:57 +0300

    CGangWarsSaveStructure::Extract

--- source/game_sa/GangWarsSaveStructure.cpp
+++ source/game_sa/GangWarsSaveStructure.cpp
@@ -11,13 +11,19 @@ void CGangWarsSaveStructure::Construct() {
     GangWarZoneInfoIndex = -1;
 
     if (CGangWars::pZoneInfoToFightOver) {
-        GangWarZoneInfoIndex = std::find(CTheZones::ZoneInfoArray,
-            CTheZones::ZoneInfoArray + CTheZones::TotalNumberOfZoneInfos, *CGangWars::pZoneInfoToFightOver)
-            - CTheZones::ZoneInfoArray;
-
-        GangWarNavigationZoneIndex = std::find(CTheZones::NavigationZoneArray,
-            CTheZones::NavigationZoneArray + CTheZones::TotalNumberOfNavigationZones, *CGangWars::pZoneToFightOver)
-            - CTheZones::NavigationZoneArray;
+        for (auto i = 0u; i < CTheZones::TotalNumberOfZoneInfos; i++) {
+            if (CGangWars::pZoneInfoToFightOver == &CTheZones::ZoneInfoArray[i]) {
+                GangWarZoneInfoIndex = i;
+                break;
+            }
+        }
+        
+        for (auto i = 0u; i < CTheZones::TotalNumberOfNavigationZones; i++) {
+            if (CGangWars::pZoneToFightOver == &CTheZones::NavigationZoneArray[i]) {
+                GangWarZoneInfoIndex = i;
+                break;
+            }
+        }
     }
 
     CoorsOfPlayerAtStartOfWar = CGangWars::CoorsOfPlayerAtStartOfWar;
@@ -37,4 +43,25 @@ void CGangWarsSaveStructure::Construct() {
 // 0x5D2740
 void CGangWarsSaveStructure::Extract() {
     assert(0);
+    CGangWars::bGangWarsActive = bGangWarsActive;
+    CGangWars::State = State;
+    CGangWars::TimeStarted = TimeStarted;
+
+    auto zoneInfoIdx = GangWarZoneInfoIndex, navZoneIdx = GangWarNavigationZoneIndex;
+    CGangWars::pZoneInfoToFightOver = (zoneInfoIdx != -1) ? &CTheZones::ZoneInfoArray[zoneInfoIdx] : nullptr;
+    CGangWars::pZoneToFightOver = (navZoneIdx != -1) ? &CTheZones::NavigationZoneArray[navZoneIdx] : nullptr;
+
+    CGangWars::CoorsOfPlayerAtStartOfWar = CoorsOfPlayerAtStartOfWar;
+    CGangWars::Gang1 = Gang1;
+    CGangWars::Gang2 = Gang2;
+    CGangWars::WarFerocity = WarFerocity;
+    CGangWars::LastTimeInArea = LastTimeInArea;
+    CGangWars::State2 = State2;
+    CGangWars::TimeTillNextAttack = TimeTillNextAttack;
+    CGangWars::PointOfAttack = PointOfAttack;
+    CGangWars::FightTimer = FightTimer;
+    CGangWars::RadarBlip = RadarBlip;
+    CGangWars::bPlayerIsCloseby = bPlayerIsCloseby;
+    CGangWars::TerritoryUnderControlPercentage = TerritoryUnderControlPercentage;
+    CGangWars::Difficulty = Difficulty;
 }
