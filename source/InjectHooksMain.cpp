8a6aa81202c5d4ffbd16e93b22156def658fc9df
Izzotop <1680033+Izzotop@users.noreply.github.com>
2022-06-07 13:26:27 +0300

    Reverse 3 CAnimBlendStaticAssociation funcs

--- source/InjectHooksMain.cpp
+++ source/InjectHooksMain.cpp
@@ -243,6 +243,7 @@ void InjectHooksMain() {
     CPad::InjectHooks();
     CFileMgr::InjectHooks();
 
+    CAnimBlendStaticAssociation::InjectHooks();
     CEscalator::InjectHooks();
     CShadowCamera::InjectHooks();
     CInterestingEvents::InjectHooks();
