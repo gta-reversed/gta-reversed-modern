b4403d7910774ba02365bc2e1d50f41ab567fc31
Izzotop <1680033+Izzotop@users.noreply.github.com>
2022-08-06 17:16:48 +0300

    Reverse CMessages::ClearAllMessagesDisplayedByGame

--- source/game_sa/Messages.cpp
+++ source/game_sa/Messages.cpp
@@ -1,6 +1,7 @@
 #include "StdInc.h"
 
 #include "Messages.h"
+#include "Hud.h"
 
 void CMessages::InjectHooks() {
     RH_ScopedClass(CMessages);
@@ -27,7 +28,7 @@ void CMessages::InjectHooks() {
     // RH_ScopedInstall(ClearThisBigPrint, 0x69EBE0);
     // RH_ScopedInstall(ClearThisPrintBigNow, 0x69ED80);
     // RH_ScopedInstall(Init, 0x69EE00);
-    // RH_ScopedInstall(ClearAllMessagesDisplayedByGame, 0x69EDC0);
+    RH_ScopedInstall(ClearAllMessagesDisplayedByGame, 0x69EDC0);
     // RH_ScopedInstall(Process, 0x69EE60);
     // RH_ScopedInstall(Display, 0x69EFC0);
     // RH_ScopedInstall(AddMessage, 0x69F0B0);
@@ -155,8 +156,10 @@ void CMessages::ClearThisPrintBigNow(eMessageStyle style) {
 
 // Removes all displayed messages
 // 0x69EDC0
-void CMessages::ClearAllMessagesDisplayedByGame(uint8 unk) {
-    plugin::Call<0x69EDC0, uint8>(unk);
+void CMessages::ClearAllMessagesDisplayedByGame(bool unk) {
+    ClearMessages(unk);
+    ClearPreviousBriefArray();
+    CHud::GetRidOfAllHudMessages(unk);
 }
 
 // Returns length of a string
--- source/game_sa/Messages.h
+++ source/game_sa/Messages.h
@@ -50,14 +50,14 @@ public:
     static void AddMessage(const char* text, uint32 time, uint16 flag, bool bPreviousBrief);
     static void AddMessageJumpQ(const char* text, uint32 time, uint16 flag, bool bPreviousBrief);
     static void AddMessageWithString(const char* text, uint32 time, uint16 flag, char* string, bool bPreviousBrief);
-    static void AddMessageWithNumber(const char* text, uint32 time, uint16 flag, int32 n1, int32 n2, int32 n3, int32 n4, int32 n5, int32 n6, bool bPreviousBrief);
-    static void AddMessageJumpQWithNumber(const char* text, uint32 time, uint16 flag, int32 n1, int32 n2, int32 n3, int32 n4, int32 n5, int32 n6, bool bPreviousBrief);
+    static void AddMessageWithNumber(const char* text, uint32 time, uint16 flag, int32 n1 = -1, int32 n2 = -1, int32 n3 = -1, int32 n4 = -1, int32 n5 = -1, int32 n6 = -1, bool bPreviousBrief = false);
+    static void AddMessageJumpQWithNumber(const char* text, uint32 time, uint16 flag, int32 n1 = -1, int32 n2 = -1, int32 n3 = -1, int32 n4 = -1, int32 n5 = -1, int32 n6 = -1, bool bPreviousBrief = false);
     static void AddMessageJumpQWithString(const char* text, uint32 time, uint16 flag, char* string, bool bPreviousBrief);
 
     static void AddBigMessage(const char* text, uint32 time, eMessageStyle style);
     static void AddBigMessageQ(const char* text, uint32 time, eMessageStyle style);
-    static void AddBigMessageWithNumber(const char* text, uint32 time, eMessageStyle style, int32 n1, int32 n2, int32 n3, int32 n4, int32 n5, int32 n6);
-    static void AddBigMessageWithNumberQ(const char* text, uint32 time, eMessageStyle style, int32 n1, int32 n2, int32 n3, int32 n4, int32 n5, int32 n6);
+    static void AddBigMessageWithNumber(const char* text, uint32 time, eMessageStyle style, int32 n1 = -1, int32 n2 = -1, int32 n3 = -1, int32 n4 = -1, int32 n5 = -1, int32 n6 = -1);
+    static void AddBigMessageWithNumberQ(const char* text, uint32 time, eMessageStyle style, int32 n1 = -1, int32 n2 = -1, int32 n3 = -1, int32 n4 = -1, int32 n5 = -1, int32 n6 = -1);
 
     static void AddToPreviousBriefArray(const char* text, int32 n1 = -1, int32 n2 = -1, int32 n3 = -1, int32 n4 = -1, int32 n5 = -1, int32 n6 = -1, char* string = nullptr);
 
@@ -67,7 +67,7 @@ public:
     static void ClearThisPrint(const char* text);
     static void ClearThisBigPrint(const char* text);
     static void ClearThisPrintBigNow(eMessageStyle style);
-    static void ClearAllMessagesDisplayedByGame(uint8 unk);
+    static void ClearAllMessagesDisplayedByGame(bool unk);
 
     static uint32 GetStringLength(const char* string);
 
@@ -75,7 +75,7 @@ public:
     static uint8 StringCompare(const char* str1, const char* str2, uint16 len);
     static void CutString(int32 count, const char* str, char** dest);
 
-    static void InsertNumberInString(const char* src, int32 n1, int32 n2, int32 n3, int32 n4, int32 n5, int32 n6, char* dst);
+    static void InsertNumberInString(const char* src, int32 n1 = -1, int32 n2 = -1, int32 n3 = -1, int32 n4 = -1, int32 n5 = -1, int32 n6 = -1, char* dst = nullptr);
     static void InsertStringInString(const char* src, char* string);
     static void InsertPlayerControlKeysInString(const char* string);
 
