9febad34dcca7c91e4b6d8138e5c27fa5bfc570d
Pirulax <patrikjankovics7@gmail.com>
2022-08-12 18:43:35 +0200

    Make DEV_LOG macro work

--- source/debug.h
+++ source/debug.h
@@ -3,8 +3,21 @@
 #define VERIFY_IMPL(expr, ...) expr
 #define VERIFY VERIFY_IMPL
 
-#define DEV_LOG(format, ...)
+#ifdef _DEBUG
+static void dev_printf(int lineno, const char* file, const char* fmt, ...) {
+    (void)printf("[%s @ %i]: ", file, lineno);
 
+    va_list vargs;
+    va_start(vargs, fmt);
+    (void)vprintf(fmt, vargs);
+    va_end(vargs);
+
+    printf("\n");
+}
+#define DEV_LOG(...) dev_printf(__LINE__, __FILE__, ##__VA_ARGS__)
+#else
+#define DEV_LOG(...)
+#endif
 #define PUSH_RENDERGROUP(str) 0
 #define POP_RENDERGROUP() 0
 
