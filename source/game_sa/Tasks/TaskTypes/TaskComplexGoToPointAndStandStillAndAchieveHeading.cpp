4d54b830f1e5f88c210571ecde2c3fc3877b8cfd
Izzotop <1680033+Izzotop@users.noreply.github.com>
2022-08-15 00:26:25 +0300

    CTaskComplexGoToPointAndStandStillAndAchieveHeading 1

--- source/game_sa/Tasks/TaskTypes/TaskComplexGoToPointAndStandStillAndAchieveHeading.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexGoToPointAndStandStillAndAchieveHeading.cpp
@@ -1,5 +1,9 @@
 #include "StdInc.h"
+
 #include "TaskComplexGoToPointAndStandStillAndAchieveHeading.h"
+#include "TaskSimpleAchieveHeading.h"
+#include "TaskSimpleStandStill.h"
+#include "TaskSimpleGoToPoint.h"
 
 void CTaskComplexGoToPointAndStandStillAndAchieveHeading::InjectHooks() {
     RH_ScopedClass(CTaskComplexGoToPointAndStandStillAndAchieveHeading);
@@ -16,19 +20,44 @@ void CTaskComplexGoToPointAndStandStillAndAchieveHeading::InjectHooks() {
 }
 
 // 0x668CD0
-CTaskComplexGoToPointAndStandStillAndAchieveHeading::CTaskComplexGoToPointAndStandStillAndAchieveHeading(int32 a2, CVector const& a3, float a4, float a5, float a6, float a7) {}
-
-// 0x668D40
-CTaskComplexGoToPointAndStandStillAndAchieveHeading::~CTaskComplexGoToPointAndStandStillAndAchieveHeading() {}
-
-// 0x66CFD0
-CTask* CTaskComplexGoToPointAndStandStillAndAchieveHeading::Clone() {
-    return plugin::CallMethodAndReturn<CTask*, 0x66CFD0, CTaskComplexGoToPointAndStandStillAndAchieveHeading*>(this);
+CTaskComplexGoToPointAndStandStillAndAchieveHeading::CTaskComplexGoToPointAndStandStillAndAchieveHeading(int32 a2, CVector const& a3, float a4, float a5, float a6, float a7)
+    : CTaskComplex()
+{
+    dwordC = a2;
+    dword10 = a3;
+    float1C = a5;
+    float28 = a7;
+    m_nFlags = m_nFlags | 1; // todo: flags
+    float20 = a4;
+    float24 = a6;
 }
 
 // 0x66DFD0
 CTask* CTaskComplexGoToPointAndStandStillAndAchieveHeading::CreateNextSubTask(CPed* ped) {
-    return plugin::CallMethodAndReturn<CTask*, 0x66DFD0, CTaskComplexGoToPointAndStandStillAndAchieveHeading*, CPed*>(this, ped);
+    switch (m_pSubTask->GetTaskType()) {
+    case TASK_SIMPLE_GO_TO_POINT:
+        return CreateSubTask(TASK_SIMPLE_STAND_STILL);
+    case TASK_SIMPLE_ACHIEVE_HEADING:
+        return CreateSubTask(TASK_FINISHED);
+    case TASK_SIMPLE_STAND_STILL:
+        return CreateSubTask(TASK_SIMPLE_ACHIEVE_HEADING);
+    default:
+        return nullptr;
+    }
+}
+
+// 0x668D50
+CTask* CTaskComplexGoToPointAndStandStillAndAchieveHeading::CreateSubTask(eTaskType taskType) {
+    switch (taskType) {
+    case TASK_SIMPLE_ACHIEVE_HEADING:
+        return new CTaskSimpleAchieveHeading(float20, float24, float28);
+    case TASK_SIMPLE_STAND_STILL:
+        return new CTaskSimpleStandStill();
+    case TASK_SIMPLE_GO_TO_POINT:
+        return new CTaskSimpleGoToPoint(dwordC, dword10, float1C, false, false);
+    default:
+        return nullptr;
+    }
 }
 
 // 0x66E030
--- source/game_sa/Tasks/TaskTypes/TaskComplexGoToPointAndStandStillAndAchieveHeading.h
+++ source/game_sa/Tasks/TaskTypes/TaskComplexGoToPointAndStandStillAndAchieveHeading.h
@@ -1,33 +1,35 @@
 #pragma once
+
 #include "TaskComplex.h"
 
 class CVector;
 
 class CTaskComplexGoToPointAndStandStillAndAchieveHeading : public CTaskComplex {
+private:
+    int32   dwordC;
+    CVector dword10;
+    float   float1C;
+    float   float20;
+    float   float24;
+    float   float28;
+    uint8   m_nFlags;
+
 public:
+    static constexpr auto Type = TASK_COMPLEX_GO_TO_POINT_AND_STAND_STILL_AND_ACHIEVE_HEADING;
+
+    CTaskComplexGoToPointAndStandStillAndAchieveHeading(int32 a2, const CVector& a3, float a4, float a5, float a6, float a7);
+    ~CTaskComplexGoToPointAndStandStillAndAchieveHeading() override = default; // 0x668D40
+
+    eTaskType GetTaskType() override { return Type; }
+    CTask* Clone() override { return new CTaskComplexGoToPointAndStandStillAndAchieveHeading(dwordC, dword10, float20, float1C, float24, float28); } // 0x66CFD0
+    CTask* CreateNextSubTask(CPed* ped) override;
+    CTask* CreateFirstSubTask(CPed* ped) override;
+    CTask* ControlSubTask(CPed* ped) override;
+    CTask* CreateSubTask(eTaskType taskType);
+
+private:
+    friend void InjectHooksMain();
     static void InjectHooks();
-
-    ~CTaskComplexGoToPointAndStandStillAndAchieveHeading();
-    CTaskComplexGoToPointAndStandStillAndAchieveHeading(int32 a2, CVector const& a3, float a4, float a5, float a6, float a7);
-
-    static constexpr auto Type = eTaskType::TASK_COMPLEX_GO_TO_POINT_AND_STAND_STILL_AND_ACHIEVE_HEADING;
-
-    virtual CTask* Clone();
-    virtual eTaskType GetTaskType() { return Type; }
-    virtual CTask* CreateNextSubTask(CPed* ped);
-    virtual CTask* CreateFirstSubTask(CPed* ped);
-    virtual CTask* ControlSubTask(CPed* ped);
-
-private: // Wrappers for hooks
-    // 0x668CD0
-    CTaskComplexGoToPointAndStandStillAndAchieveHeading* Constructor(int32 a2, CVector const& a3, float a4, float a5, float a6, float a7) {
-        this->CTaskComplexGoToPointAndStandStillAndAchieveHeading::CTaskComplexGoToPointAndStandStillAndAchieveHeading(a2, a3, a4, a5, a6, a7);
-        return this;
-    }
-
-    // 0x668D40
-    CTaskComplexGoToPointAndStandStillAndAchieveHeading* Destructor() {
-        this->CTaskComplexGoToPointAndStandStillAndAchieveHeading::~CTaskComplexGoToPointAndStandStillAndAchieveHeading();
-        return this;
-    }
+    CTaskComplexGoToPointAndStandStillAndAchieveHeading* Constructor(int32 a2, CVector const& a3, float a4, float a5, float a6, float a7) { this->CTaskComplexGoToPointAndStandStillAndAchieveHeading::CTaskComplexGoToPointAndStandStillAndAchieveHeading(a2, a3, a4, a5, a6, a7); return this; }
+    CTaskComplexGoToPointAndStandStillAndAchieveHeading* Destructor() { this->CTaskComplexGoToPointAndStandStillAndAchieveHeading::~CTaskComplexGoToPointAndStandStillAndAchieveHeading(); return this; }
 };
