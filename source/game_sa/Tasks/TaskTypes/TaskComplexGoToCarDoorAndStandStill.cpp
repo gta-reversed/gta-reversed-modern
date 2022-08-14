8820d3edc975b75d145ea88552d0d6e74b5627b7
Izzotop <1680033+Izzotop@users.noreply.github.com>
2022-08-14 23:58:05 +0300

    CTaskComplexGoToCarDoorAndStandStill 1

--- source/game_sa/Tasks/TaskTypes/TaskComplexGoToCarDoorAndStandStill.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexGoToCarDoorAndStandStill.cpp
@@ -1,4 +1,5 @@
 #include "StdInc.h"
+
 #include "TaskComplexGoToCarDoorAndStandStill.h"
 
 void CTaskComplexGoToCarDoorAndStandStill::InjectHooks() {
@@ -7,48 +8,51 @@ void CTaskComplexGoToCarDoorAndStandStill::InjectHooks() {
 
     RH_ScopedInstall(Constructor, 0x645780);
     RH_ScopedInstall(Destructor, 0x64A580);
-
     // RH_ScopedInstall(IsVehicleInRange, 0x6458A0);
     // RH_ScopedInstall(ComputeRouteToDoor, 0x645910);
-    // RH_ScopedInstall(CreateSubTask, 0x64A5F0);
-
-    // RH_ScopedInstall(Clone, 0x6498B0);
-    RH_ScopedInstall(GetTaskType, 0x645830);
-    // RH_ScopedInstall(MakeAbortable, 0x645840);
-    // RH_ScopedInstall(CreateNextSubTask, 0x64D2B0);
-    // RH_ScopedInstall(CreateFirstSubTask, 0x64D440);
-    // RH_ScopedInstall(ControlSubTask, 0x64A820);
+    // RH_ScopedInstall(CreateSubTask_Reversed, 0x64A5F0);
+    // RH_ScopedInstall(Clone_Reversed, 0x6498B0);
+    // RH_ScopedInstall(MakeAbortable_Reversed, 0x645840);
+    // RH_ScopedInstall(CreateNextSubTask_Reversed, 0x64D2B0);
+    // RH_ScopedInstall(CreateFirstSubTask_Reversed, 0x64D440);
+    // RH_ScopedInstall(ControlSubTask_Reversed, 0x64A820);
 }
 
 // 0x645780
-CTaskComplexGoToCarDoorAndStandStill::CTaskComplexGoToCarDoorAndStandStill(CVehicle* vehicle, int32 a3, bool a4, int32 a5, float a6, float a7, float a8, int32 a9) {}
+CTaskComplexGoToCarDoorAndStandStill::CTaskComplexGoToCarDoorAndStandStill(CVehicle* vehicle, int32 a3, bool a4, int32 a5, float a6, float a7, float a8, int32 a9) {
+    m_pVehicle = vehicle;
+    f4 = a3;
+    f14 = a4;
+    f18 = a6;
+    f1C = a7;
+    f20 = a8;
+    f24 = a9;
+    f28 = 0.0f;
+    f2C = 0.0f;
+    f30 = 0;
+    m_nDoorId = 0;
+    m_vecPositionToOpenCarDoor = CVector{};
+    f44 = a5;
+    f48 = 0;
+    f49 = 0;
+    f4C = nullptr;
+    CEntity::SafeRegisterRef(m_pVehicle);
+}
 
 // 0x64A580
-CTaskComplexGoToCarDoorAndStandStill::~CTaskComplexGoToCarDoorAndStandStill() {}
-
-// 0x6458A0
-BOOL CTaskComplexGoToCarDoorAndStandStill::IsVehicleInRange(int32 a2) {
-    return plugin::CallMethodAndReturn<BOOL, 0x6458A0, CTaskComplexGoToCarDoorAndStandStill*, int32>(this, a2);
-}
-
-// 0x645910
-CVector* CTaskComplexGoToCarDoorAndStandStill::ComputeRouteToDoor(CPed const& ped) {
-    return plugin::CallMethodAndReturn<CVector*, 0x645910, CTaskComplexGoToCarDoorAndStandStill*, CPed const&>(this, ped);
-}
-
-// 0x64A5F0
-void* CTaskComplexGoToCarDoorAndStandStill::CreateSubTask(eTaskType taskType, const CPed* a3) {
-    return plugin::CallMethodAndReturn<void*, 0x64A5F0, CTaskComplexGoToCarDoorAndStandStill*, eTaskType, const CPed*>(this, taskType, a3);
-}
-
-// 0x6498B0
-CTask* CTaskComplexGoToCarDoorAndStandStill::Clone() {
-    return plugin::CallMethodAndReturn<CTask*, 0x6498B0, CTaskComplexGoToCarDoorAndStandStill*>(this);
+CTaskComplexGoToCarDoorAndStandStill::~CTaskComplexGoToCarDoorAndStandStill() {
+    CEntity::SafeCleanUpRef(m_pVehicle);
+    delete f4C;
 }
 
 // 0x645840
-bool CTaskComplexGoToCarDoorAndStandStill::MakeAbortable(CPed* ped, int32 priority, CEvent const* event) {
+bool CTaskComplexGoToCarDoorAndStandStill::MakeAbortable(CPed* ped, eAbortPriority priority, const CEvent* event) {
     return plugin::CallMethodAndReturn<bool, 0x645840, CTaskComplexGoToCarDoorAndStandStill*, CPed*, int32, CEvent const*>(this, ped, priority, event);
+}
+
+// 0x64A5F0
+CTask* CTaskComplexGoToCarDoorAndStandStill::CreateSubTask(eTaskType taskType, const CPed* ped) {
+    return plugin::CallMethodAndReturn<CTask*, 0x64A5F0, CTaskComplexGoToCarDoorAndStandStill*, eTaskType, const CPed*>(this, taskType, ped);
 }
 
 // 0x64D2B0
@@ -64,4 +68,14 @@ CTask* CTaskComplexGoToCarDoorAndStandStill::CreateFirstSubTask(CPed* ped) {
 // 0x64A820
 CTask* CTaskComplexGoToCarDoorAndStandStill::ControlSubTask(CPed* ped) {
     return plugin::CallMethodAndReturn<CTask*, 0x64A820, CTaskComplexGoToCarDoorAndStandStill*, CPed*>(this, ped);
+}
+
+// 0x6458A0
+bool CTaskComplexGoToCarDoorAndStandStill::IsVehicleInRange(const CPed& ped) {
+    return plugin::CallMethodAndReturn<bool, 0x6458A0, CTaskComplexGoToCarDoorAndStandStill*, const CPed&>(this, ped);
+}
+
+// 0x645910
+CVector* CTaskComplexGoToCarDoorAndStandStill::ComputeRouteToDoor(const CPed& ped) {
+    return plugin::CallMethodAndReturn<CVector*, 0x645910, CTaskComplexGoToCarDoorAndStandStill*, CPed const&>(this, ped);
 }
--- source/game_sa/Tasks/TaskTypes/TaskComplexGoToCarDoorAndStandStill.h
+++ source/game_sa/Tasks/TaskTypes/TaskComplexGoToCarDoorAndStandStill.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "TaskComplex.h"
 
 class CVector;
@@ -6,54 +7,45 @@ class CVehicle;
 
 class CTaskComplexGoToCarDoorAndStandStill : public CTaskComplex {
 public:
-    CVehicle* m_pVehicle = {};               // 0xC
-    int32 f4 = {};                           // 0x10
-    bool f14 = {};                           // 0x14
-    float f18 = {};                          // 0x18
-    float f1C = {};                          // 0x1C
-    float f20 = {};                          // 0x20
-    int32 f24 = {};                          // 0x24
-    float f28 = {};                          // 0x28
-    float f2C = {};                          // 0x2C
-    int16 f30 = {};                          // 0x30
-    int32 m_nDoorId = {};                    // 0x34
-    CVector m_vecPositionToOpenCarDoor = {}; // 0x38
-    int32 f44 = {};                          // 0x44
-    bool f48 = {};                           // 0x48
-    bool f49 = {};                           // 0x49
-    int32 f4C = {};                          // 0x4C
-    int32 dword44 = {};                      // 0x50
+    CVehicle*    m_pVehicle;
+    int32        f4;
+    bool         f14;
+    float        f18;
+    float        f1C;
+    float        f20;
+    int32        f24;
+    float        f28;
+    float        f2C;
+    int16        f30;
+    int32        m_nDoorId;
+    CVector      m_vecPositionToOpenCarDoor;
+    int32        f44;
+    bool         f48;
+    bool         f49;
+    CPointRoute* f4C;
+    int32        dword44;
 
 public:
-    static void InjectHooks();
+    static constexpr auto Type = TASK_COMPLEX_GO_TO_CAR_DOOR_AND_STAND_STILL;
 
     CTaskComplexGoToCarDoorAndStandStill(CVehicle* vehicle, int32 a3, bool a4, int32 a5, float a6, float a7, float a8, int32 a9);
-    ~CTaskComplexGoToCarDoorAndStandStill();
+    ~CTaskComplexGoToCarDoorAndStandStill() override;
 
-    BOOL IsVehicleInRange(int32 a2);
-    CVector* ComputeRouteToDoor(CPed const& ped);
-    void* CreateSubTask(eTaskType taskType, const CPed* a3);
+    eTaskType GetTaskType() override { return Type; }
+    CTask* Clone() override { return new CTaskComplexGoToCarDoorAndStandStill(m_pVehicle, f4, f14, f44, f18, f1C, f20, f24); } // 0x6498B0
+    bool MakeAbortable(CPed* ped, eAbortPriority priority, const CEvent* event) override;
+    CTask* CreateNextSubTask(CPed* ped) override;
+    CTask* CreateFirstSubTask(CPed* ped) override;
+    CTask* ControlSubTask(CPed* ped) override;
 
-    static constexpr auto Type = eTaskType::TASK_COMPLEX_GO_TO_CAR_DOOR_AND_STAND_STILL;
+    bool IsVehicleInRange(const CPed& ped);
+    CVector* ComputeRouteToDoor(const CPed & ped);
+    CTask* CreateSubTask(eTaskType taskType, const CPed* ped);
 
-    virtual CTask* Clone();
-    virtual eTaskType GetTaskType() { return Type; }
-    virtual bool MakeAbortable(CPed* ped, int32 priority, CEvent const* event);
-    virtual CTask* CreateNextSubTask(CPed* ped);
-    virtual CTask* CreateFirstSubTask(CPed* ped);
-    virtual CTask* ControlSubTask(CPed* ped);
-
-private: // Wrappers for hooks
-    // 0x645780
-    CTaskComplexGoToCarDoorAndStandStill* Constructor(CVehicle* vehicle, int32 a3, bool a4, int32 a5, float a6, float a7, float a8, int32 a9) {
-        this->CTaskComplexGoToCarDoorAndStandStill::CTaskComplexGoToCarDoorAndStandStill(vehicle, a3, a4, a5, a6, a7, a8, a9);
-        return this;
-    }
-
-    // 0x64A580
-    CTaskComplexGoToCarDoorAndStandStill* Destructor() {
-        this->CTaskComplexGoToCarDoorAndStandStill::~CTaskComplexGoToCarDoorAndStandStill();
-        return this;
-    }
+private:
+    friend void InjectHooksMain();
+    static void InjectHooks();
+    CTaskComplexGoToCarDoorAndStandStill* Constructor(CVehicle* vehicle, int32 a3, bool a4, int32 a5, float a6, float a7, float a8, int32 a9) { this->CTaskComplexGoToCarDoorAndStandStill::CTaskComplexGoToCarDoorAndStandStill(vehicle, a3, a4, a5, a6, a7, a8, a9); return this; }
+    CTaskComplexGoToCarDoorAndStandStill* Destructor() { this->CTaskComplexGoToCarDoorAndStandStill::~CTaskComplexGoToCarDoorAndStandStill(); return this; }
 };
 VALIDATE_SIZE(CTaskComplexGoToCarDoorAndStandStill, 0x54);
