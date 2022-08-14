a23156ec83d365b0f4521ad5119e0468ae71b5c8
Izzotop <1680033+Izzotop@users.noreply.github.com>
2022-08-14 23:38:47 +0300

    CTaskComplexGoToAttractor 1

--- source/game_sa/Tasks/TaskTypes/TaskComplexGoToAttractor.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexGoToAttractor.cpp
@@ -1,5 +1,7 @@
 #include "StdInc.h"
+
 #include "TaskComplexGoToAttractor.h"
+#include "PedAtmAttractor.h"
 
 void CTaskComplexGoToAttractor::InjectHooks() {
     RH_ScopedClass(CTaskComplexGoToAttractor);
@@ -7,34 +9,33 @@ void CTaskComplexGoToAttractor::InjectHooks() {
 
     RH_ScopedInstall(Constructor, 0x66B640);
     RH_ScopedInstall(Destructor, 0x66B6A0);
-
-    // RH_ScopedInstall(Clone, 0x66D130);
-    RH_ScopedInstall(GetTaskType, 0x66B690);
-    // RH_ScopedInstall(MakeAbortable, 0x66B6B0);
-    // RH_ScopedInstall(CreateNextSubTask, 0x66B6C0);
-    // RH_ScopedInstall(CreateFirstSubTask, 0x670420);
-    // RH_ScopedInstall(ControlSubTask, 0x66B710);
+    // RH_ScopedInstall(Clone_Reversed, 0x66D130);
+    // RH_ScopedInstall(CreateNextSubTask_Reversed, 0x66B6C0);
+    // RH_ScopedInstall(CreateFirstSubTask_Reversed, 0x670420);
 }
 
 // 0x66B640
-CTaskComplexGoToAttractor::CTaskComplexGoToAttractor(CPedAttractor* attractor, CVector const& attrPosn, float attrHeading, float attrTime, int32 queueNumber, int32 a7) {}
-
-// 0x66B6A0
-CTaskComplexGoToAttractor::~CTaskComplexGoToAttractor() {}
-
-// 0x66D130
-CTask* CTaskComplexGoToAttractor::Clone() {
-    return plugin::CallMethodAndReturn<CTask*, 0x66D130, CTaskComplexGoToAttractor*>(this);
+CTaskComplexGoToAttractor::CTaskComplexGoToAttractor(CPedAttractor* attractor, const CVector& pos, float heading, float attrTime, int32 queueNumber, int32 a7) : CTaskComplex() {
+    m_Attractor = attractor;
+    m_vecAttrPosn = pos;
+    m_fAttrHeading = heading;
+    field_28 = a7;
+    m_fAttrTime = attrTime;
+    m_nQueueNumber = queueNumber;
 }
 
 // 0x66B6B0
-bool CTaskComplexGoToAttractor::MakeAbortable(CPed* ped, int32 priority, CEvent const* event) {
-    return plugin::CallMethodAndReturn<bool, 0x66B6B0, CTaskComplexGoToAttractor*, CPed*, int32, CEvent const*>(this, ped, priority, event);
+bool CTaskComplexGoToAttractor::MakeAbortable(CPed* ped, eAbortPriority priority, const CEvent* event) {
+    return m_pSubTask->MakeAbortable(ped, priority, event);
 }
 
 // 0x66B6C0
 CTask* CTaskComplexGoToAttractor::CreateNextSubTask(CPed* ped) {
-    return plugin::CallMethodAndReturn<CTask*, 0x66B6C0, CTaskComplexGoToAttractor*, CPed*>(this, ped);
+    GetPedAttractorManager()->BroadcastArrival(ped, m_Attractor);
+    if (ped->bUseAttractorInstantly && m_Attractor->GetHeadOfQueue() != ped) {
+        ped->bUseAttractorInstantly = false;
+    }
+    return nullptr;
 }
 
 // 0x670420
@@ -44,5 +45,5 @@ CTask* CTaskComplexGoToAttractor::CreateFirstSubTask(CPed* ped) {
 
 // 0x66B710
 CTask* CTaskComplexGoToAttractor::ControlSubTask(CPed* ped) {
-    return plugin::CallMethodAndReturn<CTask*, 0x66B710, CTaskComplexGoToAttractor*, CPed*>(this, ped);
+    return m_pSubTask;
 }
--- source/game_sa/Tasks/TaskTypes/TaskComplexGoToAttractor.h
+++ source/game_sa/Tasks/TaskTypes/TaskComplexGoToAttractor.h
@@ -1,41 +1,33 @@
 #pragma once
+
 #include "TaskComplex.h"
 
 class CTaskComplexGoToAttractor : public CTaskComplex {
 public:
-    CPedAttractor* m_pAttractor = {}; // 0xC
-    CVector m_vecAttrPosn = {};       // 0x10
-    float m_fAttrHeading = {};        // 0x1C
-    float m_fAttrTime = {};           // 0x20
-    int32 m_nQueueNumber = {};        // 0x24
-    int32 field_28 = {};              // 0x28
+    CPedAttractor* m_Attractor;
+    CVector        m_vecAttrPosn;
+    float          m_fAttrHeading;
+    float          m_fAttrTime;
+    int32          m_nQueueNumber;
+    int32          field_28;
 
 public:
+    static constexpr auto Type = TASK_COMPLEX_GO_TO_ATTRACTOR;
+
+    CTaskComplexGoToAttractor(CPedAttractor* attractor, CVector const& pos, float heading, float attrTime, int32 queueNumber, int32 a7);
+    ~CTaskComplexGoToAttractor() override = default; // 0x66B6A0
+
+    eTaskType GetTaskType() override { return Type;  }
+    CTask* Clone() override { return new CTaskComplexGoToAttractor(m_Attractor, m_vecAttrPosn, m_fAttrHeading, m_fAttrTime, m_nQueueNumber, field_28); } // 0x66D130
+    bool MakeAbortable(CPed* ped, eAbortPriority priority, const CEvent* event) override;
+    CTask* CreateNextSubTask(CPed* ped) override;
+    CTask* CreateFirstSubTask(CPed* ped) override;
+    CTask* ControlSubTask(CPed* ped) override;
+
+private:
+    friend void InjectHooksMain();
     static void InjectHooks();
-
-    ~CTaskComplexGoToAttractor();
-    CTaskComplexGoToAttractor(CPedAttractor* attractor, CVector const& attrPosn, float attrHeading, float attrTime, int32 queueNumber, int32 a7);
-
-    static constexpr auto Type = eTaskType::TASK_COMPLEX_GO_TO_ATTRACTOR;
-
-    virtual CTask* Clone();
-    virtual eTaskType GetTaskType() { return Type; }
-    virtual bool MakeAbortable(CPed* ped, int32 priority, CEvent const* event);
-    virtual CTask* CreateNextSubTask(CPed* ped);
-    virtual CTask* CreateFirstSubTask(CPed* ped);
-    virtual CTask* ControlSubTask(CPed* ped);
-
-private: // Wrappers for hooks
-    // 0x66B640
-    CTaskComplexGoToAttractor* Constructor(CPedAttractor* attractor, CVector const& attrPosn, float attrHeading, float attrTime, int32 queueNumber, int32 a7) {
-        this->CTaskComplexGoToAttractor::CTaskComplexGoToAttractor(attractor, attrPosn, attrHeading, attrTime, queueNumber, a7);
-        return this;
-    }
-
-    // 0x66B6A0
-    CTaskComplexGoToAttractor* Destructor() {
-        this->CTaskComplexGoToAttractor::~CTaskComplexGoToAttractor();
-        return this;
-    }
+    CTaskComplexGoToAttractor* Constructor(CPedAttractor* attractor, CVector const& attrPosn, float attrHeading, float attrTime, int32 queueNumber, int32 a7) { this->CTaskComplexGoToAttractor::CTaskComplexGoToAttractor(attractor, attrPosn, attrHeading, attrTime, queueNumber, a7); return this; }
+    CTaskComplexGoToAttractor* Destructor() { this->CTaskComplexGoToAttractor::~CTaskComplexGoToAttractor(); return this; }
 };
 VALIDATE_SIZE(CTaskComplexGoToAttractor, 0x2C);
