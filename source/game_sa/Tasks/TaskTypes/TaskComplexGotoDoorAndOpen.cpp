e7cfb6759866eb21d8a27b34c219d36db2f9678d
Izzotop <1680033+Izzotop@users.noreply.github.com>
2022-08-15 00:06:31 +0300

    CTaskComplexGotoDoorAndOpen 1

--- source/game_sa/Tasks/TaskTypes/TaskComplexGotoDoorAndOpen.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexGotoDoorAndOpen.cpp
@@ -1,4 +1,5 @@
 #include "StdInc.h"
+
 #include "TaskComplexGotoDoorAndOpen.h"
 
 void CTaskComplexGotoDoorAndOpen::InjectHooks() {
@@ -8,32 +9,55 @@ void CTaskComplexGotoDoorAndOpen::InjectHooks() {
     RH_ScopedOverloadedInstall(Constructor, "", 0x66BB20, CTaskComplexGotoDoorAndOpen*(CTaskComplexGotoDoorAndOpen::*)(CObject *));
     RH_ScopedOverloadedInstall(Constructor, "", 0x66BBA0, CTaskComplexGotoDoorAndOpen*(CTaskComplexGotoDoorAndOpen::*)(CVector const&, CVector const&));
     RH_ScopedInstall(Destructor, 0x66BC00);
-
     // RH_ScopedInstall(Clone, 0x66BCA0);
-    RH_ScopedInstall(GetTaskType, 0x66BB90);
-    // RH_ScopedInstall(MakeAbortable, 0x66BC80);
     // RH_ScopedInstall(CreateNextSubTask, 0x66C0D0);
     // RH_ScopedInstall(CreateFirstSubTask, 0x66BD40);
     // RH_ScopedInstall(ControlSubTask, 0x66C1F0);
 }
 
 // 0x66BB20
-CTaskComplexGotoDoorAndOpen::CTaskComplexGotoDoorAndOpen(CObject* door) {}
+CTaskComplexGotoDoorAndOpen::CTaskComplexGotoDoorAndOpen(CObject* door) : CTaskComplex() {
+    m_Object = door;
+    m_nStartTime = 0;
+    m_nOffsetTime = 0;
+    byte30 = 0;
+    byte31 = 0;
+    m_nFlags = m_nFlags & 0xF0 | 1; // todo: flags
+    CEntity::SafeRegisterRef(m_Object);
+}
 
 // 0x66BBA0
-CTaskComplexGotoDoorAndOpen::CTaskComplexGotoDoorAndOpen(CVector const& start, CVector const& end) {}
+CTaskComplexGotoDoorAndOpen::CTaskComplexGotoDoorAndOpen(const CVector& start, const CVector& end) : CTaskComplex() {
+    m_Object = nullptr;
+    m_Start = start;
+    m_End = end;
+    m_nStartTime = 0;
+    m_nOffsetTime = 0;
+    byte30 = 0;
+    byte31 = 0;
+    m_nFlags &= 244u; // todo: flags
+}
 
 // 0x66BC00
-CTaskComplexGotoDoorAndOpen::~CTaskComplexGotoDoorAndOpen() {}
+CTaskComplexGotoDoorAndOpen::~CTaskComplexGotoDoorAndOpen() {
+    CEntity::SafeCleanUpRef(m_Object);
+    if ((m_nFlags & 8) != 0) { // transition finished
+        CPad::GetPad()->bPlayerOnInteriorTransition = false;
+    }
+}
 
 // 0x66BCA0
 CTask* CTaskComplexGotoDoorAndOpen::Clone() {
-    return plugin::CallMethodAndReturn<CTask*, 0x66BCA0, CTaskComplexGotoDoorAndOpen*>(this);
+    if ((this->m_nFlags & 1) != 0) { // todo: flags
+        return new CTaskComplexGotoDoorAndOpen(m_Object);
+    } else {
+        return new CTaskComplexGotoDoorAndOpen(m_Start, m_End);
+    }
 }
 
 // 0x66BC80
-bool CTaskComplexGotoDoorAndOpen::MakeAbortable(CPed* ped, int32 priority, CEvent const* event) {
-    return plugin::CallMethodAndReturn<bool, 0x66BC80, CTaskComplexGotoDoorAndOpen*, CPed*, int32, CEvent const*>(this, ped, priority, event);
+bool CTaskComplexGotoDoorAndOpen::MakeAbortable(CPed* ped, eAbortPriority priority, const CEvent* event) {
+    return priority == ABORT_PRIORITY_IMMEDIATE && m_pSubTask->MakeAbortable(ped, ABORT_PRIORITY_IMMEDIATE, event);
 }
 
 // 0x66C0D0
--- source/game_sa/Tasks/TaskTypes/TaskComplexGotoDoorAndOpen.h
+++ source/game_sa/Tasks/TaskTypes/TaskComplexGotoDoorAndOpen.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "TaskComplex.h"
 #include "Vector.h"
 
@@ -7,48 +8,35 @@ class CObject;
 
 class CTaskComplexGotoDoorAndOpen : public CTaskComplex {
 public:
-    CEntity* m_pEntity = {};       // 0xC
-    CVector m_vecStart = {};       // 0x10
-    CVector m_vecEnd = {};         // 0x1C
-    uint32 m_nStartTime = {};      // 0x28
-    uint32 m_nOffsetTime = {};     // 0x2C
-    uint8 byte30 = {};             // 0x30
-    uint8 byte31 = {};             // 0x31
-    uint8 gap32[2] = {};           // 0x32
-    uint8 m_nFlags = {};           // 0x34
+    CObject* m_Object;
+    CVector  m_Start;
+    CVector  m_End;
+    uint32   m_nStartTime;
+    uint32   m_nOffsetTime;
+    uint8    byte30;
+    uint8    byte31;
+    uint8    gap32[2];
+    uint8    m_nFlags;
 
 public:
+    static constexpr auto Type = TASK_COMPLEX_GOTO_DOOR_AND_OPEN;
+
+    explicit CTaskComplexGotoDoorAndOpen(CObject* door);
+    CTaskComplexGotoDoorAndOpen(const CVector& start, const CVector& end);
+    ~CTaskComplexGotoDoorAndOpen() override;
+
+    eTaskType GetTaskType() override{ return Type; } // 0x66BB90
+    CTask* Clone() override;
+    bool MakeAbortable(CPed* ped, eAbortPriority priority, const CEvent* event) override;
+    CTask* CreateNextSubTask(CPed* ped) override;
+    CTask* CreateFirstSubTask(CPed* ped) override;
+    CTask* ControlSubTask(CPed* ped) override;
+
+private:
+    friend void InjectHooksMain();
     static void InjectHooks();
-
-    CTaskComplexGotoDoorAndOpen(CObject* door);
-    CTaskComplexGotoDoorAndOpen(CVector const& start, CVector const& end);
-    ~CTaskComplexGotoDoorAndOpen();
-
-    static constexpr auto Type = eTaskType::TASK_COMPLEX_GOTO_DOOR_AND_OPEN;
-
-    virtual CTask* Clone();
-    virtual eTaskType GetTaskType() { return Type; }
-    virtual bool MakeAbortable(CPed* ped, int32 priority, CEvent const* event);
-    virtual CTask* CreateNextSubTask(CPed* ped);
-    virtual CTask* CreateFirstSubTask(CPed* ped);
-    virtual CTask* ControlSubTask(CPed* ped);
-
-private: // Wrappers for hooks
-    // 0x66BB20
-    CTaskComplexGotoDoorAndOpen* Constructor(CObject* door) {
-        this->CTaskComplexGotoDoorAndOpen::CTaskComplexGotoDoorAndOpen(door);
-        return this;
-    }
-    // 0x66BBA0
-    CTaskComplexGotoDoorAndOpen* Constructor(CVector const& start, CVector const& end) {
-        this->CTaskComplexGotoDoorAndOpen::CTaskComplexGotoDoorAndOpen(start, end);
-        return this;
-    }
-
-    // 0x66BC00
-    CTaskComplexGotoDoorAndOpen* Destructor() {
-        this->CTaskComplexGotoDoorAndOpen::~CTaskComplexGotoDoorAndOpen();
-        return this;
-    }
+    CTaskComplexGotoDoorAndOpen* Constructor(CObject* door) { this->CTaskComplexGotoDoorAndOpen::CTaskComplexGotoDoorAndOpen(door); return this; }
+    CTaskComplexGotoDoorAndOpen* Constructor(CVector const& start, CVector const& end) { this->CTaskComplexGotoDoorAndOpen::CTaskComplexGotoDoorAndOpen(start, end); return this; }
+    CTaskComplexGotoDoorAndOpen* Destructor() { this->CTaskComplexGotoDoorAndOpen::~CTaskComplexGotoDoorAndOpen(); return this; }
 };
 VALIDATE_SIZE(CTaskComplexGotoDoorAndOpen, 0x38);
