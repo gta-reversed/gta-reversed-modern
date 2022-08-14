8919a7efb34464c57fc9195e0a13848b9603cd23
Izzotop <1680033+Izzotop@users.noreply.github.com>
2022-08-15 01:23:51 +0300

    CTaskComplexHitResponse 1

--- source/game_sa/Tasks/TaskTypes/TaskComplexHitResponse.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexHitResponse.cpp
@@ -1,42 +1,36 @@
 #include "StdInc.h"
+
 #include "TaskComplexHitResponse.h"
+// #include "TaskSimpleHitFromFront.h"
+// #include "TaskSimpleHitFromLeft.h"
+// #include "TaskSimpleHitFromBack.h"
+// #include "TaskSimpleHitFromRight.h"
 
 void CTaskComplexHitResponse::InjectHooks() {
     RH_ScopedClass(CTaskComplexHitResponse);
     RH_ScopedCategory("Tasks/TaskTypes");
 
     RH_ScopedInstall(Constructor, 0x631D70);
-    RH_ScopedInstall(Destructor, 0x631DA0);
-
-    // RH_ScopedInstall(Clone, 0x6365E0);
-    RH_ScopedInstall(GetTaskType, 0x631D90);
     // RH_ScopedInstall(CreateNextSubTask, 0x631DB0);
     // RH_ScopedInstall(CreateFirstSubTask, 0x638040);
     // RH_ScopedInstall(ControlSubTask, 0x631DC0);
 }
 
 // 0x631D70
-CTaskComplexHitResponse::CTaskComplexHitResponse(int32 a1) {}
-
-// 0x631DA0
-CTaskComplexHitResponse::~CTaskComplexHitResponse() {}
-
-// 0x6365E0
-CTask* CTaskComplexHitResponse::Clone() {
-    return plugin::CallMethodAndReturn<CTask*, 0x6365E0, CTaskComplexHitResponse*>(this);
-}
-
-// 0x631DB0
-CTask* CTaskComplexHitResponse::CreateNextSubTask(CPed* ped) {
-    return plugin::CallMethodAndReturn<CTask*, 0x631DB0, CTaskComplexHitResponse*, CPed*>(this, ped);
+CTaskComplexHitResponse::CTaskComplexHitResponse(int32 direction) : CTaskComplex() {
+    m_Direction = direction;
 }
 
 // 0x638040
 CTask* CTaskComplexHitResponse::CreateFirstSubTask(CPed* ped) {
     return plugin::CallMethodAndReturn<CTask*, 0x638040, CTaskComplexHitResponse*, CPed*>(this, ped);
-}
-
-// 0x631DC0
-CTask* CTaskComplexHitResponse::ControlSubTask(CPed* ped) {
-    return plugin::CallMethodAndReturn<CTask*, 0x631DC0, CTaskComplexHitResponse*, CPed*>(this, ped);
+    /*
+    switch (m_Direction) {
+    case 0:  return new CTaskSimpleHitFromFront();
+    case 1:  return new CTaskSimpleHitFromLeft();
+    case 2:  return new CTaskSimpleHitFromBack();
+    case 3:  return new CTaskSimpleHitFromRight();
+    default: return nullptr;
+    }
+    */
 }
--- source/game_sa/Tasks/TaskTypes/TaskComplexHitResponse.h
+++ source/game_sa/Tasks/TaskTypes/TaskComplexHitResponse.h
@@ -1,31 +1,26 @@
 #pragma once
+
 #include "TaskComplex.h"
 
 class CTaskComplexHitResponse : public CTaskComplex {
 public:
+    int32 m_Direction;
+
+public:
+    static constexpr auto Type = TASK_COMPLEX_HIT_RESPONSE;
+
+    explicit CTaskComplexHitResponse(int32 direction);
+    ~CTaskComplexHitResponse() override = default; // 0x631DA0
+
+    eTaskType GetTaskType() override { return Type; } // 0x631D90
+    CTask* Clone() override { return new CTaskComplexHitResponse(m_Direction); } // 0x6365E0
+    CTask* CreateNextSubTask(CPed* ped) override { return nullptr; } // 0x631DB0
+    CTask* CreateFirstSubTask(CPed* ped) override;
+    CTask* ControlSubTask(CPed* ped) override { return m_pSubTask; } // 0x631DC0
+
+private:
+    friend void InjectHooksMain();
     static void InjectHooks();
-
-    ~CTaskComplexHitResponse();
-    CTaskComplexHitResponse(int32 a1);
-
-    static constexpr auto Type = eTaskType::TASK_COMPLEX_HIT_RESPONSE;
-
-    virtual CTask* Clone();
-    virtual eTaskType GetTaskType() { return Type; }
-    virtual CTask* CreateNextSubTask(CPed* ped);
-    virtual CTask* CreateFirstSubTask(CPed* ped);
-    virtual CTask* ControlSubTask(CPed* ped);
-
-private: // Wrappers for hooks
-    // 0x631D70
-    CTaskComplexHitResponse* Constructor(int32 a1) {
-        this->CTaskComplexHitResponse::CTaskComplexHitResponse(a1);
-        return this;
-    }
-
-    // 0x631DA0
-    CTaskComplexHitResponse* Destructor() {
-        this->CTaskComplexHitResponse::~CTaskComplexHitResponse();
-        return this;
-    }
+    CTaskComplexHitResponse* Constructor(int32 a1) { this->CTaskComplexHitResponse::CTaskComplexHitResponse(a1); return this; }
+    CTaskComplexHitResponse* Destructor() { this->CTaskComplexHitResponse::~CTaskComplexHitResponse(); return this; }
 };
