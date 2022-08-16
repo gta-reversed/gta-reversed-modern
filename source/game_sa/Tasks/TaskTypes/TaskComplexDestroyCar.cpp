82683afef3a7c76e5e1b9723879cfdc5eab6ff9e
Pirulax <patrikjankovics7@gmail.com>
2022-08-16 04:08:01 +0200

    Fix CTaskComplexDestroyCar

--- source/game_sa/Tasks/TaskTypes/TaskComplexDestroyCar.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexDestroyCar.cpp
@@ -1,5 +1,8 @@
 #include "StdInc.h"
 #include "TaskComplexDestroyCar.h"
+#include "TaskComplexDestroyCarArmed.h"
+#include "TaskComplexDestroyCarMelee.h"
+#include "TaskComplexLeaveCar.h"
 
 void CTaskComplexDestroyCar::InjectHooks() {
     RH_ScopedClass(CTaskComplexDestroyCar);
@@ -8,14 +11,14 @@ void CTaskComplexDestroyCar::InjectHooks() {
     RH_ScopedInstall(Constructor, 0x621C00);
     RH_ScopedInstall(Destructor, 0x621CB0);
 
-    // RH_ScopedInstall(CreateSubTask, 0x6287A0);
+    RH_ScopedInstall(CreateSubTask, 0x6287A0);
 
-    // RH_ScopedVirtualInstall(Clone, 0x623530);
-    // RH_ScopedVirtualInstall(GetTaskType, 0x621C70);
-    // RH_ScopedVirtualInstall(MakeAbortable, 0x621C80);
-    // RH_ScopedVirtualInstall(CreateNextSubTask, 0x62D9E0);
-    // RH_ScopedVirtualInstall(CreateFirstSubTask, 0x62DA90);
-    // RH_ScopedVirtualInstall(ControlSubTask, 0x6288C0);
+    RH_ScopedVirtualInstall2(Clone, 0x623530);
+    RH_ScopedVirtualInstall2(GetTaskType, 0x621C70);
+    RH_ScopedVirtualInstall2(MakeAbortable, 0x621C80);
+    RH_ScopedVirtualInstall2(CreateNextSubTask, 0x62D9E0);
+    RH_ScopedVirtualInstall2(CreateFirstSubTask, 0x62DA90);
+    RH_ScopedVirtualInstall2(ControlSubTask, 0x6288C0, { .enabled = false, .locked = true });
 }
 
 // 0x621C00
@@ -28,6 +31,11 @@ CTaskComplexDestroyCar::CTaskComplexDestroyCar(CVehicle* vehicleToDestroy, CVect
     CEntity::SafeRegisterRef(m_vehicleToDestroy);
 }
 
+CTaskComplexDestroyCar::CTaskComplexDestroyCar(const CTaskComplexDestroyCar& o) :
+    CTaskComplexDestroyCar{o.m_vehicleToDestroy, o.m_vehPosn}
+{
+}
+
 // 0x621CB0
 CTaskComplexDestroyCar::~CTaskComplexDestroyCar() {
     CEntity::SafeCleanUpRef(m_vehicleToDestroy);
@@ -35,27 +43,59 @@ CTaskComplexDestroyCar::~CTaskComplexDestroyCar() {
 
 // 0x6287A0
 CTask* CTaskComplexDestroyCar::CreateSubTask(eTaskType taskType, CPed* ped) {
-    return plugin::CallMethodAndReturn<CTask*, 0x6287A0, CTaskComplexDestroyCar*, eTaskType, CPed*>(this, taskType, ped);
-}
-
-// 0x623530
-CTask* CTaskComplexDestroyCar::Clone() {
-    return plugin::CallMethodAndReturn<CTask*, 0x623530, CTaskComplexDestroyCar*>(this);
+    switch (taskType) {
+    case TASK_COMPLEX_DESTROY_CAR_ARMED:
+        return new CTaskComplexDestroyCarArmed{ m_vehicleToDestroy, m_vehPosn };
+    case TASK_COMPLEX_LEAVE_CAR:
+        return new CTaskComplexLeaveCar{ ped->m_pVehicle, 0, 0, true, false };
+    case TASK_COMPLEX_DESTROY_CAR_MELEE:
+        return new CTaskComplexDestroyCarMelee{ m_vehicleToDestroy };
+    default:
+        return nullptr;
+    }
 }
 
 // 0x621C80
 bool CTaskComplexDestroyCar::MakeAbortable(CPed* ped, eAbortPriority priority, CEvent const* event) {
-    return plugin::CallMethodAndReturn<bool, 0x621C80, CTaskComplexDestroyCar*, CPed*, eAbortPriority, CEvent const*>(this, ped, priority, event);
+    switch (priority) {
+    case ABORT_PRIORITY_URGENT:
+    case ABORT_PRIORITY_IMMEDIATE:
+        return m_pSubTask->MakeAbortable(ped, priority, event);
+    default:
+        return false;
+    }
 }
 
 // 0x62D9E0
 CTask* CTaskComplexDestroyCar::CreateNextSubTask(CPed* ped) {
-    return plugin::CallMethodAndReturn<CTask*, 0x62D9E0, CTaskComplexDestroyCar*, CPed*>(this, ped);
+    switch (m_pSubTask->GetTaskType()) {
+    case TASK_SIMPLE_BE_HIT:
+    case TASK_COMPLEX_DESTROY_CAR_ARMED:
+    case TASK_COMPLEX_DESTROY_CAR_MELEE:
+    case TASK_COMPLEX_FALL_AND_GET_UP:
+        return CreateSubTask(TASK_FINISHED, ped);
+    case TASK_COMPLEX_LEAVE_CAR:
+    case TASK_COMPLEX_DRAG_PED_FROM_CAR:
+        return CreateSubTask(ped->GetActiveWeapon().IsTypeMelee() ? TASK_COMPLEX_DESTROY_CAR_MELEE : TASK_COMPLEX_DESTROY_CAR_ARMED, ped);
+    default:
+        return nullptr;
+    }
 }
 
 // 0x62DA90
 CTask* CTaskComplexDestroyCar::CreateFirstSubTask(CPed* ped) {
-    return plugin::CallMethodAndReturn<CTask*, 0x62DA90, CTaskComplexDestroyCar*, CPed*>(this, ped);
+    m_arg0 = false;
+
+    if (!m_vehicleToDestroy) {
+        return CreateSubTask(TASK_FINISHED, nullptr);
+    }
+    if (ped->m_pVehicle && ped->bInVehicle) {
+        return CreateSubTask(TASK_COMPLEX_LEAVE_CAR, ped);
+    }
+    if (ped->GetActiveWeapon().IsTypeMelee()) {
+        return CreateSubTask(TASK_COMPLEX_DESTROY_CAR_MELEE, nullptr);
+    }
+    return CreateSubTask(TASK_COMPLEX_DESTROY_CAR_ARMED, nullptr);
 }
 
 // 0x6288C0
--- source/game_sa/Tasks/TaskTypes/TaskComplexDestroyCar.h
+++ source/game_sa/Tasks/TaskTypes/TaskComplexDestroyCar.h
@@ -7,23 +7,24 @@ class CVehicle;
 
 class CTaskComplexDestroyCar : public CTaskComplex {
 public:
-    bool m_arg0 = {};                  // 0xC
-    bool m_arg1 = {};                  // 0xD
-    CVehicle* m_vehicleToDestroy = {}; // 0x10
-    CVector m_vehPosn = {};             // 0x14
+    bool m_arg0 = {};
+    bool m_arg1 = {};
+    CVehicle* m_vehicleToDestroy = {};
+    CVector m_vehPosn = {};
 
 public:
     static void InjectHooks();
 
-    ~CTaskComplexDestroyCar();
-    CTaskComplexDestroyCar(CVehicle* vehicleToDestroy, CVector vehPosn);
-
     static constexpr auto Type = eTaskType::TASK_COMPLEX_DESTROY_CAR;
+
+    CTaskComplexDestroyCar(CVehicle* vehicleToDestroy, CVector vehPosn);
+    CTaskComplexDestroyCar(const CTaskComplexDestroyCar&);
+    ~CTaskComplexDestroyCar() override;
 
     CTask* CreateSubTask(eTaskType taskType, CPed* ped);
 
-    CTask* Clone() override;
     eTaskType GetTaskType() override { return Type; }
+    CTask* Clone() override { return new CTaskComplexDestroyCar{ *this }; }
     bool MakeAbortable(CPed* ped, eAbortPriority priority, CEvent const* event) override;
     CTask* CreateNextSubTask(CPed* ped) override;
     CTask* CreateFirstSubTask(CPed* ped) override;
@@ -31,15 +32,9 @@ public:
 
 private: // Wrappers for hooks
     // 0x621C00
-    CTaskComplexDestroyCar* Constructor(CVehicle* vehicleToDestroy, CVector vehPosn) {
-        this->CTaskComplexDestroyCar::CTaskComplexDestroyCar(vehicleToDestroy, vehPosn);
-        return this;
-    }
+    CTaskComplexDestroyCar* Constructor(CVehicle* vehicleToDestroy, CVector vehPosn) { this->CTaskComplexDestroyCar::CTaskComplexDestroyCar(vehicleToDestroy, vehPosn); return this; }
 
     // 0x621CB0
-    CTaskComplexDestroyCar* Destructor() {
-        this->CTaskComplexDestroyCar::~CTaskComplexDestroyCar();
-        return this;
-    }
+    CTaskComplexDestroyCar* Destructor() { this->CTaskComplexDestroyCar::~CTaskComplexDestroyCar(); return this; }
 };
 VALIDATE_SIZE(CTaskComplexDestroyCar, 0x20);
