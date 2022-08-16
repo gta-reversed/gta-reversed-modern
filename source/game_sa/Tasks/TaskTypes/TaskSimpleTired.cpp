a8f00c9093cce728358e67b638cb9f8380dd5e48
Izzotop <1680033+Izzotop@users.noreply.github.com>
2022-08-17 01:45:41 +0300

    Fix ProcessPed

--- source/game_sa/Tasks/TaskTypes/TaskSimpleTired.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskSimpleTired.cpp
@@ -1,4 +1,5 @@
 #include "StdInc.h"
+
 #include "TaskSimpleTired.h"
 
 void CTaskSimpleTired::InjectHooks() {
@@ -7,34 +8,29 @@ void CTaskSimpleTired::InjectHooks() {
 
     RH_ScopedInstall(Constructor, 0x630F20);
     RH_ScopedInstall(Destructor, 0x630F60);
-
     RH_ScopedInstall(StartAnim, 0x630F70);
-
-    RH_ScopedVirtualInstall2(Clone, 0x636180);
-    RH_ScopedVirtualInstall2(GetTaskType, 0x630F50);
-    RH_ScopedVirtualInstall2(MakeAbortable, 0x630FF0);
-    RH_ScopedVirtualInstall2(ProcessPed, 0x637970);
 }
 
 // 0x630F20
 CTaskSimpleTired::CTaskSimpleTired(uint32 tiredDurationMs) :
-    m_tiredDurationMs{ tiredDurationMs }
+    CTaskSimple(),
+    m_TiredDurationMs{ tiredDurationMs }
 {
-}
-
-CTaskSimpleTired::CTaskSimpleTired(const CTaskSimpleTired& o) :
-    CTaskSimpleTired{o.m_tiredDurationMs}
-{
+    m_AnimStartMs = 0;
+    m_TiredDurationMs_Copy = 0;
+    m_AnimHasStarted = false;
+    m_NeedsToStartAnim = false;
+    m_TiredAnim = nullptr;
 }
 
 // 0x630F70
 void CTaskSimpleTired::StartAnim(CPed* ped) {
     ped->SetMoveState(PEDMOVE_STILL);
     ped->SetMoveAnim();
-    m_animStartMs = CTimer::GetTimeInMS();
-    m_tiredDurationCpy = m_tiredDurationMs;
-    m_animHasStarted = true;
-    m_tiredAnim = CAnimManager::BlendAnimation(
+    m_AnimStartMs = CTimer::GetTimeInMS();
+    m_TiredDurationMs_Copy = m_TiredDurationMs;
+    m_AnimHasStarted = true;
+    m_TiredAnim = CAnimManager::BlendAnimation(
         ped->m_pRwClump,
         FindPlayerPed() == ped && CClothes::GetDefaultPlayerMotionGroup() == ANIM_GROUP_FAT ? ANIM_GROUP_FAT_TIRED : ANIM_GROUP_DEFAULT,
         ANIM_ID_IDLE_TIRED,
@@ -43,33 +39,32 @@ void CTaskSimpleTired::StartAnim(CPed* ped) {
 }
 
 // 0x630FF0
-bool CTaskSimpleTired::MakeAbortable(CPed* ped, eAbortPriority priority, CEvent const* event) {
-    if (m_tiredAnim && m_tiredAnim == RpAnimBlendClumpGetAssociation(ped->m_pRwClump, ANIM_ID_IDLE_TIRED)) {
-        m_tiredAnim->m_fBlendDelta = -4.f;
+bool CTaskSimpleTired::MakeAbortable(CPed* ped, eAbortPriority priority, const CEvent* event) {
+    if (m_TiredAnim && m_TiredAnim == RpAnimBlendClumpGetAssociation(ped->m_pRwClump, ANIM_ID_IDLE_TIRED)) {
+        m_TiredAnim->m_fBlendDelta = -4.f;
     }
     return true;
 }
 
 // 0x637970
 bool CTaskSimpleTired::ProcessPed(CPed* ped) {
-    if (!m_tiredAnim) {
+    if (!m_TiredAnim) {
         StartAnim(ped);
     }
 
-    if (!m_animHasStarted) {
+    if (!m_AnimHasStarted) {
         return false;
     }
 
-    if (m_needsToStartAnim) {
-        m_animStartMs = CTimer::GetTimeInMS();
-        m_needsToStartAnim = true;
+    if (m_NeedsToStartAnim) {
+        m_AnimStartMs = CTimer::GetTimeInMS();
+        m_NeedsToStartAnim = false;
     }
 
-    if (CTimer::GetTimeInMS() < m_animStartMs + m_tiredDurationCpy) {
+    if (CTimer::GetTimeInMS() < m_AnimStartMs + m_TiredDurationMs_Copy) {
         return false;
     }
 
     MakeAbortable(ped, ABORT_PRIORITY_LEISURE, nullptr);
-
     return true;
 }
--- source/game_sa/Tasks/TaskTypes/TaskSimpleTired.h
+++ source/game_sa/Tasks/TaskTypes/TaskSimpleTired.h
@@ -7,40 +7,28 @@ class CAnimBlendAssociation;
 
 class CTaskSimpleTired : public CTaskSimple {
 public:
-    uint32 m_tiredDurationMs = {};
-    uint32 m_animStartMs = {};
-    uint32 m_tiredDurationCpy = {};
-    bool  m_animHasStarted = {};
-    bool  m_needsToStartAnim = {};
-    CAnimBlendAssociation* m_tiredAnim = {};
+    uint32 m_TiredDurationMs;
+    uint32 m_AnimStartMs;
+    uint32 m_TiredDurationMs_Copy;
+    bool   m_AnimHasStarted;
+    bool   m_NeedsToStartAnim;
+    CAnimBlendAssociation* m_TiredAnim;
 
 public:
-    static void InjectHooks();
+    constexpr static auto Type = TASK_SIMPLE_TIRED;
 
-    constexpr static auto Type = eTaskType::TASK_SIMPLE_TIRED;
-
-    CTaskSimpleTired(uint32 tiredDurationMs);
-    CTaskSimpleTired(const CTaskSimpleTired&);
+    explicit CTaskSimpleTired(uint32 tiredDurationMs);
     ~CTaskSimpleTired() override = default;
 
+    eTaskType GetTaskType() override { return Type; } // 0x630F50
+    CTask* Clone() override { return new CTaskSimpleTired(m_TiredDurationMs); } // 0x636180
+    bool MakeAbortable(CPed* ped, eAbortPriority priority, const CEvent* event) override;
+
+    bool ProcessPed(CPed* ped) override;
     void StartAnim(CPed* ped);
 
-    eTaskType GetTaskType() override { return Type; }
-    CTask* Clone() override { return new CTaskSimpleTired{ *this }; }
-    bool MakeAbortable(CPed* ped, eAbortPriority priority, CEvent const* event) override;
-    bool ProcessPed(CPed* ped) override;
-
-private: // Wrappers for hooks
-    // 0x630F20
-    CTaskSimpleTired* Constructor(int32 tiredDurationMs) {
-        this->CTaskSimpleTired::CTaskSimpleTired(tiredDurationMs);
-        return this;
-    }
-
-    // 0x630F60
-    CTaskSimpleTired* Destructor() {
-        this->CTaskSimpleTired::~CTaskSimpleTired();
-        return this;
-    }
+    static void InjectHooks();
+    CTaskSimpleTired* Constructor(int32 tiredDurationMs) { this->CTaskSimpleTired::CTaskSimpleTired(tiredDurationMs); return this; }
+    CTaskSimpleTired* Destructor() { this->CTaskSimpleTired::~CTaskSimpleTired(); return this; }
 };
-//VALIDATE_SIZE(CTaskSimpleTired, 0x1C);
+VALIDATE_SIZE(CTaskSimpleTired, 0x1C);
