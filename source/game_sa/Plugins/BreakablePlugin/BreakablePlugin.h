cee0603ade32fec1d0d82dd55b7cbb814b474f2f
Izzotop <1680033+Izzotop@users.noreply.github.com>
2022-08-17 17:45:36 +0300

    Refactor

--- source/game_sa/Plugins/BreakablePlugin/BreakablePlugin.h
+++ source/game_sa/Plugins/BreakablePlugin/BreakablePlugin.h
@@ -71,7 +71,7 @@ struct BreakablePluginData {
     // RwTexture*          m_aTexturePointers[m_usNumMaterial];
 
 // NOTSA: Helper functions
-    static uint32 GetRequiredAllocationSize(const BreakInfo_t& baseInfo) {
+    static size_t GetRequiredAllocationSize(const BreakInfo_t& baseInfo) {
         auto size = sizeof(BreakInfo_t);
         size += sizeof(RwV3d) * baseInfo.m_usNumVertices;
         size += sizeof(RwTexCoords) * baseInfo.m_usNumVertices;
@@ -82,91 +82,33 @@ struct BreakablePluginData {
         size += sizeof(char[32]) * baseInfo.m_usNumMaterials;
         size += sizeof(BreakInfoColor) * baseInfo.m_usNumMaterials;
         size += sizeof(RwTexture*) * baseInfo.m_usNumMaterials;
-
         return size;
     };
 
-// UGLY METHODS BELOW
-    inline RwV3d* GetVertexPosPtr() {
-        auto byteOffset = sizeof(BreakInfo_t);
-        return reinterpret_cast<RwV3d*>(&reinterpret_cast<uint8*>(this)[byteOffset]);
-    };
+    auto GetVertexPosOffset()       { return sizeof(BreakInfo_t); };
+    auto GetTexCoordsOffset()       { return GetVertexPosOffset()       + sizeof(RwV3d)             * m_Info.m_usNumVertices; };
+    auto GetVertColorsOffset()      { return GetTexCoordsOffset()       + sizeof(RwTexCoords)       * m_Info.m_usNumVertices; };
+    auto GetTrianglesOffset()       { return GetVertColorsOffset()      + sizeof(RwRGBA)            * m_Info.m_usNumVertices; };
+    auto GetMaterialIndicesOffset() { return GetTrianglesOffset()       + sizeof(BreakInfoTriangle) * m_Info.m_usNumTriangles; };
+    auto GetTextureNamesOffset()    { return GetMaterialIndicesOffset() + sizeof(uint16)            * m_Info.m_usNumTriangles; };
+    auto GetMaskNamesOffset()       { return GetTextureNamesOffset()    + sizeof(char[32])          * m_Info.m_usNumMaterials; };
+    auto GetSurfacePropsOffset()    { return GetMaskNamesOffset()       + sizeof(char[32])          * m_Info.m_usNumMaterials; };
+    auto GetTexturesOffset()        { return GetSurfacePropsOffset()    + sizeof(BreakInfoColor)    * m_Info.m_usNumMaterials; };
 
-    inline RwTexCoords* GetTexCoordsPtr() {
-        auto byteOffset = sizeof(BreakInfo_t);
-        byteOffset += sizeof(RwV3d) * m_Info.m_usNumVertices;
-        return reinterpret_cast<RwTexCoords*>(&reinterpret_cast<uint8*>(this)[byteOffset]);
-    };
+    template <typename T>
+    auto As(auto byteOffset) {
+        return reinterpret_cast<T>(&reinterpret_cast<uint8*>(this)[byteOffset]);
+    }
 
-    inline RwRGBA* GetVertColorsPtr() {
-        auto byteOffset = sizeof(BreakInfo_t);
-        byteOffset += sizeof(RwV3d) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(RwTexCoords) * m_Info.m_usNumVertices;
-        return reinterpret_cast<RwRGBA*>(&reinterpret_cast<uint8*>(this)[byteOffset]);
-    };
-
-    inline BreakInfoTriangle* GetTrianglesPtr() {
-        auto byteOffset = sizeof(BreakInfo_t);
-        byteOffset += sizeof(RwV3d) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(RwTexCoords) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(RwRGBA) * m_Info.m_usNumVertices;
-        return reinterpret_cast<BreakInfoTriangle*>(&reinterpret_cast<uint8*>(this)[byteOffset]);
-    };
-
-    inline uint16* GetMaterialIndicesPtr() {
-        auto byteOffset = sizeof(BreakInfo_t);
-        byteOffset += sizeof(RwV3d) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(RwTexCoords) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(RwRGBA) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(BreakInfoTriangle) * m_Info.m_usNumTriangles;
-        return reinterpret_cast<uint16*>(&reinterpret_cast<uint8*>(this)[byteOffset]);
-    };
-
-    inline char* GetTextureNamesPtr() {
-        auto byteOffset = sizeof(BreakInfo_t);
-        byteOffset += sizeof(RwV3d) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(RwTexCoords) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(RwRGBA) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(BreakInfoTriangle) * m_Info.m_usNumTriangles;
-        byteOffset += sizeof(uint16) * m_Info.m_usNumTriangles;
-        return reinterpret_cast<char*>(&reinterpret_cast<uint8*>(this)[byteOffset]);
-    };
-
-    inline char* GetMaskNamesPtr() {
-        auto byteOffset = sizeof(BreakInfo_t);
-        byteOffset += sizeof(RwV3d) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(RwTexCoords) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(RwRGBA) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(BreakInfoTriangle) * m_Info.m_usNumTriangles;
-        byteOffset += sizeof(uint16) * m_Info.m_usNumTriangles;
-        byteOffset += sizeof(char[32]) * m_Info.m_usNumMaterials;
-        return reinterpret_cast<char*>(&reinterpret_cast<uint8*>(this)[byteOffset]);
-    };
-
-    inline BreakInfoColor* GetSurfacePropsPtr() {
-        auto byteOffset = sizeof(BreakInfo_t);
-        byteOffset += sizeof(RwV3d) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(RwTexCoords) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(RwRGBA) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(BreakInfoTriangle) * m_Info.m_usNumTriangles;
-        byteOffset += sizeof(uint16) * m_Info.m_usNumTriangles;
-        byteOffset += sizeof(char[32]) * m_Info.m_usNumMaterials;
-        byteOffset += sizeof(char[32]) * m_Info.m_usNumMaterials;
-        return reinterpret_cast<BreakInfoColor*>(&reinterpret_cast<uint8*>(this)[byteOffset]);
-    };
-
-    inline RwTexture** GetTexturesPtr() {
-        auto byteOffset = sizeof(BreakInfo_t);
-        byteOffset += sizeof(RwV3d) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(RwTexCoords) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(RwRGBA) * m_Info.m_usNumVertices;
-        byteOffset += sizeof(BreakInfoTriangle) * m_Info.m_usNumTriangles;
-        byteOffset += sizeof(uint16) * m_Info.m_usNumTriangles;
-        byteOffset += sizeof(char[32]) * m_Info.m_usNumMaterials;
-        byteOffset += sizeof(char[32]) * m_Info.m_usNumMaterials;
-        byteOffset += sizeof(BreakInfoColor) * m_Info.m_usNumMaterials;
-        return reinterpret_cast<RwTexture**>(&reinterpret_cast<uint8*>(this)[byteOffset]);
-    };
+    auto GetVertexPosPtr()       { return As<RwV3d*>(GetVertexPosOffset()); }
+    auto GetTexCoordsPtr()       { return As<RwTexCoords*>(GetTexCoordsOffset()); }
+    auto GetVertColorsPtr()      { return As<RwRGBA*>(GetVertColorsOffset()); }
+    auto GetTrianglesPtr()       { return As<BreakInfoTriangle*>(GetTrianglesOffset()); }
+    auto GetMaterialIndicesPtr() { return As<uint16*>(GetMaterialIndicesOffset()); }
+    auto GetTextureNamesPtr()    { return As<char*>(GetTextureNamesOffset()); }
+    auto GetMaskNamesPtr()       { return As<char*>(GetMaskNamesOffset()); }
+    auto GetSurfacePropsPtr()    { return As<BreakInfoColor*>(GetSurfacePropsOffset()); }
+    auto GetTexturesPtr()        { return As<RwTexture**>(GetTexturesOffset()); }
 };
 
 #define BREAKABLEPLG(geometry, var) \
