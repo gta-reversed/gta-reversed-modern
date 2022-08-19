212a05f7947489e84a667ec54f5f3038854ec77e
Pirulax <patrikjankovics7@gmail.com>
2022-08-19 23:08:18 +0200

    Interior/FurnitureSubGroup_c.cpp

--- source/game_sa/Interior/FurnitureSubGroup_c.cpp
+++ source/game_sa/Interior/FurnitureSubGroup_c.cpp
@@ -5,9 +5,9 @@ void FurnitureSubGroup_c::InjectHooks() {
     RH_ScopedClass(FurnitureSubGroup_c);
     RH_ScopedCategory("Interior");
 
-    // RH_ScopedInstall(GetFurniture, 0x590EE0);
-    // RH_ScopedInstall(GetRandomId, 0x590FD0);
-    // RH_ScopedInstall(AddFurniture, 0x5C00C0);
+    RH_ScopedInstall(GetFurniture, 0x590EE0, { .reversed = false });
+    RH_ScopedInstall(GetRandomId, 0x590FD0, { .reversed = false });
+    RH_ScopedInstall(AddFurniture, 0x5C00C0, { .reversed = false });
 }
 
 // 0x5C00C0
