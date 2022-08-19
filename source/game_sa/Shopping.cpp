393b133b968e684991f2171f444c014411ad589a
yukani <yukani@zohomail.eu>
2022-08-20 02:22:36 +0300

    use unreversed opt

--- source/game_sa/Shopping.cpp
+++ source/game_sa/Shopping.cpp
@@ -18,39 +18,39 @@ void CShopping::InjectHooks() {
     RH_ScopedClass(CShopping);
     RH_ScopedCategoryGlobal();
 
-    //RH_ScopedInstall(Init, 0x49C290);
-    //RH_ScopedInstall(AddPriceModifier, 0x0);
-    //RH_ScopedInstall(AddPriceModifier, 0x0);
-    //RH_ScopedInstall(Buy, 0x49BF70);
-    //RH_ScopedInstall(FindItem, 0x0);
-    //RH_ScopedInstall(FindSection, 0x49AE70);
-    //RH_ScopedInstall(FindSectionInSection, 0x49AF90);
-    //RH_ScopedInstall(GetExtraInfo, 0x49ADE0);
-    //RH_ScopedInstall(GetItemIndex, 0x49AB10);
-    //RH_ScopedInstall(GetKey, 0x49AB30);
-    //RH_ScopedInstall(GetNameTag, 0x0);
-    //RH_ScopedInstall(GetNextSection, 0x49AF10);
-    //RH_ScopedInstall(GetPrice, 0x49AD50);
-    //RH_ScopedInstall(GetPriceSectionFromName, 0x49AAD0);
-    //RH_ScopedInstall(HasPlayerBought, 0x49B5E0);
-    //RH_ScopedInstall(IncrementStat, 0x0);
-    //RH_ScopedInstall(IncrementStat2, 0x0);
-    //RH_ScopedInstall(LoadPrices, 0x49B8D0);
-    //RH_ScopedInstall(LoadShop, 0x49BBE0);
-    //RH_ScopedInstall(LoadStats, 0x49B6A0);
-    //RH_ScopedInstall(RemoveLoadedPrices, 0x0);
-    //RH_ScopedInstall(RemoveLoadedShop, 0x49AE30);
-    //RH_ScopedInstall(RemovePriceModifier, 0x0);
-    //RH_ScopedInstall(RemovePriceModifier, 0x0);
-    //RH_ScopedInstall(RestoreClothesState, 0x49B240);
-    //RH_ScopedInstall(RestoreVehicleMods, 0x49B3C0);
-    //RH_ScopedInstall(SetPlayerHasBought, 0x49B610);
-    //RH_ScopedInstall(ShutdownForRestart, 0x49B640);
-    //RH_ScopedInstall(StoreClothesState, 0x49B200);
-    //RH_ScopedInstall(StoreVehicleMods, 0x0);
-    //RH_ScopedInstall(UpdateStats, 0x0);
-    //RH_ScopedInstall(Load, 0x5D3E40);
-    //RH_ScopedInstall(Save, 0x5D3DE0);
+    RH_ScopedInstall(Init, 0x49C290, {.reversed = false });
+    //RH_ScopedOverloadedInstall(AddPriceModifier, 0x0, { .reversed = false });
+    //RH_ScopedOverloadedInstall(AddPriceModifier, 0x0, { .reversed = false });
+    RH_ScopedInstall(Buy, 0x49BF70, { .reversed = false });
+    RH_ScopedInstall(FindItem, 0x0, { .reversed = false });
+    RH_ScopedInstall(FindSection, 0x49AE70, { .reversed = false });
+    RH_ScopedInstall(FindSectionInSection, 0x49AF90, { .reversed = false });
+    RH_ScopedInstall(GetExtraInfo, 0x49ADE0, { .reversed = false });
+    RH_ScopedInstall(GetItemIndex, 0x49AB10, { .reversed = false });
+    RH_ScopedInstall(GetKey, 0x49AB30, { .reversed = false });
+    RH_ScopedInstall(GetNameTag, 0x0, { .reversed = false });
+    RH_ScopedInstall(GetNextSection, 0x49AF10, { .reversed = false });
+    RH_ScopedInstall(GetPrice, 0x49AD50, { .reversed = false });
+    RH_ScopedInstall(GetPriceSectionFromName, 0x49AAD0, { .reversed = false });
+    RH_ScopedInstall(HasPlayerBought, 0x49B5E0, { .reversed = false });
+    RH_ScopedInstall(IncrementStat, 0x0, { .reversed = false });
+    RH_ScopedInstall(IncrementStat2, 0x0, { .reversed = false });
+    RH_ScopedInstall(LoadPrices, 0x49B8D0, { .reversed = false });
+    RH_ScopedInstall(LoadShop, 0x49BBE0, { .reversed = false });
+    RH_ScopedInstall(LoadStats, 0x49B6A0, { .reversed = false });
+    RH_ScopedInstall(RemoveLoadedPrices, 0x0, { .reversed = false });
+    RH_ScopedInstall(RemoveLoadedShop, 0x49AE30, { .reversed = false });
+    //RH_ScopedInstall(RemovePriceModifier, 0x0, { .reversed = false });
+    //RH_ScopedInstall(RemovePriceModifier, 0x0, { .reversed = false });
+    RH_ScopedInstall(RestoreClothesState, 0x49B240, { .reversed = false });
+    RH_ScopedInstall(RestoreVehicleMods, 0x49B3C0, { .reversed = false });
+    RH_ScopedInstall(SetPlayerHasBought, 0x49B610, { .reversed = false });
+    RH_ScopedInstall(ShutdownForRestart, 0x49B640, { .reversed = false });
+    RH_ScopedInstall(StoreClothesState, 0x49B200, { .reversed = false });
+    RH_ScopedInstall(StoreVehicleMods, 0x0, { .reversed = false });
+    RH_ScopedInstall(UpdateStats, 0x0, { .reversed = false });
+    RH_ScopedInstall(Load, 0x5D3E40, { .reversed = false });
+    RH_ScopedInstall(Save, 0x5D3DE0, { .reversed = false });
 }
 
 // 0x49C290
@@ -183,8 +183,6 @@ void CShopping::RemoveLoadedPrices() {
 
 // 0x49AE30
 void CShopping::RemoveLoadedShop() {
-    plugin::Call<0x49AE30>();
-
     ms_shopLoaded[0] = false;
     auto animBlockIndex = CAnimManager::GetAnimationBlockIndex(ms_sectionNames[ms_priceSectionLoaded]);
     if (animBlockIndex != -1) {
@@ -205,8 +203,6 @@ void CShopping::RemovePriceModifier(uint32 a1) {
 
 // 0x49B240
 void CShopping::RestoreClothesState() {
-    plugin::Call<0x49B240>();
-
     memcpy(FindPlayerPed()->m_pPlayerData->m_pPedClothesDesc, &gStoredClothesState, sizeof(CPedClothesDesc));
     gClothesHaveBeenStored = 0u;
 }
@@ -224,8 +220,6 @@ void CShopping::SetCurrentProperty(CMultiBuilding* property) {
 
 // 0x49B610
 void CShopping::SetPlayerHasBought(uint32 itemKey) {
-    plugin::Call<0x49B610, uint32>(itemKey);
-
     for (auto&& [i, key] : notsa::enumerate(ms_keys)) {
         if (key == itemKey) {
             ms_bHasBought[i] = true;
@@ -240,8 +234,6 @@ void CShopping::ShutdownForRestart() {
 
 // 0x49B200
 void CShopping::StoreClothesState() {
-    plugin::Call<0x49B200>();
-
     // todo: operator=
     memcpy(&gStoredClothesState, FindPlayerPed()->m_pPlayerData->m_pPedClothesDesc, sizeof(CPedClothesDesc));
     gClothesHaveBeenStored = 1u;
@@ -249,8 +241,6 @@ void CShopping::StoreClothesState() {
 
 // 0x49B280
 void CShopping::StoreVehicleMods() {
-    plugin::Call<0x49B280>();
-
     auto veh = FindPlayerVehicle()->AsAutomobile();
     if (!veh) // NOTSA
         return;
@@ -301,8 +291,6 @@ void CShopping::UpdateStats(int32 a1, bool a2) {
 
 // 0x5D3E40
 void CShopping::Load() {
-    plugin::Call<0x5D3E40>();
-
     CGenericGameStorage::LoadDataFromWorkBuffer(&ms_numPriceModifiers, sizeof(int32));
     for (auto i = 0; i < ms_numPriceModifiers; i++) {
         CGenericGameStorage::LoadDataFromWorkBuffer(&ms_priceModifiers[i], sizeof(tPriceModifier));
@@ -314,8 +302,6 @@ void CShopping::Load() {
 
 // 0x5D3DE0
 void CShopping::Save() {
-    plugin::Call<0x5D3DE0>();
-
     CGenericGameStorage::SaveDataToWorkBuffer(&ms_numPriceModifiers, sizeof(int32));
     for (auto i = 0; i < ms_numPriceModifiers; i++) {
         CGenericGameStorage::SaveDataToWorkBuffer(&ms_priceModifiers[i], sizeof(tPriceModifier));
