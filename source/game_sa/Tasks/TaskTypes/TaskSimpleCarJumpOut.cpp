a9e869751d902484467e8d27c9d7da6928343c8a
Izzotop <1680033+Izzotop@users.noreply.github.com>
2022-08-22 20:29:30 +0300

    CTaskSimpleCarJumpOut

--- source/game_sa/Tasks/TaskTypes/TaskSimpleCarJumpOut.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskSimpleCarJumpOut.cpp
@@ -1,7 +1,8 @@
 #include "StdInc.h"
-#include "PedPlacement.h"
+
 #include "TaskSimpleCarJumpOut.h"
 #include "TaskSimpleCarSetPedOut.h"
+#include "PedPlacement.h"
 
 void CTaskSimpleCarJumpOut::InjectHooks() {
     RH_ScopedVirtualClass(CTaskSimpleCarJumpOut, 0x86ee70, 9);
@@ -9,12 +10,9 @@ void CTaskSimpleCarJumpOut::InjectHooks() {
 
     RH_ScopedInstall(Constructor, 0x64BE40);
     RH_ScopedInstall(Destructor, 0x647490);
-
     RH_ScopedGlobalInstall(FinishAnimCarJumpOutCB, 0x647700);
-
     RH_ScopedInstall(ComputeAnimID_ToHook, 0x647690);
     RH_ScopedInstall(StartAnim, 0x64BF00);
-
     RH_ScopedVMTInstall(Clone, 0x64D220);
     RH_ScopedVMTInstall(GetTaskType, 0x647500);
     RH_ScopedVMTInstall(MakeAbortable, 0x647510);
@@ -23,48 +21,43 @@ void CTaskSimpleCarJumpOut::InjectHooks() {
 }
 
 // 0x64BE40
-CTaskSimpleCarJumpOut::CTaskSimpleCarJumpOut(CVehicle* veh, uint32 door, CTaskUtilityLineUpPedWithCar* lineUpTask) :
-    m_veh{veh},
-    m_door{door},
-    m_lineUpTask{lineUpTask},
-    m_initialVehSpeed{ veh ? veh->GetMoveSpeed().Magnitude() : 0.f }
+CTaskSimpleCarJumpOut::CTaskSimpleCarJumpOut(CVehicle* targetVehicle, uint32 targetDoor, CTaskUtilityLineUpPedWithCar* utility) :
+    CTaskSimple(),
+    m_TargetVehicle{ targetVehicle },
+    m_nTargetDoor{ targetDoor },
+    m_Utility{ utility },
+    m_fStartCarSpeed{ targetVehicle ? targetVehicle->GetMoveSpeed().Magnitude() : 0.f }
 {
-    CEntity::SafeCleanUpRef(m_veh);
-}
-
-CTaskSimpleCarJumpOut::CTaskSimpleCarJumpOut(const CTaskSimpleCarJumpOut& o) :
-    CTaskSimpleCarJumpOut{o.m_veh, o.m_door, o.m_lineUpTask}
-{
+    CEntity::SafeCleanUpRef(m_TargetVehicle);
 }
 
 // 0x647490
 CTaskSimpleCarJumpOut::~CTaskSimpleCarJumpOut() {
-    CEntity::SafeCleanUpRef(m_veh);
-
-    if (m_anim) {
-        m_anim->SetFinishCallback(CDefaultAnimCallback::DefaultAnimCB, nullptr);
+    CEntity::SafeCleanUpRef(m_TargetVehicle);
+    if (m_Anim) {
+        m_Anim->SetFinishCallback(CDefaultAnimCallback::DefaultAnimCB, nullptr);
     }
 }
 
 // 0x647700
 void CTaskSimpleCarJumpOut::FinishAnimCarJumpOutCB(CAnimBlendAssociation* anim, void* data) {
     const auto self = static_cast<CTaskSimpleCarJumpOut*>(data);
-    self->m_finishedAnim = true;
-    self->m_anim = nullptr;
+    self->m_bIsFinished = true;
+    self->m_Anim = nullptr;
 }
 
 // 0x64BF00
 void CTaskSimpleCarJumpOut::StartAnim(const CPed* ped) {
     const auto [groupId, animId] = ComputeAnimID();
-    m_anim = CAnimManager::BlendAnimation(ped->m_pRwClump, groupId, animId, 8.f);
-    m_anim->SetFinishCallback(FinishAnimCarJumpOutCB, this);
+    m_Anim = CAnimManager::BlendAnimation(ped->m_pRwClump, groupId, animId, 8.f);
+    m_Anim->SetFinishCallback(FinishAnimCarJumpOutCB, this);
 }
 
 // Signature changed, `ComputeAnimID_Wrapper` is hooked instead.
 // 0X647690
 auto CTaskSimpleCarJumpOut::ComputeAnimID() -> std::pair<AssocGroupId, AnimationId> {
     const auto animId = [this] {
-        switch (m_door) {
+        switch (m_nTargetDoor) {
         case TARGET_DOOR_FRONT_RIGHT:
         case TARGET_DOOR_REAR_RIGHT:
             return ANIM_ID_CAR_ROLLOUT_RHS;;
@@ -73,27 +66,28 @@ auto CTaskSimpleCarJumpOut::ComputeAnimID() -> std::pair<AssocGroupId, Animation
             return ANIM_ID_CAR_ROLLOUT_LHS;
         default:
             NOTSA_UNREACHABLE();
+            return ANIM_ID_UNDEFINED;
         }
     }();
-    return { (AssocGroupId)m_veh->GetAnimGroup().GetGroup(animId), animId };
+    return { (AssocGroupId)m_TargetVehicle->GetAnimGroup().GetGroup(animId), animId };
 }
 
 // 0x647510
-bool CTaskSimpleCarJumpOut::MakeAbortable(CPed* ped, eAbortPriority priority, CEvent const* event) {
+bool CTaskSimpleCarJumpOut::MakeAbortable(CPed* ped, eAbortPriority priority, const CEvent* event) {
     switch (priority) {
     case ABORT_PRIORITY_IMMEDIATE: {
-        if (m_anim) {
-            m_anim->m_fBlendDelta = -1000.f;
+        if (m_Anim) {
+            m_Anim->m_fBlendDelta = -1000.f;
         }
         return true;
     }
     case ABORT_PRIORITY_URGENT: {
-        if (event && m_anim && !ped->bInVehicle) {
+        if (event && m_Anim && !ped->bInVehicle) {
             switch (event->GetEventType()) {
             case EVENT_IN_AIR:
             case EVENT_IN_WATER: {
-                m_anim->m_fBlendDelta = -2.f;
-                m_finishedAnim = true;
+                m_Anim->m_fBlendDelta = -2.f;
+                m_bIsFinished = true;
                 return true;
             }
             }
@@ -106,69 +100,69 @@ bool CTaskSimpleCarJumpOut::MakeAbortable(CPed* ped, eAbortPriority priority, CE
 
 // 0x64DD60
 bool CTaskSimpleCarJumpOut::ProcessPed(CPed* ped) {
-    if (m_finishedAnim) {
+    if (m_bIsFinished) {
         return true;
     }
 
-    if (!m_veh) {
+    if (!m_TargetVehicle) {
         return true;
     }
 
     // Start calculating animation shift for ped in certain vehicles
     if ([this]{
-        if (!m_anim) {
+        if (!m_Anim) {
             return true;
         }
 
-        if (m_veh->IsBike() || m_veh->IsSubQuad()) {
-            return m_anim->m_fCurrentTime < 0.7333333f; // TODO: Magic?
-        } else if (m_veh->IsAutomobile()) {
-            return m_anim->m_fCurrentTime < 0.45f;
+        if (m_TargetVehicle->IsBike() || m_TargetVehicle->IsSubQuad()) {
+            return m_Anim->m_fCurrentTime < 0.7333333f; // TODO: Magic?
+        } else if (m_TargetVehicle->IsAutomobile()) {
+            return m_Anim->m_fCurrentTime < 0.45f;
         }
 
-        return false;    
+        return false;
     }()) {
-        m_initialVehSpeed *= std::pow(0.96f, CTimer::GetTimeStep());
-        ped->m_vecAnimMovingShiftLocal.y += m_initialVehSpeed * CTimer::GetTimeStep();
+        m_fStartCarSpeed *= std::pow(0.96f, CTimer::GetTimeStep());
+        ped->m_vecAnimMovingShiftLocal.y += m_fStartCarSpeed * CTimer::GetTimeStep();
     }
 
     // Not in vehicle anymore?
     if (!ped->bInVehicle) { // Inverted
-        if (m_veh && m_veh->IsAutomobile()) { // 0x64DE0E
+        if (m_TargetVehicle && m_TargetVehicle->IsAutomobile()) { // 0x64DE0E
             ped->m_pedIK.bUseArm = true;
             ped->bFallenDown = true;
         }
-        return !m_anim;
+        return !m_Anim;
     }
 
     // Anim not playing yet?
-    if (!m_anim) { // Inverted - 0x64DE56
-        StartAnim(ped); 
+    if (!m_Anim) { // Inverted - 0x64DE56
+        StartAnim(ped);
         return false;
     }
 
     // Start opening the door - 0x64DE74
     const auto GetDoorOpenTimeThreshold = [this] {
-        if (m_veh->IsBike() || m_veh->IsSubQuad()) {
+        if (m_TargetVehicle->IsBike() || m_TargetVehicle->IsSubQuad()) {
             return 0.35f;
-        } else if (m_veh->IsPlane()) {
+        } else if (m_TargetVehicle->IsPlane()) {
             return 0.15f;
         }
         return 0.07f;
     };
-    if (GetDoorOpenTimeThreshold() >= m_anim->m_fCurrentTime) {
-        if (m_veh->IsAutomobile()) {
+    if (GetDoorOpenTimeThreshold() >= m_Anim->m_fCurrentTime) {
+        if (m_TargetVehicle->IsAutomobile()) {
             const auto [grpId, animId] = ComputeAnimID(); // 0x64E00D
-            m_veh->ProcessOpenDoor(ped, m_door, grpId, animId, m_anim->m_fCurrentTime);
+            m_TargetVehicle->ProcessOpenDoor(ped, m_nTargetDoor, grpId, animId, m_Anim->m_fCurrentTime);
         }
         return false;
     }
 
     // 0x64DEC0
     {
-        CTaskSimpleCarSetPedOut getOutTask{ m_veh, m_door, true };
-        getOutTask.m_nDoorFlagsToClear = m_doorFlagsToClear;
-        getOutTask.m_nNumGettingInToClear = m_numGettingInToClear;
+        CTaskSimpleCarSetPedOut getOutTask{m_TargetVehicle, m_nTargetDoor, true };
+        getOutTask.m_nDoorFlagsToClear = m_nDoorFlagsToClear;
+        getOutTask.m_nNumGettingInToClear = m_nNumGettingInToClear;
         getOutTask.ProcessPed(ped);
     }
 
@@ -176,17 +170,17 @@ bool CTaskSimpleCarJumpOut::ProcessPed(CPed* ped) {
 
     // Change rotation of ped
     {
-        const auto rot = m_veh->GetHeading();
-        ped->m_fAimingRotation = ped->m_fCurrentRotation = m_veh->IsTotallyUpsideDown() ? CGeneral::LimitRadianAngle(rot + PI) : rot;
+        const auto rot = m_TargetVehicle->GetHeading();
+        ped->m_fAimingRotation = ped->m_fCurrentRotation = m_TargetVehicle->IsTotallyUpsideDown() ? CGeneral::LimitRadianAngle(rot + PI) : rot;
         ped->SetHeading(ped->m_fCurrentRotation);
     }
-    ped->GetMoveSpeed() = m_veh->GetMoveSpeed();
-    ped->m_pEntityIgnoredCollision = m_veh;
+    ped->GetMoveSpeed() = m_TargetVehicle->GetMoveSpeed();
+    ped->m_pEntityIgnoredCollision = m_TargetVehicle;
     ped->bKnockedOffBike = true;
-    if (m_veh->IsAutomobile()) {
-        m_veh->AsAutomobile()->m_damageManager.SetDoorOpen((eDoors)m_door);
+    if (m_TargetVehicle->IsAutomobile()) {
+        m_TargetVehicle->AsAutomobile()->m_damageManager.SetDoorOpen((eDoors)m_nTargetDoor);
     }
-    
+
     return false;
 }
 
@@ -199,8 +193,8 @@ bool CTaskSimpleCarJumpOut::SetPedPosition(CPed* ped) {
     ped->SetPedPositionInCar();
 
     // Do some Z position interpolation for ped for specific anim types
-    if (m_anim) {
-        switch (m_anim->m_nAnimId) {
+    if (m_Anim) {
+        switch (m_Anim->m_nAnimId) {
         case ANIM_ID_CAR_ROLLOUT_RHS:
         case ANIM_ID_CAR_ROLLOUT_LHS: {
             const auto pedPos = ped->GetPosition();
@@ -211,18 +205,17 @@ bool CTaskSimpleCarJumpOut::SetPedPosition(CPed* ped) {
             CWorld::pIgnoreEntity = nullptr;
 
             if (pedPosAdj.z > pedPos.z) {
-                const auto timeDivier = [this] {
-                    if (m_veh->IsBike() || m_veh->IsSubQuad()) {
+                const auto timeDivider = [this] {
+                    if (m_TargetVehicle->IsBike() || m_TargetVehicle->IsSubQuad()) {
                         return 0.27f;
                     }
                     return 0.07f;
                 }();
-                ped->GetMatrix().GetPosition().z += m_anim->m_fCurrentTime / timeDivier * (pedPosAdj.z - pedPos.z);
+                ped->GetMatrix().GetPosition().z += m_Anim->m_fCurrentTime / timeDivider * (pedPosAdj.z - pedPos.z);
             }
             break;
         }
         }
-
     }
 
     return true;
--- source/game_sa/Tasks/TaskTypes/TaskSimpleCarJumpOut.h
+++ source/game_sa/Tasks/TaskTypes/TaskSimpleCarJumpOut.h
@@ -6,50 +6,37 @@ class CAnimBlendAssociation;
 class CTaskUtilityLineUpPedWithCar;
 class CVehicle;
 
-// VMT: 0x86ee70 | Size: 9
 class NOTSA_EXPORT_VTABLE CTaskSimpleCarJumpOut : public CTaskSimple {
+public:
+    bool m_bIsFinished;
+    CAnimBlendAssociation* m_Anim;
+    CVehicle* m_TargetVehicle;
+    float m_fStartCarSpeed;
+    uint32 m_nTargetDoor;
+    uint8 m_nDoorFlagsToClear;
+    uint8 m_nNumGettingInToClear;
+    CTaskUtilityLineUpPedWithCar* m_Utility;
 
 public:
-    bool m_finishedAnim = {};                        // 8
-    CAnimBlendAssociation* m_anim = {};              // 0xC
-    CVehicle* m_veh = {};                            // 0x10
-    float m_initialVehSpeed = {};                    // 0x14
-    uint32 m_door = {};                               // 0x18
-    uint8 m_doorFlagsToClear = {};                   // 0x1C
-    uint8 m_numGettingInToClear = {};                // 0x1D
-    CTaskUtilityLineUpPedWithCar* m_lineUpTask = {}; // 0x20
+    constexpr static auto Type = TASK_SIMPLE_CAR_JUMP_OUT;
 
-public:
-    static void InjectHooks();
+    CTaskSimpleCarJumpOut(CVehicle* targetVehicle, uint32 targetDoor, CTaskUtilityLineUpPedWithCar* utility);
+    ~CTaskSimpleCarJumpOut() override;
 
-    constexpr static auto Type = eTaskType::TASK_SIMPLE_CAR_JUMP_OUT;
-
-    CTaskSimpleCarJumpOut(CVehicle* veh, uint32 door, CTaskUtilityLineUpPedWithCar* lineUpTask);
-    CTaskSimpleCarJumpOut(const CTaskSimpleCarJumpOut&);
-    ~CTaskSimpleCarJumpOut();
-
-    static void FinishAnimCarJumpOutCB(CAnimBlendAssociation* anim, void* data);
+    eTaskType GetTaskType() override { return Type; }
+    CTask* Clone() override { return new CTaskSimpleCarJumpOut(m_TargetVehicle, m_nTargetDoor, m_Utility); }
+    bool MakeAbortable(CPed* ped, eAbortPriority priority, const CEvent* event) override;
+    bool ProcessPed(CPed* ped) override;
+    bool SetPedPosition(CPed* ped) override;
 
     void StartAnim(const CPed* ped);
     auto ComputeAnimID() -> std::pair<AssocGroupId, AnimationId>; // NOTSA: Changed return type
     void ComputeAnimID_ToHook(AssocGroupId& animGroup, AnimationId& animId) { std::tie(animGroup, animId) = ComputeAnimID(); } // Hooking this instead
 
-    CTask* Clone() override { return new CTaskSimpleCarJumpOut{ *this }; }
-    eTaskType GetTaskType() override { return Type; }
-    bool MakeAbortable(CPed* ped, eAbortPriority priority, CEvent const* event) override;
-    bool ProcessPed(CPed* ped) override;
-    bool SetPedPosition(CPed* ped) override;
+    static void FinishAnimCarJumpOutCB(CAnimBlendAssociation* anim, void* data);
 
-private: // Wrappers for hooks
-    // 0x64BE40
-    CTaskSimpleCarJumpOut* Constructor(CVehicle* veh, uint32 door, CTaskUtilityLineUpPedWithCar* lineUpTask) {
-        this->CTaskSimpleCarJumpOut::CTaskSimpleCarJumpOut(veh, door, lineUpTask);
-        return this;
-    }
-
-    // 0x647490
-    CTaskSimpleCarJumpOut* Destructor() {
-        this->CTaskSimpleCarJumpOut::~CTaskSimpleCarJumpOut();
-        return this;
-    }
+    static void InjectHooks();
+    CTaskSimpleCarJumpOut* Constructor(CVehicle* veh, uint32 door, CTaskUtilityLineUpPedWithCar* lineUpTask) { this->CTaskSimpleCarJumpOut::CTaskSimpleCarJumpOut(veh, door, lineUpTask); return this; }
+    CTaskSimpleCarJumpOut* Destructor() { this->CTaskSimpleCarJumpOut::~CTaskSimpleCarJumpOut(); return this; }
 };
+VALIDATE_SIZE(CTaskSimpleCarJumpOut, 0x24);
