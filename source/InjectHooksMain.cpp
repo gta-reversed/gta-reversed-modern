3006def2fc2c169c40f806cb5da36d3a2ea6d0a4
Pirulax <patrikjankovics7@gmail.com>
2022-08-28 16:01:30 +0200

    Hook `CTaskComplexEnterCar` (#413)

--- source/InjectHooksMain.cpp
+++ source/InjectHooksMain.cpp
@@ -284,6 +284,7 @@
 #include "TaskSimpleBikeJacked.h"
 #include "TaskSimpleRunTimedAnim.h"
 #include "TaskSimpleSay.h"
+#include "TaskComplexEnterCar.h"
 #include "TaskComplexSmartFleePoint.h"
 
 #include "EventSeenPanickedPed.h"
@@ -843,7 +844,7 @@ void InjectHooksMain() {
         CTaskComplexJump::InjectHooks();
         // CTaskComplexWanderStandard::InjectHooks();
         // CTaskComplexSunbathe::InjectHooks();
-        // CTaskComplexEnterCar::InjectHooks();
+        CTaskComplexEnterCar::InjectHooks();
         // CTaskSimpleTogglePedThreatScanner::InjectHooks();
         CTaskComplexUseGoggles::InjectHooks();
         // CTaskComplexCrossRoadLookAndAchieveHeading::InjectHooks();
