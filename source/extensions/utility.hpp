a8d4cdada176b5293c340a93c3e807c41124d1c0
Pirulax <patrikjankovics7@gmail.com>
2022-09-04 13:21:16 +0200

    CEntityScanner: `GetEntities`

--- source/extensions/utility.hpp
+++ source/extensions/utility.hpp
@@ -20,4 +20,14 @@ private:
     Fn m_fn;
 };
 
+
+/// Predicate to check if `value` is null
+template<typename T>
+    requires(std::is_pointer_v<T>)
+bool IsNull(T value) { return value == nullptr; }
+
+/// Negate another predicate function
+template<typename T>
+auto Not(bool(*fn)(T)) { return [fn](const T& value) { return !fn(value); }; }
+
 };
