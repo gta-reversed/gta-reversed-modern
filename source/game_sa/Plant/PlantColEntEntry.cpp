c0ac6f29938e0170001ca26d1fd70c5aa9ef44e8
yukani <yukani@zohomail.eu>
2022-09-23 17:43:10 +0300

    CPlantColEntEntry::ReleaseEntry

--- source/game_sa/Plant/PlantColEntEntry.cpp
+++ source/game_sa/Plant/PlantColEntEntry.cpp
@@ -2,23 +2,23 @@
 
 #include "PlantColEntEntry.h"
 #include "PlantMgr.h"
+#include "PlantLocTri.h"
 
 void CPlantColEntEntry::InjectHooks() {
     RH_ScopedClass(CPlantColEntEntry);
     RH_ScopedCategoryGlobal();
 
     RH_ScopedInstall(AddEntry, 0x5DB7D0);
-    RH_ScopedInstall(ReleaseEntry, 0x5DB8A0, {.reversed = false});
+    RH_ScopedInstall(ReleaseEntry, 0x5DB8A0);
 }
 
 // 0x5DB7D0
 CPlantColEntEntry* CPlantColEntEntry::AddEntry(CEntity* entity) {
-    //return plugin::CallMethodAndReturn<CPlantColEntEntry*, 0x5DB7D0, CPlantColEntEntry*, CEntity*>(this, entity);
     m_Entity = entity;
     entity->RegisterReference(&m_Entity);
 
     auto colData = entity->GetColData();
-    if (!colData || colData->m_nNumTriangles < 1) {
+    if (!colData || colData->m_nNumTriangles < 1u) {
         return nullptr;
     }
     m_numTriangles = colData->m_nNumTriangles;
@@ -50,5 +50,40 @@ CPlantColEntEntry* CPlantColEntEntry::AddEntry(CEntity* entity) {
 
 // 0x5DB8A0
 void CPlantColEntEntry::ReleaseEntry() {
-    plugin::CallMethod<0x5DB7D0, CPlantColEntEntry*>(this);
+    if (m_Objects) {
+        if (m_numTriangles) {
+            for (auto& tri : std::span{m_Objects, m_numTriangles}) {
+                if (tri) {
+                    tri->Release();
+                }
+            }
+        }
+        CMemoryMgr::Free(m_Objects);
+        m_Objects = nullptr;
+        m_numTriangles = 0u;
+    }
+    CEntity::SafeCleanUpRef(m_Entity);
+    m_Entity = nullptr;
+
+    if (m_NextEntry) {
+        if (m_PrevEntry) {
+            m_PrevEntry->m_NextEntry = m_NextEntry;
+            m_NextEntry->m_PrevEntry = m_PrevEntry;
+        } else {
+            m_NextEntry->m_PrevEntry = nullptr;
+        }
+    } else {
+        CPlantMgr::m_CloseColEntListHead = m_PrevEntry;
+
+        if (CPlantMgr::m_CloseColEntListHead) {
+            CPlantMgr::m_CloseColEntListHead->m_NextEntry = nullptr;
+        }
+    }
+
+    m_PrevEntry = CPlantMgr::m_UnusedColEntListHead;
+    m_NextEntry = nullptr;
+    CPlantMgr::m_UnusedColEntListHead = this;
+    if (m_PrevEntry) {
+        m_PrevEntry->m_NextEntry = this;
+    }
 }
