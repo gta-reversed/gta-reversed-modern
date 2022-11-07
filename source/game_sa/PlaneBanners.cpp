d4c3b2d9bc708a6e4232b9d4d19fd0cc52861577
yukani <yukani@zohomail.eu>
2022-11-07 14:04:48 +0300

    Add PlaneBanners

---
+++ source/game_sa/PlaneBanners.cpp
@@ -0,0 +1,21 @@
+#include "PlaneBanners.h"
+
+// 0x717820
+void CPlaneBanners::Init() {
+    // NOP
+}
+
+// 0x717850
+void CPlaneBanners::RegisterPoint(CVector point, uint32 bannerIndex) {
+    // NOP
+}
+
+// 0x717830
+void CPlaneBanners::Render() {
+    // NOP
+}
+
+// 0x717840
+void CPlaneBanners::Update() {
+    // NOP
+}
