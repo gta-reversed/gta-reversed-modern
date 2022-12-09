63cff75954cde1c928c05add3ba00eac035cee1e
Pirulax <patrikjankovics7@gmail.com>
2022-12-09 16:33:39 +0100

    `CRealTimeShadow::SetLightProperties`

--- source/game_sa/RealTimeShadow.cpp
+++ source/game_sa/RealTimeShadow.cpp
@@ -3,12 +3,12 @@
 
 void CRealTimeShadow::InjectHooks() {
     RH_ScopedClass(CRealTimeShadow);
-    RH_ScopedCategory(); // TODO: Change this to the appropriate category!
+    RH_ScopedCategoryGlobal();
 
     RH_ScopedInstall(Constructor, 0x706410);
     RH_ScopedInstall(Destructor, 0x706440);
 
-    RH_ScopedInstall(SetLightProperties, 0x705900, {.reversed = false});
+    RH_ScopedInstall(SetLightProperties, 0x705900);
     RH_ScopedInstall(GetShadowRwTexture, 0x7059F0, {.reversed = false});
     RH_ScopedInstall(DrawBorderAroundTexture, 0x705A00, {.reversed = false});
     RH_ScopedInstall(Create, 0x706460, {.reversed = false});
@@ -21,8 +21,21 @@ CRealTimeShadow::~CRealTimeShadow() {
 }
 
 // 0x705900
-RwFrame* CRealTimeShadow::SetLightProperties(float angle, float unused, bool setLight) {
-    return plugin::CallMethodAndReturn<RwFrame*, 0x705900, CRealTimeShadow*, float, float, bool>(this, angle, unused, setLight);
+RwFrame* CRealTimeShadow::SetLightProperties(float angle, float, bool doSetCamLight) {
+    if (const auto frame = RpLightGetFrame(m_pLight)) {
+        const RwV3d xaxis{ 0.f, 1.f, 0.f };
+        RwFrameRotate(frame, &xaxis, angle, rwCOMBINEREPLACE);
+
+        const RwV3d yaxis{ 1.f, 0.f, 0.f };
+        RwFrameRotate(frame, &yaxis, angle, rwCOMBINEPOSTCONCAT);
+
+        if (doSetCamLight) {
+            m_camera.SetLight(m_pLight);
+        }
+
+        return frame;
+    }
+    return nullptr;
 }
 
 // 0x7059F0
