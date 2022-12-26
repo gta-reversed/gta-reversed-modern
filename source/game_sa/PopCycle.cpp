1e7cc242d2d8a3eb622ff0eac87c7eda5054460b
Pirulax <patrikjankovics7@gmail.com>
2022-12-26 21:45:46 +0100

    Update source/game_sa/PopCycle.cpp

--- source/game_sa/PopCycle.cpp
+++ source/game_sa/PopCycle.cpp
@@ -319,7 +319,7 @@ void CPopCycle::Update() {
 
     m_nCurrentTimeIndex = CClock::GetGameClockHours() / 2;
 
-    if (const auto pos = FindPlayerCentreOfWorld(); pos.z < 950.f || !m_pCurrZoneInfo) {
+    if (const auto& pos = FindPlayerCentreOfWorld(); pos.z < 950.f || !m_pCurrZoneInfo) {
         m_pCurrZoneInfo = CTheZones::GetZoneInfo(pos, &m_pCurrZone);
         m_nCurrentZoneType = m_pCurrZoneInfo->zonePopulationType;
     }
