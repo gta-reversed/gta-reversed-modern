6838faa0ac1e4279941cd3f8c6d0ca7559701cd0
Pirulax <patrikjankovics7@gmail.com>
2022-12-27 18:58:49 +0100

    Revert "game: use lambdas instead of seperate functions"
    This reverts commit 34f77f32f1388facc8c00632fe017b4e29b3cc76.

--- source/game_sa/Scripts/Commands/Game.hpp
+++ source/game_sa/Scripts/Commands/Game.hpp
@@ -16,108 +16,177 @@
 * Various game realted commands
 */
 
-REGISTER_COMMAND_HANDLER(COMMAND_SET_ALL_TAXIS_HAVE_NITRO, [](bool enabled) { (enabled ? CCheat::ApplyCheat : CCheat::Disable)(CHEAT_ALL_TAXIS_NITRO); });
+void SetAllTaxisHaveNitro(bool enabled) {
+    (enabled ? CCheat::ApplyCheat : CCheat::Disable)(CHEAT_ALL_TAXIS_NITRO);
+}
+REGISTER_COMMAND_HANDLER(COMMAND_SET_ALL_TAXIS_HAVE_NITRO, SetAllTaxisHaveNitro);
 
-REGISTER_COMMAND_HANDLER(COMMAND_ACTIVATE_PIMP_CHEAT, [](bool enabled) { (enabled ? CCheat::ApplyCheat : CCheat::Disable)(CHEAT_PROSTITUTES_PAY_YOU); });
+void ActivatePimpCheat(bool enabled) {
+    (enabled ? CCheat::ApplyCheat : CCheat::Disable)(CHEAT_PROSTITUTES_PAY_YOU);
+}
+REGISTER_COMMAND_HANDLER(COMMAND_ACTIVATE_PIMP_CHEAT, ActivatePimpCheat);
 
-REGISTER_COMMAND_HANDLER(COMMAND_ARE_ANY_CAR_CHEATS_ACTIVATED, []() {
+bool AreAnyCarCheatsActivated() {
     return rng::any_of(std::array{CHEAT_PERFECT_HANDLING, CHEAT_CARS_ON_WATER, CHEAT_BOATS_FLY}, CCheat::IsActive);
-});
+}
+REGISTER_COMMAND_HANDLER(COMMAND_ARE_ANY_CAR_CHEATS_ACTIVATED, AreAnyCarCheatsActivated);
 
-REGISTER_COMMAND_HANDLER(COMMAND_FAIL_CURRENT_MISSION, []() { CTheScripts::FailCurrentMission = 2; });
+void FailCurrentMission() {
+    CTheScripts::FailCurrentMission = 2;
+}
+REGISTER_COMMAND_HANDLER(COMMAND_FAIL_CURRENT_MISSION, FailCurrentMission);
 
-REGISTER_COMMAND_HANDLER(COMMAND_GET_GROUND_Z_FOR_3D_COORD, [](CVector coord) {
+float GetGroundZFor3DCoord(CVector coord) {
     return CWorld::FindGroundZFor3DCoord(coord, nullptr, nullptr);
-});
+}
+REGISTER_COMMAND_HANDLER(COMMAND_GET_GROUND_Z_FOR_3D_COORD, GetGroundZFor3DCoord);
 
-REGISTER_COMMAND_HANDLER(COMMAND_PLAYER_MADE_PROGRESS, []() { return CStats::IncrementStat(STAT_PROGRESS_MADE); });
+void PlayerMadeProgress() {
+    return CStats::IncrementStat(STAT_PROGRESS_MADE);
+}
+REGISTER_COMMAND_HANDLER(COMMAND_PLAYER_MADE_PROGRESS, PlayerMadeProgress);
 
-REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_MISSION_GIVEN, []() { CStats::IncrementStat(STAT_MISSION_ATTEMPTS); });
+void RegisterMissionGiven() {
+    CStats::IncrementStat(STAT_MISSION_ATTEMPTS);
+}
+REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_MISSION_GIVEN, RegisterMissionGiven);
 
-REGISTER_COMMAND_HANDLER(COMMAND_SET_PED_DENSITY_MULTIPLIER, [](float mult) {
+void SetPedDensityMultiplier(float mult) {
     CPopulation::PedDensityMultiplier = mult;
-});
+}
+REGISTER_COMMAND_HANDLER(COMMAND_SET_PED_DENSITY_MULTIPLIER, SetPedDensityMultiplier);
 
-REGISTER_COMMAND_HANDLER(COMMAND_IS_JAPANESE_VERSION, []() {
+bool IsJapaneseVersion() {
     return false; // TODO: make this configurable perhaps?
-});
+}
+REGISTER_COMMAND_HANDLER(COMMAND_IS_JAPANESE_VERSION, IsJapaneseVersion);
 
-REGISTER_COMMAND_HANDLER(COMMAND_IS_WIDESCREEN_ON_IN_OPTIONS, []() -> bool { return FrontEndMenuManager.m_bWidescreenOn; });
+bool IsWidescreenOnInOptions() {
+    return FrontEndMenuManager.m_bWidescreenOn;
+}
+REGISTER_COMMAND_HANDLER(COMMAND_IS_WIDESCREEN_ON_IN_OPTIONS, IsWidescreenOnInOptions);
 
-REGISTER_COMMAND_HANDLER(COMMAND_GET_RID_OF_PLAYER_PROSTITUTE, CTaskComplexProstituteSolicit::GetRidOfPlayerProstitute);
+void GetRidOfPlayerProstitute() {
+    CTaskComplexProstituteSolicit::GetRidOfPlayerProstitute();
+}
+REGISTER_COMMAND_HANDLER(COMMAND_GET_RID_OF_PLAYER_PROSTITUTE, GetRidOfPlayerProstitute);
 
-REGISTER_COMMAND_HANDLER(COMMAND_SET_AREA_NAME, [](const char* key) { CHud::SetZoneName(TheText.Get(key), true); });
+void SetAreaName(const char* key) {
+    CHud::SetZoneName(TheText.Get(key), true);
+}
+REGISTER_COMMAND_HANDLER(COMMAND_SET_AREA_NAME, SetAreaName);
 
-REGISTER_COMMAND_HANDLER(COMMAND_SET_RADIO_TO_PLAYERS_FAVOURITE_STATION, []() { AudioEngine.RetuneRadio(CStats::FindMostFavoriteRadioStation()); });
+void SetRadioToPlayersFavorite() {
+    AudioEngine.RetuneRadio(CStats::FindMostFavoriteRadioStation());
+}
+REGISTER_COMMAND_HANDLER(COMMAND_SET_RADIO_TO_PLAYERS_FAVOURITE_STATION, SetRadioToPlayersFavorite);
 
-REGISTER_COMMAND_HANDLER(COMMAND_SET_DISABLE_MILITARY_ZONES, [](bool disable) { CCullZones::bMilitaryZonesDisabled = disable; });
+void SetDisableMilitaryZones(bool disable) {
+    CCullZones::bMilitaryZonesDisabled = disable;
+}
+REGISTER_COMMAND_HANDLER(COMMAND_SET_DISABLE_MILITARY_ZONES, SetDisableMilitaryZones);
 
-REGISTER_COMMAND_HANDLER(COMMAND_INCREMENT_FLOAT_STAT_NO_MESSAGE, CStats::IncrementStat);
-REGISTER_COMMAND_HANDLER(COMMAND_INCREMENT_INT_STAT_NO_MESSAGE, CStats::IncrementStat);
+void IncrementFloatStatNoMessage(eStats stat, float value) {
+    CStats::IncrementStat(stat, value);
+}
+REGISTER_COMMAND_HANDLER(COMMAND_INCREMENT_FLOAT_STAT_NO_MESSAGE, IncrementFloatStatNoMessage);
+REGISTER_COMMAND_HANDLER(COMMAND_INCREMENT_INT_STAT_NO_MESSAGE, IncrementFloatStatNoMessage);
 
-REGISTER_COMMAND_HANDLER(COMMAND_TAKE_PHOTO, [](bool save) {
+void TakePhoto(bool save) {
     CWeapon::ms_bTakePhoto = true;
     CPostEffects::m_bSavePhotoFromScript = save;
-});
+}
+REGISTER_COMMAND_HANDLER(COMMAND_TAKE_PHOTO, TakePhoto);
 
-REGISTER_COMMAND_HANDLER(COMMAND_SET_NO_RESPRAYS, [](bool enabled) {
+void SetNoResprays(bool enabled) {
     CGarages::NoResprays = enabled;
     CGarages::AllRespraysCloseOrOpen(enabled);
-});
+}
+REGISTER_COMMAND_HANDLER(COMMAND_SET_NO_RESPRAYS, SetNoResprays);
 
-REGISTER_COMMAND_HANDLER(COMMAND_SET_RESPAWN_POINT_FOR_DURATION_OF_MISSION, CRestart::SetRespawnPointForDurationOfMission);
+void SetRespawnPointForDurationOfMission(CVector point) {
+    CRestart::SetRespawnPointForDurationOfMission(point);
+}
+REGISTER_COMMAND_HANDLER(COMMAND_SET_RESPAWN_POINT_FOR_DURATION_OF_MISSION, SetRespawnPointForDurationOfMission);
 
-REGISTER_COMMAND_HANDLER(COMMAND_GET_CURRENT_LANGUAGE, []() { return FrontEndMenuManager.m_nPrefsLanguage; });
+eLanguage GetCurrentLanguage() {
+    return FrontEndMenuManager.m_nPrefsLanguage;
+}
+REGISTER_COMMAND_HANDLER(COMMAND_GET_CURRENT_LANGUAGE, GetCurrentLanguage);
 
-REGISTER_COMMAND_HANDLER(COMMAND_DO_WEAPON_STUFF_AT_START_OF_2P_GAME, []() { CGameLogic::DoWeaponStuffAtStartOf2PlayerGame(true); });
+void DoWeaponStuffAtStartOf2PlayerGame() {
+    CGameLogic::DoWeaponStuffAtStartOf2PlayerGame(true);
+}
+REGISTER_COMMAND_HANDLER(COMMAND_DO_WEAPON_STUFF_AT_START_OF_2P_GAME, DoWeaponStuffAtStartOf2PlayerGame);
 
-REGISTER_COMMAND_HANDLER(COMMAND_DISPLAY_RADAR, [](bool enable) {
+void DisplayRadar(bool enable) {
     // izzotop: CTheScripts::HideAllFrontEndMapBlips = enable;
     CHud::bScriptDontDisplayRadar = !enable;
-});
+}
+REGISTER_COMMAND_HANDLER(COMMAND_DISPLAY_RADAR, DisplayRadar);
 
-REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_BEST_POSITION, CStats::RegisterBestPosition);
+void RegisterBestPosition(uint8 stat, int32 position) {
+    CStats::RegisterBestPosition(static_cast<eStats>(stat), position);
+}
+REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_BEST_POSITION, RegisterBestPosition);
 
-REGISTER_COMMAND_HANDLER(COMMAND_SET_MENU_HEADER_ORIENTATION, []() {
+void SetMenuHeaderOrientation() {
     // NOP
 
     // CMenuSystem::SetHeaderOrientation();
-});
+}
+REGISTER_COMMAND_HANDLER(COMMAND_SET_MENU_HEADER_ORIENTATION, SetMenuHeaderOrientation);
 
 #ifdef IMPLEMENT_UNSUPPORTED_OPCODES
-REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_JUMP_DISTANCE, [](float distance) {
+void RegisterJumpDistance(float distance) {
     CStats::SetStatValue(STAT_MAXIMUM_INSANE_JUMP_DISTANCE, std::max(
         CStats::GetStatValue(STAT_MAXIMUM_INSANE_JUMP_DISTANCE),
         distance
     ));
-});
+}
+REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_JUMP_DISTANCE, RegisterJumpDistance);
 
-REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_JUMP_HEIGHT, [](float height) {
+void RegisterJumpHeight(float height) {
     CStats::SetStatValue(STAT_MAXIMUM_INSANE_JUMP_FLIPS, std::max(
         CStats::GetStatValue(STAT_MAXIMUM_INSANE_JUMP_FLIPS),
         height
     ));
-});
+}
+REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_JUMP_HEIGHT, RegisterJumpHeight);
 
-REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_JUMP_SPINS, [](float rotation) {
+void RegisterJumpSpins(float rotation) {
     CStats::SetStatValue(STAT_MAXIMUM_INSANE_JUMP_ROTATION, std::max(
         CStats::GetStatValue(STAT_MAXIMUM_INSANE_JUMP_ROTATION),
         rotation
     ));
-});
+}
+REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_JUMP_SPINS, RegisterJumpSpins);
 
-REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_JUMP_STUNT, [](float stunt) {
+void RegisterJumpStunt(float stunt) {
     CStats::SetStatValue(STAT_BEST_INSANE_STUNT_AWARDED, std::max(
         CStats::GetStatValue(STAT_BEST_INSANE_STUNT_AWARDED),
         stunt
     ));
-});
+}
+REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_JUMP_STUNT, RegisterJumpStunt);
 
-REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_UNIQUE_JUMP_FOUND, []() { CStats::IncrementStat(STAT_UNIQUE_JUMPS_FOUND); });
+void RegisterUniqueJumpFound() {
+    CStats::IncrementStat(STAT_UNIQUE_JUMPS_FOUND);
+}
+REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_UNIQUE_JUMP_FOUND, RegisterUniqueJumpFound);
 
-REGISTER_COMMAND_HANDLER(COMMAND_SET_UNIQUE_JUMPS_TOTAL, [](float amount) { CStats::SetStatValue(STAT_UNIQUE_JUMPS_DONE, amount); });
+void SetUniqueJumpsTotal(float amount) {
+    CStats::SetStatValue(STAT_UNIQUE_JUMPS_DONE, amount);
+}
+REGISTER_COMMAND_HANDLER(COMMAND_SET_UNIQUE_JUMPS_TOTAL, SetUniqueJumpsTotal);
 
-REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_PASSENGER_DROPPED_OFF_TAXI, []() { CStats::IncrementStat(STAT_PASSENGERS_DROPPED_OFF); });
+void RegisterPassengerDroppedOffTaxi() {
+    CStats::IncrementStat(STAT_PASSENGERS_DROPPED_OFF);
+}
+REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_PASSENGER_DROPPED_OFF_TAXI, RegisterPassengerDroppedOffTaxi);
 
-REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_MONEY_MADE_TAXI, [](float amount) { CStats::SetStatValue(STAT_CASH_MADE_IN_A_TAXI, amount); });
+void RegisterMoneyMadeTaxi(float amount) {
+    CStats::SetStatValue(STAT_CASH_MADE_IN_A_TAXI, amount);
+}
+REGISTER_COMMAND_HANDLER(COMMAND_REGISTER_MONEY_MADE_TAXI, RegisterMoneyMadeTaxi);
 #endif
