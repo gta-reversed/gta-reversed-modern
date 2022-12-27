4cae3740de0a5fddbe20b356a7a34f6491de794b
yukani <yukani@zohomail.eu>
2022-12-27 18:50:28 +0300

    sequence: use lambdas instead of seperate functions

--- source/game_sa/Scripts/Commands/Sequence.hpp
+++ source/game_sa/Scripts/Commands/Sequence.hpp
@@ -4,12 +4,11 @@
 * Various Sequence commands
 */
 
-void SetSequenceToRepeat(int32 index, bool repeat) {
+REGISTER_COMMAND_HANDLER(COMMAND_SET_SEQUENCE_TO_REPEAT, [](int32 index, bool repeat) {
     const auto actualIndex = CTheScripts::GetActualScriptThingIndex(index, SCRIPT_THING_SEQUENCE_TASK);
     if (actualIndex < 0 || actualIndex >= MAX_NUM_SCRIPT_SEQUENCE_TASKS) {
         DEV_LOG("COMMAND_SET_SEQUENCE_TO_REPEAT: Index should be in [0, 64], it is {}", actualIndex);
         return;
     }
     CTaskSequences::GetActiveSequence().m_bRepeatSequence = repeat;
-}
-REGISTER_COMMAND_HANDLER(COMMAND_SET_SEQUENCE_TO_REPEAT, SetSequenceToRepeat);
+});
