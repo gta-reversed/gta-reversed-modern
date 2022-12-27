fd680fe9438d3629901b5e2cb3ed578b8c0b3db0
Pirulax <patrikjankovics7@gmail.com>
2022-12-27 18:58:14 +0100

    Revert "clock: use lambdas instead of seperate functions"
    This reverts commit b9ec77443ef69aaff74a474b36102bb27ad50007.

--- source/game_sa/Scripts/Commands/Clock.hpp
+++ source/game_sa/Scripts/Commands/Clock.hpp
@@ -8,12 +8,27 @@ using namespace notsa::script;
 */
 
 /// Get hours and minutes
-REGISTER_COMMAND_HANDLER(COMMAND_GET_TIME_OF_DAY, ([]() -> MultiRet<uint8, uint8> { return {CClock::ms_nGameClockHours, CClock::ms_nGameClockMinutes}; }));
+MultiRet<uint8, uint8> GetTimeOfDay() {
+    return { CClock::ms_nGameClockHours, CClock::ms_nGameClockMinutes };
+}
+REGISTER_COMMAND_HANDLER(COMMAND_GET_TIME_OF_DAY, GetTimeOfDay);
 
-REGISTER_COMMAND_HANDLER(COMMAND_SET_TIME_OF_DAY, [](uint8 hours, uint8 minutes) { CClock::SetGameClock(hours, minutes, 0); });
+void SetTimeOfDay(uint8 hours, uint8 minutes) {
+    CClock::SetGameClock(hours, minutes, 0);
+}
+REGISTER_COMMAND_HANDLER(COMMAND_SET_TIME_OF_DAY, SetTimeOfDay);
 
-REGISTER_COMMAND_HANDLER(COMMAND_GET_MINUTES_TO_TIME_OF_DAY, [](uint8 hours, uint8 minutes) { return CClock::GetGameClockMinutesUntil(hours, minutes); });
+uint16 GetMinutesToTimeOfDay(uint8 hours, uint8 minutes) {
+    return CClock::GetGameClockMinutesUntil(hours, minutes);
+}
+REGISTER_COMMAND_HANDLER(COMMAND_GET_MINUTES_TO_TIME_OF_DAY, GetMinutesToTimeOfDay);
 
-REGISTER_COMMAND_HANDLER(COMMAND_STORE_CLOCK, []() { CClock::StoreClock(); });
+void StoreClock() {
+    CClock::StoreClock();
+}
+REGISTER_COMMAND_HANDLER(COMMAND_STORE_CLOCK, StoreClock);
 
-REGISTER_COMMAND_HANDLER(COMMAND_RESTORE_CLOCK, []() { CClock::RestoreClock(); });
+void RestoreClock() {
+    CClock::RestoreClock();
+}
+REGISTER_COMMAND_HANDLER(COMMAND_RESTORE_CLOCK, RestoreClock);
