805531cf7527cc776f7ad421b46e3c5b32d1d8cb
yukani <yukani@zohomail.eu>
2022-12-27 18:51:52 +0300

    text: use lambdas instead of seperate functions

--- source/game_sa/Scripts/Commands/Text.hpp
+++ source/game_sa/Scripts/Commands/Text.hpp
@@ -7,50 +7,32 @@
 * Various text commands
 */
 
-void ClearSmallPrints() {
-    CMessages::ClearSmallMessagesOnly();
-}
-REGISTER_COMMAND_HANDLER(COMMAND_CLEAR_SMALL_PRINTS, ClearSmallPrints);
+REGISTER_COMMAND_HANDLER(COMMAND_CLEAR_SMALL_PRINTS, CMessages::ClearSmallMessagesOnly);
 
-void ClearHelp() {
-    CHud::SetHelpMessage(nullptr, true, false, false);
-}
-REGISTER_COMMAND_HANDLER(COMMAND_CLEAR_HELP, ClearHelp);
+REGISTER_COMMAND_HANDLER(COMMAND_CLEAR_HELP, []() { CHud::SetHelpMessage(nullptr, true, false, false); });
 
-void FlashHudObject(eHudItem item) {
-    CHud::m_ItemToFlash = item;
-}
-REGISTER_COMMAND_HANDLER(COMMAND_FLASH_HUD_OBJECT, FlashHudObject);
+REGISTER_COMMAND_HANDLER(COMMAND_FLASH_HUD_OBJECT, [](eHudItem item) { CHud::m_ItemToFlash = item; });
 
 // NOTSA: time: int32 -> uint32
-void PrintBig(const char* key, uint32 time, uint32 flags) {
+REGISTER_COMMAND_HANDLER(COMMAND_PRINT_BIG, [](const char* key, uint32 time, uint32 flags) {
     const auto text = TheText.Get(key);
     CMessages::AddBigMessage(text, time, static_cast<eMessageStyle>(flags - 1));
-}
-REGISTER_COMMAND_HANDLER(COMMAND_PRINT_BIG, PrintBig);
+});
 
-void Print(const char* key, uint32 time, uint32 flags) {
+REGISTER_COMMAND_HANDLER(COMMAND_PRINT, [](const char* key, uint32 time, uint32 flags) {
     const auto text = TheText.Get(key);
     if (!text || strncmp(text, "~z~", 3u) != 0 || FrontEndMenuManager.m_bShowSubtitles)
         CMessages::AddMessage(text, time, flags, CTheScripts::bAddNextMessageToPreviousBriefs);
     CTheScripts::bAddNextMessageToPreviousBriefs = true;
-}
-REGISTER_COMMAND_HANDLER(COMMAND_PRINT, Print);
+});
 
-void PrintNow(const char* key, uint32 time, uint32 flags) {
+REGISTER_COMMAND_HANDLER(COMMAND_PRINT_NOW, [](const char* key, uint32 time, uint32 flags) {
     const auto text = TheText.Get(key);
     if (!text || strncmp(text, "~z~", 3u) != 0 || FrontEndMenuManager.m_bShowSubtitles)
         CMessages::AddMessageJumpQ(text, time, flags, CTheScripts::bAddNextMessageToPreviousBriefs);
     CTheScripts::bAddNextMessageToPreviousBriefs = true;
-}
-REGISTER_COMMAND_HANDLER(COMMAND_PRINT_NOW, PrintNow);
+});
 
-void ClearThisPrintBigNow(uint16 item) {
-    CMessages::ClearThisPrintBigNow(static_cast<eMessageStyle>(item - 1));
-}
-REGISTER_COMMAND_HANDLER(COMMAND_CLEAR_THIS_PRINT_BIG_NOW, ClearThisPrintBigNow);
+REGISTER_COMMAND_HANDLER(COMMAND_CLEAR_THIS_PRINT_BIG_NOW, [](uint16 item) { CMessages::ClearThisPrintBigNow(static_cast<eMessageStyle>(item - 1)); });
 
-void ClearPrints() {
-    CMessages::ClearMessages(false);
-}
-REGISTER_COMMAND_HANDLER(COMMAND_CLEAR_PRINTS, ClearPrints);
+REGISTER_COMMAND_HANDLER(COMMAND_CLEAR_PRINTS, []() { CMessages::ClearMessages(false); });
