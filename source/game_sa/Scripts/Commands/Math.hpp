4801dd9550fbd2d8f08f2cf65c00dad9a7b6b5b9
yukani <yukani@zohomail.eu>
2022-12-27 18:24:07 +0300

    fix build

--- source/game_sa/Scripts/Commands/Math.hpp
+++ source/game_sa/Scripts/Commands/Math.hpp
@@ -5,15 +5,13 @@
 * Various utility commands
 */
 
-REGISTER_COMMAND_HANDLER(COMMAND_GET_DISTANCE_BETWEEN_COORDS_2D, DistanceBetweenPoints2D);
+REGISTER_COMMAND_HANDLER(COMMAND_GET_DISTANCE_BETWEEN_COORDS_2D, [](CVector2D a, CVector2D b) { return DistanceBetweenPoints2D(a, b); });
 
-REGISTER_COMMAND_HANDLER(COMMAND_GET_DISTANCE_BETWEEN_COORDS_3D, DistanceBetweenPoints);
+REGISTER_COMMAND_HANDLER(COMMAND_GET_DISTANCE_BETWEEN_COORDS_3D, [](CVector a, CVector b) { return DistanceBetweenPoints(a, b); });
 
 REGISTER_COMMAND_HANDLER(COMMAND_GENERATE_RANDOM_INT, [](int32& var) { var = CGeneral::GetRandomNumber(); });
 
 REGISTER_COMMAND_HANDLER(COMMAND_GENERATE_RANDOM_INT_IN_RANGE, [](int32 a, int32 b) { return CGeneral::GetRandomNumberInRange(a, b); });
-
-REGISTER_COMMAND_HANDLER(COMMAND_GENERATE_RANDOM_INT_IN_RANGE, [](uint32 a, uint32 b) { return CGeneral::GetRandomNumberInRange(a, b); });
 
 REGISTER_COMMAND_HANDLER(COMMAND_GENERATE_RANDOM_FLOAT_IN_RANGE, [](float a, float b) { return CGeneral::GetRandomNumberInRange(a, b); });
 
