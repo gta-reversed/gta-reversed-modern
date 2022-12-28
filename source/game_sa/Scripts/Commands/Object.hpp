d1733bcf759c211c662d2dfeacc20af9251c547b
Pirulax <patrikjankovics7@gmail.com>
2022-12-28 02:04:46 +0100

    Some Object commands

--- source/game_sa/Scripts/Commands/Object.hpp
+++ source/game_sa/Scripts/Commands/Object.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "Object.h"
 #include "SetPieces.h"
 #include "Fx.h"
@@ -18,11 +19,53 @@ REGISTER_COMMAND_HANDLER(COMMAND_DRAW_LIGHT, DrawLight);
 
 void AddBigGunFlash(CVector origin, CVector target) {
     CPointLights::AddLight(ePointLightType::PLTYPE_POINTLIGHT, origin, {}, 5.0f, 1.0f, 0.8f, 0.0f, 0, false, nullptr);
-    g_fx.TriggerGunshot(nullptr, origin, (target - origin).Magnitude(), true);
+    g_fx.TriggerGunshot(nullptr, origin, (target - origin).Normalized(), true);
 }
 REGISTER_COMMAND_HANDLER(COMMAND_ADD_BIG_GUN_FLASH, AddBigGunFlash);
 
 void EnableDisabledAttractorOnObject(CObject& object, bool enabled) {
-    object.objectFlags.b0x1000000 = enabled; // todo: rename obj->objectFlags.b0x1000000
+    object.objectFlags.bEnableDisabledAttractors = enabled; // todo: rename obj->objectFlags.b0x1000000
 }
 REGISTER_COMMAND_HANDLER(COMMAND_ENABLE_DISABLED_ATTRACTORS_ON_OBJECT, EnableDisabledAttractorOnObject);
+
+//
+// Animation
+//
+
+void SetObjectAnimSpeed(CObject& obj, const char* animName, float speed) {
+    if (const auto anim = RpAnimBlendClumpGetAssociation(obj.m_pRwClump, animName)) {
+        anim->m_fSpeed = speed;
+    }
+}
+REGISTER_COMMAND_HANDLER(COMMAND_SET_OBJECT_ANIM_SPEED, SetObjectAnimSpeed);
+
+bool IsObjectPlayingAnim(CObject& obj, const char* animName) {
+    if (!obj.m_pRwClump) {
+        return false;
+    }
+    if (RwObjectGetType(obj.m_pRwClump) != rpCLUMP) {
+        return false;
+    }
+    if (!RpAnimBlendClumpIsInitialized(obj.m_pRwClump)) {
+        return false;
+    }
+    return RpAnimBlendClumpGetAssociation(obj.m_pRwClump, animName) != nullptr; 
+}
+REGISTER_COMMAND_HANDLER(COMMAND_IS_OBJECT_PLAYING_ANIM, IsObjectPlayingAnim);
+
+auto GetObjectAnimCurrentTime(CObject& obj, const char* animName) {
+    if (const auto anim = RpAnimBlendClumpGetAssociation(obj.m_pRwClump, animName)) {
+        return anim->m_fCurrentTime / anim->m_pHierarchy->m_fTotalTime;
+    }
+    return 0.f;
+}
+REGISTER_COMMAND_HANDLER(COMMAND_GET_OBJECT_ANIM_CURRENT_TIME, GetObjectAnimCurrentTime);
+
+auto SetObjectAnimCurrentTime(CObject& obj, const char* animName, float progress) {
+    if (const auto anim = RpAnimBlendClumpGetAssociation(obj.m_pRwClump, animName)) {
+        anim->SetCurrentTime(anim->m_fSpeed * progress);
+    }
+}
+REGISTER_COMMAND_HANDLER(COMMAND_SET_OBJECT_ANIM_CURRENT_TIME, SetObjectAnimCurrentTime);
+
+
