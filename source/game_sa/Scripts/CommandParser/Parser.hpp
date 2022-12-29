gta-reversed/gta-reversed-modern

commit c2ffcde88ff385240158877d15bfaf84c0d8b643

`Parser`: Add support for `eScriptCommands` function argument + fix value store when return value is a reference

source/game_sa/Scripts/CommandParser/Parser.hpp
@@ -11,46 +11,62 @@ namespace notsa {
 namespace script {
 namespace detail {
 template<typename T_FnRet, typename... T_FnArgs, typename... T_CollectedArgs>
-OpcodeResult CollectArgsAndCall(CRunningScript* S, T_FnRet(*CommandFn)(T_FnArgs...), T_CollectedArgs&&... args) {
+OpcodeResult CollectArgsAndCall(CRunningScript* S, eScriptCommands command, T_FnRet(*CommandFn)(T_FnArgs...), T_CollectedArgs&&... args) {
     if constexpr (sizeof...(T_CollectedArgs) == sizeof...(T_FnArgs)) { // Has it collected enough args?
-        const auto CallCommandFn = [&] {
+        const auto CallCommandFn = [&]() -> T_FnRet {
             return std::invoke(CommandFn, std::forward<T_CollectedArgs>(args)...);
         };
 
         if constexpr (std::is_same_v<T_FnRet, void>) {
             CallCommandFn(); // Returns void, nothing to push
         } else {
-            const auto ret = CallCommandFn(); 
-            if constexpr (std::is_same_v<T_FnRet, OpcodeResult>) { // Special handling for OpcodeResult's, hopefully soon to be removed (once all commands start using the parser)
-                return ret;
+            T_FnRet ret = CallCommandFn();
+            if constexpr (std::is_same_v<T_FnRet, OpcodeResult>) { 
+                return CallCommandFn();
             } else {
-                StoreArg(S, ret); // Store result to script
+                StoreArg(S, std::forward<T_FnRet>(ret)); // Store result to script
             }
         }
+
         return OR_CONTINUE;
-    } else {
-        // Not enough args, collect more.
-        try {
+    } else { // Not enough args, read more
+        using T_ToRead = nth_element_t<sizeof...(T_CollectedArgs), T_FnArgs...>;
+        const auto ContinueWithArg = [&]<typename T>(T&& arg) {
             return CollectArgsAndCall(
                 S,
+                command,
                 CommandFn,
-                std::forward<T_CollectedArgs>(args)..., // Forward read ones
-                notsa::script::Read<nth_element_t<sizeof...(T_CollectedArgs), T_FnArgs...>>(S) // Read next parameter
-            ); 
-        } catch (const notsa::script::detail::ArgReaderIgnorableException&) {
+                std::forward<T_CollectedArgs>(args)...,
+                std::forward<T>(arg)
+            );
+        };
+
+        if constexpr (std::is_same_v<T_ToRead, eScriptCommands>) { // Special case for reading the current script comamnd
+            return ContinueWithArg(command);
+        } else {
+            return ContinueWithArg(notsa::script::Read<T_ToRead>(S)); // Read next parameter and continue
+        }
+
+        // Don't remove this, we might need it!
+        //try {
+        //} catch (const std::exception&) {
             // In cases the argument parser has errored out we still
             // have to read all values before returning.
             // Reason: The IP has to be increased
-            ((void)(notsa::script::Read<T_FnArgs>(S)), ...);
-            return OR_CONTINUE;
-        }
+            //((void)(notsa::script::Read<T_FnArgs>(S)), ...);
+            //return OR_CONTINUE;
+        //}
     }
 }
+
+//! Called for unimplemented commands
+//! This originally returned `OR_INTERRUPT`, but it isn't handled specially at all, so I'd just pass our debug checks
+void NotImplemented() { NOTSA_UNREACHABLE(); }
 }; // namespace detail
 
 template<eScriptCommands Command, auto* CommandFn>
 OpcodeResult CommandParser(CRunningScript* S) {
-    return detail::CollectArgsAndCall(S, CommandFn);
+    return detail::CollectArgsAndCall(S, Command, CommandFn);
 }
 
 };

commit 09ab0700b31ceb0803ff2f8ccc796a6d396bb23d

Fix bug introduced by c2ffcde88ff

source/game_sa/Scripts/CommandParser/Parser.hpp
@@ -22,7 +22,7 @@ OpcodeResult CollectArgsAndCall(CRunningScript* S, eScriptCommands command, T_Fn
         } else {
             T_FnRet ret = CallCommandFn();
             if constexpr (std::is_same_v<T_FnRet, OpcodeResult>) { 
-                return CallCommandFn();
+                return ret;
             } else {
                 StoreArg(S, std::forward<T_FnRet>(ret)); // Store result to script
             }