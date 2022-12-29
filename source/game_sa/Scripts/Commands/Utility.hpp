91c1b96c1fcf4bfb0ad72f910129fd918c2255c2
Pirulax <patrikjankovics7@gmail.com>
2022-12-29 05:05:37 +0100

    `Utility`: Add `HighlightImportantArea`

--- source/game_sa/Scripts/Commands/Utility.hpp
+++ source/game_sa/Scripts/Commands/Utility.hpp
@@ -4,3 +4,11 @@
 * Various utility commands
 */
 
+//! Not a script command, but used in other commands
+void HighlightImportantArea(CRunningScript& S, CVector2D from, CVector2D to, float z = -100.f) {
+    CTheScripts::HighlightImportantArea(reinterpret_cast<int32>(&S) + reinterpret_cast<int32>(S.m_IP), from.x, from.y, to.x, to.y, z);
+}
+
+void HighlightImportantArea(CRunningScript& S, CVector from, CVector to) {
+    HighlightImportantArea(S, CVector2D{ from }, CVector2D{ to }, (from.z + to.z) / 2.f);
+}
