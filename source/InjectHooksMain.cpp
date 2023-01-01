2a3596393f1bcf023681c3bb2cc7c01a09de07cf
Pirulax <patrikjankovics7@gmail.com>
2023-01-01 03:40:18 +0100

    `CPedAttractorPedPlacer`

--- source/InjectHooksMain.cpp
+++ source/InjectHooksMain.cpp
@@ -105,6 +105,7 @@
 #include "Scripted2dEffects.h"
 #include "ScriptResourceManager.h"
 #include "PedAttractorManager.h"
+#include "Attractors/PedAttractorPedPlacer.h"
 #include "LoadingScreen.h"
 #include "Securom.h"
 #include "GridRef.h"
@@ -426,6 +427,7 @@ void InjectHooksMain() {
     CMenuSystem::InjectHooks();
     CCarFXRenderer::InjectHooks();
     CPedAttractorManager::InjectHooks();
+    CPedAttractorPedPlacer::InjectHooks();
     BoneNode_c::InjectHooks();
     BoneNodeManager_c::InjectHooks();
     CAnimBlendClumpData::InjectHooks();
