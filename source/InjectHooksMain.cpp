6143bf2b857c95fd81ef33d55e5969b60c0409a3
Pirulax <patrikjankovics7@gmail.com>
2023-01-03 05:59:37 +0100

    `CPedGroupPlacer` + `CPedGroupPlacer::PlaceGroup`

--- source/InjectHooksMain.cpp
+++ source/InjectHooksMain.cpp
@@ -404,8 +404,9 @@ void InjectHooksMain() {
     HookInstall(0x53E230, &Render2dStuff);   // [ImGui] This one shouldn't be reversible, it contains imgui debug menu logic, and makes game unplayable without
     HookInstall(0x541DD0, CPad::UpdatePads); // [ImGui] Changes logic of the function and shouldn't be toggled on/off
     HookInstall(0x459F70, CVehicleRecording::Render); // [ImGui] Debug stuff rendering
-    CFileMgr::InjectHooks();
 
+    CFileMgr::InjectHooks();
+    CPedGroupPlacer::InjectHooks();
     RenderBuffer::InjectHooks();
     CStaticShadow::InjectHooks();
     CRealTimeShadowManager::InjectHooks();
