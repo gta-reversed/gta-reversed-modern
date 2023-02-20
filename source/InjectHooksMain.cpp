cb54aca4e11de31e677f293287d9194ebe0d0652
Pirulax <patrikjankovics7@gmail.com>
2023-02-20 11:08:24 +0100

    `CTaskComplexInvestigateDisturbance`

--- source/InjectHooksMain.cpp
+++ source/InjectHooksMain.cpp
@@ -233,6 +233,7 @@
 #include "TaskSimpleCarSetPedOut.h"
 #include "TaskSimpleAnim.h"
 #include "TaskSimpleRunAnim.h"
+#include "Tasks/TaskTypes/TaskComplexInvestigateDisturbance.h"
 #include "TaskComplexWanderCriminal.h"
 #include "TaskComplexWanderProstitute.h"
 #include "TaskComplexWalkRoundBuildingAttempt.h"
@@ -795,7 +796,7 @@ void InjectHooksMain() {
         CTaskComplexHitResponse::InjectHooks();
         CTaskComplexInWater::InjectHooks();
         CTaskComplexInvestigateDeadPed::InjectHooks();
-        // CTaskComplexInvestigateDisturbance::InjectHooks();
+        CTaskComplexInvestigateDisturbance::InjectHooks();
         // CTaskComplexKillAllThreats::InjectHooks();
         // CTaskComplexKillCriminal::InjectHooks();
         // CTaskComplexKillPedGroupOnFoot::InjectHooks();
