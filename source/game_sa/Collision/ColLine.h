12f579d102f911f74e123a4c36e5fb4cbfc9687d
Pirulax <patrikjankovics7@gmail.com>
2023-02-24 19:25:27 +0100

    `CColLine::IsVertical`

--- source/game_sa/Collision/ColLine.h
+++ source/game_sa/Collision/ColLine.h
@@ -31,7 +31,10 @@ public:
     * @param color     Color of the lines used
     */
     void DrawWireFrame(CRGBA color, const CMatrix& transform) const;
-    
+
+    //! Is the line vertical
+    bool IsVertical() const { return CVector2D{ m_vecStart } == CVector2D{ m_vecEnd }; }
+
 public:
     CVector m_vecStart;
     float   m_fStartSize;
