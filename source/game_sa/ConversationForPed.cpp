08dbd5afed974e3baf1dd0c15dea5d7b749ba3b9
yukani <yukani@zohomail.eu>
2023-02-28 18:04:09 +0300

    CConversationForPed::Update

--- source/game_sa/ConversationForPed.cpp
+++ source/game_sa/ConversationForPed.cpp
@@ -7,13 +7,100 @@ void CConversationForPed::InjectHooks() {
     RH_ScopedCategory("Conversations");
 
     RH_ScopedInstall(Clear, 0x43A770);
-    RH_ScopedInstall(Update, 0x43C190, {.reversed = false});
+    RH_ScopedInstall(Update, 0x43C190);
     RH_ScopedInstall(IsPlayerInPositionForConversation, 0x43AC40);
 }
 
 // 0x43C190
 void CConversationForPed::Update() {
+    if (!m_Ped || !m_bEnabled || !IsPlayerInPositionForConversation(false)) {
+        m_Status = 0;
+        return;
+    }
 
+    const auto pad = CPad::GetPad();
+    if (CTimer::GetTimeInMS() > m_LastTimeWeWereCloseEnough + 1000 || m_Status == 1 &&
+        (CTimer::GetTimeInMS() > m_LastChange + 4000 || pad->ConversationYesJustDown() || pad->ConversationNoJustDown())) {
+
+        assert(GetCurrentNode());
+        if (!m_bSuppressSubtitles) {
+            CMessages::ClearSmallMessagesOnly();
+            CMessages::AddMessageJumpQ(TheText.Get(GetCurrentNode()->m_Name), 4000, 1, true);
+        }
+
+        if (GetCurrentNode()->m_Speech > 0) {
+            m_Ped->Say(GetCurrentNode()->m_Speech);
+        }
+
+        if (GetCurrentNode()->m_Speech < 0) {
+            CConversations::AwkwardSay(-GetCurrentNode()->m_Speech, m_Ped);
+        }
+
+        m_Status = 2;
+        m_LastChange = CTimer::GetTimeInMS();
+    } else if (CTimer::GetTimeInMS() > m_LastChange + 400) {
+        switch (m_Status) {
+        case 0:
+        case 2:
+        case 3: {
+            assert(GetCurrentNode());
+            if (pad->ConversationNoJustDown() && GetCurrentNode()->m_NodeNo >= 0) {
+                if (!m_bSuppressSubtitles) {
+                    CMessages::ClearSmallMessagesOnly();
+                    strcpy_s(gString, GetCurrentNode()->m_Name);
+                    gString[strlen(gString)] = 'N';
+                    CMessages::AddMessageJumpQ(gString, 4000, 3, true);
+                }
+
+                const auto player = FindPlayerPed();
+                if (GetCurrentNode()->m_SpeechN > 0) {
+                    player->Say(GetCurrentNode()->m_SpeechN);
+                }
+
+                if (GetCurrentNode()->m_SpeechN < 0) {
+                    CConversations::AwkwardSay(-GetCurrentNode()->m_SpeechN, player);
+                }
+
+                m_CurrentNode = GetCurrentNode()->m_NodeNo;
+                m_LastChange = CTimer::GetTimeInMS();
+
+                if (!m_bSuppressSubtitles) {
+                    CMessages::AddMessageJumpQ(TheText.Get(GetCurrentNode()->m_Name), 4000, 1, true);
+                }
+                m_Status = 1;
+            } else if (pad->ConversationYesJustDown() && GetCurrentNode()->m_NodeYes >= 0) {
+                if (!m_bSuppressSubtitles) {
+                    CMessages::ClearSmallMessagesOnly();
+                    strcpy_s(gString, GetCurrentNode()->m_Name);
+                    gString[strlen(gString)] = 'Y';
+                    CMessages::AddMessageJumpQ(gString, 4000, 3, true);
+                }
+
+                const auto player = FindPlayerPed();
+                if (GetCurrentNode()->m_SpeechY > 0) {
+                    player->Say(GetCurrentNode()->m_SpeechY);
+                }
+
+                if (GetCurrentNode()->m_SpeechY < 0) {
+                    CConversations::AwkwardSay(-GetCurrentNode()->m_SpeechY, player);
+                }
+
+                m_CurrentNode = GetCurrentNode()->m_NodeYes;
+                m_LastChange = CTimer::GetTimeInMS();
+
+                if (!m_bSuppressSubtitles) {
+                    CMessages::AddMessageJumpQ(TheText.Get(GetCurrentNode()->m_Name), 4000, 1, true);
+                }
+                m_Status = 1;
+            }
+            break;
+        }
+        }
+    }
+    m_LastTimeWeWereCloseEnough = CTimer::GetTimeInMS();
+    if (m_Status == 2 && CTimer::GetTimeInMS() > m_LastChange + 4000) {
+        m_Status = 3;
+    }
 }
 
 // 0x43AC40
