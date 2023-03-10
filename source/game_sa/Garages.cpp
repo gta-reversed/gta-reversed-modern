09c72233bf5084f7b19c0b645f1df240df4dc8c6
Pirulax <patrikjankovics7@gmail.com>
2023-03-10 22:58:36 +0100

    Hook `CountCarsInHideoutGarage`

--- source/game_sa/Garages.cpp
+++ source/game_sa/Garages.cpp
@@ -31,7 +31,7 @@ void CGarages::InjectHooks() {
     RH_ScopedInstall(PrintMessages, 0x447790);
     RH_ScopedInstall(ChangeGarageType, 0x4476D0);
     RH_ScopedInstall(GetGarageNumberByName, 0x447680);
-    RH_ScopedInstall(CountCarsInHideoutGarage, 0x44A210, {.reversed = false});
+    RH_ScopedInstall(CountCarsInHideoutGarage, 0x44A210);
     // RH_ScopedInstall(Load, 0x5D3270, true); // bad
     // RH_ScopedInstall(Save, 0x5D3160, true); // possible bad
 }
