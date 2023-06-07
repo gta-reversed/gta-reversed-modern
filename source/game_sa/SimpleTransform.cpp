b28b87722fbec9a8e640ca03d5c582cd3e3a79d8
plakapenka <plakapenok1@gmail.com>
2023-06-07 18:46:49 +0300

    requested changes CSimpleTransform

--- source/game_sa/SimpleTransform.cpp
+++ source/game_sa/SimpleTransform.cpp
@@ -9,39 +9,36 @@
 
 #include "SimpleTransform.h"
 
+// 0x54EF40
 void CSimpleTransform::UpdateRwMatrix(RwMatrix* out)
 {
-    const float heading = this->m_fHeading;
+    const float heading = m_fHeading;
     const float sinHeading = sinf(heading);
     const float cosHeading = cosf(heading);
 
     out->right = { cosHeading, sinHeading, 0.0 };
     out->up = { -sinHeading, cosHeading, 0.0 };
     out->at = { 0.0, 0.0, 1.0 };
-    out->pos = this->m_vPosn;
+    out->pos = m_vPosn;
 
     RwMatrixUpdate(out);
-
-  //  ((void(__thiscall*)(CSimpleTransform*, RwMatrix*))0x54EF40)(this, out);
 }
 
+// 0x54EF90
 void CSimpleTransform::Invert(const CSimpleTransform& base)
 {
     const float cosHeading = cosf(base.m_fHeading);
     const float sinHeading = sinf(base.m_fHeading);
 
-    this->m_vPosn.x = -(cosHeading * base.m_vPosn.x) - (sinHeading * base.m_vPosn.y);
-    this->m_vPosn.y = (sinHeading * base.m_vPosn.x) - (cosHeading * base.m_vPosn.y);
-    this->m_vPosn.z = -base.m_vPosn.z;
-    this->m_fHeading = -base.m_fHeading;
-
-  //  ((void(__thiscall*)(CSimpleTransform*, const CSimpleTransform&))0x54EF90)(this, base);
+    m_vPosn.x = -(cosHeading * base.m_vPosn.x) - (sinHeading * base.m_vPosn.y);
+    m_vPosn.y = (sinHeading * base.m_vPosn.x) - (cosHeading * base.m_vPosn.y);
+    m_vPosn.z = -base.m_vPosn.z;
+    m_fHeading = -base.m_fHeading;
 }
 
+// 0x54F1B0
 void CSimpleTransform::UpdateMatrix(CMatrix* out)
 {
     out->SetTranslate(m_vPosn);
     out->SetRotateZOnly(m_fHeading);
-
-   // ((void(__thiscall*)(CSimpleTransform*, class CMatrix*))0x54F1B0)(this, out);
 }
