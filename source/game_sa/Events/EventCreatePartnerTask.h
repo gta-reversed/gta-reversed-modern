b49e51256699601369f4d00e2f22333ca76db9ee
Pirulax <patrikjankovics7@gmail.com>
2023-08-17 21:58:14 +0200

    Format `CEventCreatePartnerTask`

--- source/game_sa/Events/EventCreatePartnerTask.h
+++ source/game_sa/Events/EventCreatePartnerTask.h
@@ -4,19 +4,19 @@
 
 class NOTSA_EXPORT_VTABLE CEventCreatePartnerTask : public CEvent {
 public:
-    int32 m_randomNumber;
+    int32 m_type;
     CPed* m_partner;
-    bool  m_leadSpeaker;
-    float m_distanceMultiplier;
+    bool  m_isLeadSpeaker;
+    float m_meetDist;
 
 public:
-    CEventCreatePartnerTask(int32 randomNumber, CPed* partner, bool leadSpeaker, float distanceMultiplier);
+    CEventCreatePartnerTask(int32 type, CPed* partner, bool isLeadSpeaker, float meetDist);
     ~CEventCreatePartnerTask() override;
 
-    eEventType GetEventType() const override { return EVENT_CREATE_PARTNER_TASK; }
-    int32 GetEventPriority() const override { return 6; }
-    int32 GetLifeTime() override { return 0; }
-    CEvent* Clone() override { return new CEventCreatePartnerTask(m_randomNumber, m_partner, m_leadSpeaker, m_distanceMultiplier); }
-    bool AffectsPed(CPed* ped) override { return true; }
+    eEventType GetEventType()        const override { return EVENT_CREATE_PARTNER_TASK; }
+    int32      GetEventPriority()    const override { return 6; }
+    int32      GetLifeTime()               override { return 0; }
+    CEvent*    Clone()                     override { return new CEventCreatePartnerTask(m_type, m_partner, m_isLeadSpeaker, m_meetDist); }
+    bool       AffectsPed(CPed* ped)       override { return true; }
 };
 VALIDATE_SIZE(CEventCreatePartnerTask, 0x1C);
