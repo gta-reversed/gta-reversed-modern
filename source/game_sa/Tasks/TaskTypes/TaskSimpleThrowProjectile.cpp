2f4fd3a2ed0f1505949af795e95cca280ba1e3e9
Pirulax <patrikjankovics7@gmail.com>
2023-10-07 22:54:15 +0200

    íCTaskSimpleThrowProjectileí

--- source/game_sa/Tasks/TaskTypes/TaskSimpleThrowProjectile.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskSimpleThrowProjectile.cpp
@@ -2,46 +2,119 @@
 
 #include "TaskSimpleThrowProjectile.h"
 
+void CTaskSimpleThrowProjectile::InjectHooks() {
+    RH_ScopedVirtualClass(CTaskSimpleThrowProjectile, 0x86d790, 9);
+    RH_ScopedCategory("Tasks/TaskTypes");
+
+    RH_ScopedInstall(Constructor, 0x61F660);
+    RH_ScopedInstall(Destructor, 0x61F700);
+
+    RH_ScopedInstall(ControlThrow, 0x61F810);
+    RH_ScopedInstall(StartAnim, 0x6259E0);
+    RH_ScopedInstall(FinishAnimThrowProjectileCB, 0x61F890);
+
+    RH_ScopedVMTInstall(Clone, 0x623030);
+    RH_ScopedVMTInstall(GetTaskType, 0x61F6F0);
+    RH_ScopedVMTInstall(MakeAbortable, 0x61F780);
+    RH_ScopedVMTInstall(ProcessPed, 0x62AF50);
+}
+
 // 0x61F660
-CTaskSimpleThrowProjectile::CTaskSimpleThrowProjectile(CEntity* target, CVector posn) : CTaskSimple() {
-    plugin::CallMethod<0x61F660, CTaskSimpleThrowProjectile*, CEntity*, CVector>(this, target, posn);
+CTaskSimpleThrowProjectile::CTaskSimpleThrowProjectile(CEntity* targetEntity, CVector targetPos) :
+    m_TargetPos{ targetPos },
+    m_TargetEntity{ targetEntity },
+    m_ButtonCounter{ CTimer::GetTimeInMS() }
+{
+    CEntity::SafeRegisterRef(m_TargetEntity);
 }
 
+// 0x623030
+CTaskSimpleThrowProjectile::CTaskSimpleThrowProjectile(const CTaskSimpleThrowProjectile& o) :
+    CTaskSimpleThrowProjectile{ o.m_TargetEntity, o.m_TargetPos }
+{
+}
+
+// 0x61F700
 CTaskSimpleThrowProjectile::~CTaskSimpleThrowProjectile() {
-    if (m_pAnim) {
-        m_pAnim->SetFinishCallback(CDefaultAnimCallback::DefaultAnimCB, nullptr);
-        m_pAnim = nullptr;
+    if (m_Anim) {
+        m_Anim->SetDefaultFinishCallback();
+        m_Anim = nullptr;
     }
-    CEntity::SafeCleanUpRef(m_pTarget);
+    CEntity::SafeCleanUpRef(m_TargetEntity);
 }
-
 
 // 0x61F810
-bool CTaskSimpleThrowProjectile::ControlThrow(bool bUpdateStartTime, CEntity* entity, CVector* entity2) {
-    return plugin::CallMethodAndReturn<bool, 0x61F810, CTaskSimpleThrowProjectile*, bool, CEntity*, CVector*>(this, bUpdateStartTime, entity, entity2);
+bool CTaskSimpleThrowProjectile::ControlThrow(bool bButtonReleased, CEntity* targetEntity, const CVector* targetPos) {
+    if (m_bIsFinished) {
+        return true;
+    }
+
+    if (bButtonReleased && !m_bButtonReleased) {
+        m_ButtonCounter = CTimer::GetTimeInMS() - m_ButtonCounter;
+        m_bButtonReleased = true;
+    }
+
+    if (targetEntity && m_TargetEntity != targetEntity) {
+        CEntity::ChangeEntityReference(m_TargetEntity, targetEntity);
+    }
+
+    if (targetPos) {
+        m_TargetPos = targetPos;
+    }
+
+    return false;
+}
+
+// 0x61F890
+void CTaskSimpleThrowProjectile::FinishAnimThrowProjectileCB(CAnimBlendAssociation* anim, void* data) {
+    const auto self = static_cast<CTaskSimpleThrowProjectile*>(data);
+
+    self->m_Anim = nullptr;
+    if (self->m_bStartThrowFinished) {
+        self->m_bIsFinished = true;
+    } else {
+        self->m_bStartThrowFinished = true;
+    }
+}
+
+// 0x6259E0
+void CTaskSimpleThrowProjectile::StartAnim(CPed* ped) {
+    const auto StartAnim = [&, this](AnimationId animId, float blendDelta) {
+        VERIFY(m_Anim = CAnimManager::BlendAnimation(ped->m_pRwClump, ped->GetActiveWeapon().GetWeaponInfo().m_eAnimGroup, animId, blendDelta));
+        m_Anim->SetFinishCallback(FinishAnimThrowProjectileCB, this);
+    };
+
+    if (!m_bStartThrowFinished) {
+        StartAnim(ANIM_ID_GRENADE_WEAPON_START_THROW, 16.f);
+    } else {
+        const auto IsPtCloseToPed = [ped](CVector pos) { return (pos - ped->GetPosition()).SquaredMagnitude() <= sq(10.f); };
+        if (   (!m_TargetEntity || IsPtCloseToPed(m_TargetEntity->GetPosition()))
+            || (m_TargetPos.IsZero() || IsPtCloseToPed(m_TargetPos))
+            || (m_bButtonReleased || ped->GetActiveWeapon().GetType() == WEAPON_REMOTE_SATCHEL_CHARGE)
+        ) {
+            StartAnim(ANIM_ID_GRENADE_WEAPON_THROW, 1000.f);
+        } else {
+            StartAnim(ANIM_ID_GRENADE_WEAPON_THROWU, 1000.f); // "U" as in "UP?
+        }
+    }
 }
 
 // 0x61F780
 bool CTaskSimpleThrowProjectile::MakeAbortable(CPed* ped, eAbortPriority priority, const CEvent* event) {
-    return plugin::CallMethodAndReturn<bool, 0x61F780, CTaskSimpleThrowProjectile*, CPed*, eAbortPriority, const CEvent*>(this, ped, priority, event);
-
-    // untested
-    if (priority == ABORT_PRIORITY_URGENT) {
-        if (m_pAnim) {
-            m_pAnim->SetFinishCallback(CDefaultAnimCallback::DefaultAnimCB, nullptr);
-            m_pAnim->m_fBlendDelta = -1000.0f;
-            m_pAnim = nullptr;
-            return true;
+    if (priority == ABORT_PRIORITY_IMMEDIATE) {
+        if (m_Anim) {
+            m_Anim->SetDefaultFinishCallback();
+            m_Anim->m_fBlendDelta = -1000.0f;
+            m_Anim                = nullptr;
         }
-    } else if (event && event->GetEventType() == EVENT_DAMAGE) {
-        const auto* eventDamage = static_cast<const CEventDamage*>(event);
-        if (eventDamage->m_damageResponse.m_bHealthZero && eventDamage->m_bAddToEventGroup || eventDamage->m_bKnockOffPed) {
-            if (m_pAnim) {
-                m_pAnim->SetFinishCallback(CDefaultAnimCallback::DefaultAnimCB, nullptr);
-                m_pAnim->m_fBlendDelta = -4.0f;
-                m_pAnim = nullptr;
+    } else if (const auto eDmg = CEvent::DynCast<const CEventDamage>(event)) {
+        if (eDmg->m_damageResponse.m_bHealthZero && eDmg->m_bAddToEventGroup || eDmg->m_bKnockOffPed) {
+            if (m_Anim) {
+                m_Anim->SetDefaultFinishCallback();
+                m_Anim->m_fBlendDelta = -4.0f;
+                m_Anim                = nullptr;
             }
-            m_bIsAborting = true;
+            m_bIsFinished = true;
         }
     }
     return true;
@@ -49,16 +122,45 @@ bool CTaskSimpleThrowProjectile::MakeAbortable(CPed* ped, eAbortPriority priorit
 
 // 0x62AF50
 bool CTaskSimpleThrowProjectile::ProcessPed(CPed* ped) {
-    return plugin::CallMethodAndReturn<bool, 0x62AF50, CTaskSimpleThrowProjectile*, CPed*>(this, ped);
-}
+    if (m_bIsFinished) {
+        return true;
+    }
 
-void CTaskSimpleThrowProjectile::InjectHooks() {
-    RH_ScopedClass(CTaskSimpleThrowProjectile);
-    RH_ScopedCategory("Tasks/TaskTypes");
-    using namespace ReversibleHooks;
-}
+    const auto wi = &ped->GetActiveWeapon().GetWeaponInfo(ped);
 
-// 0x61F660
-CTaskSimpleThrowProjectile* CTaskSimpleThrowProjectile::Constructor(CEntity* pTarget, CVector Posn) {
-    return plugin::CallMethodAndReturn<CTaskSimpleThrowProjectile*, 0x61F660, CTaskSimpleThrowProjectile*, CEntity*, CVector>(this, pTarget, Posn);
+    if (!wi->flags.bThrow) {
+        return true;
+    }
+
+    if (!m_Anim) {
+        StartAnim(ped);
+        return false;
+    }
+
+    switch (m_Anim->m_nAnimId) {
+    case ANIM_ID_GRENADE_WEAPON_THROW:
+    case ANIM_ID_GRENADE_WEAPON_THROWU:
+        const auto animFireTime = m_Anim->m_nAnimId == ANIM_ID_GRENADE_WEAPON_THROW
+            ? wi->m_fAnimLoop2Fire
+            : wi->m_fAnimLoopFire;
+        if (animFireTime < m_Anim->m_fCurrentTime && (m_Anim->m_fCurrentTime - m_Anim->m_fTimeStep) <= animFireTime && m_Anim->IsRunning()) {
+            if (ped->IsPlayer()) {
+                if (!m_bButtonReleased) {
+                    m_ButtonCounter = CTimer::GetTimeInMS() - m_ButtonCounter;
+                }
+                m_ButtonCounter = std::min(m_ButtonCounter, 533u);
+                ped->m_pPlayerData->m_fAttackButtonCounter = (float)m_ButtonCounter / 20.f;
+            }
+
+            if (m_TargetEntity) {
+                m_TargetPos = m_TargetEntity->GetPosition();
+            }
+
+            CVector firePos;
+            RwV3dTransformPoint(&firePos, &wi->m_vecFireOffset, &ped->GetBoneMatrix((ePedBones)ped->m_apBones[PED_NODE_RIGHT_HAND]->m_nNodeId));
+            ped->GetActiveWeapon().Fire(ped, &firePos, &firePos, nullptr, m_TargetPos.IsZero() ? nullptr : &m_TargetPos, nullptr);
+        }
+    }
+
+    return false;
 }
