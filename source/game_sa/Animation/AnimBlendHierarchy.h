c46ad7cc6db17808fad648b37533f056f1f99f41
Pirulax <patrikjankovics7@gmail.com>
2024-01-03 02:40:33 +0100

    Some fixes in `CAnimBlendHierarchy`

--- source/game_sa/Animation/AnimBlendHierarchy.h
+++ source/game_sa/Animation/AnimBlendHierarchy.h
@@ -25,25 +25,45 @@ public:
 
     void Shutdown();
 
-    uint8* AllocSequenceBlock(bool compressed);
+    uint8* AllocSequenceBlock(bool compressed) const;
 
-    void CalcTotalTime();
-    void CalcTotalTimeCompressed();
+    //! @addr 0x4CF2F0
+    void CalcTotalTime() { ICalcTotalTime<false>(); }
+
+    //! @addr 0x4CF3E0
+    void CalcTotalTimeCompressed() { ICalcTotalTime<true>(); }
 
     void RemoveAnimSequences();
-    void RemoveQuaternionFlips();
+    void RemoveQuaternionFlips() const;
     void RemoveUncompressedData();
 
     void SetName(const char* string);
     void Uncompress();
 
-    CAnimBlendSequence* FindSequence(const char* name);
-    void* GetSequenceBlock();
-    void CompressKeyframes();
+    CAnimBlendSequence* FindSequence(const char* name) const;
+    void* GetSequenceBlock() const;
+    void CompressKeyframes() const;
     void MoveMemory();
     void Print();
 
     auto GetSequences() const { return std::span{ m_pSequences, (size_t)m_nSeqCount }; }
+
+private: // Function implementations
+    template<bool Compressed>
+    void ICalcTotalTime() {
+        m_fTotalTime = 0.0f;
+        for (auto& sequence : GetSequences()) {
+            if (sequence.m_FramesNum == 0) { // FIX_BUGS by Mitchell Tobass
+                continue;
+            }
+            m_fTotalTime = std::max<float>(m_fTotalTime, sequence.GetKeyFrame<Compressed>(sequence.m_FramesNum - 1)->DeltaTime);
+            for (auto j = sequence.m_FramesNum - 1; j >= 1; j--) {
+                const auto kf1 = sequence.GetKeyFrame<Compressed>(j);
+                const auto kf2 = sequence.GetKeyFrame<Compressed>(j - 1);
+                kf1->DeltaTime = kf1->DeltaTime - kf2->DeltaTime; // TODO/NOTE: With `FixedFloat` this has unncesessary conversions float <=> int
+            }
+        }
+    }
 
 private:
     friend void InjectHooksMain();
