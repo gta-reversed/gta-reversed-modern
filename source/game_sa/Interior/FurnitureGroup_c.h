797705786f90edf91024e1c224091926d4d78a65
iAmir <hhm6@yahoo.com>
2024-01-23 02:46:43 +0330

    `FurnitureGroup_c`

--- source/game_sa/Interior/FurnitureGroup_c.h
+++ source/game_sa/Interior/FurnitureGroup_c.h
@@ -18,10 +18,10 @@ public:
     ~FurnitureGroup_c() = default; // 0x590DF0
 
     int32 Init();
-    int32 Exit();
-    bool AddSubGroup(int32 subgroupId, int32 minWidth, int32 minDepth, int32 maxWidth, int32 maxDepth, uint8 canPlaceInFrontOfWindow, bool isTall, bool canSteal);
-    Furniture_c* GetFurniture(int32 subGroupId, int16, uint8);
-    int32 GetRandomId(int32 subgroupId, uint8 a2);
-    bool AddFurniture(int32 subgroupId, uint16 modelId, int16 id, uint8 wealthMin, uint8 wealthMax, uint8 maxAng);
+    void Exit();
+    bool AddSubGroup(int32 subGroupId, int32 minWidth, int32 minDepth, int32 maxWidth, int32 maxDepth, uint8 canPlaceInFrontOfWindow, bool isTall, bool canSteal);
+    Furniture_c* GetFurniture(int32 subGroupId, int16 refId, uint8 rating);
+    int32 GetRandomId(int32 subGroupId, uint8 rating);
+    bool AddFurniture(int32 subGroupId, uint16 modelId, int16 id, uint8 wealthMin, uint8 wealthMax, uint8 maxAng);
 };
 VALIDATE_SIZE(FurnitureGroup_c, 0xC);
