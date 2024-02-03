180a2fe431cd22da71a737aa6c6d6c5f30645992
iAmir <hhm6@yahoo.com>
2024-02-04 01:03:44 +0330

    `FurnitureManager_c` & `FurnitureGroup_c` (#682)
    * `notsa::make_mapping`
    * `notsa::find_value_or`
    * `notsa::find_value`
    * Refactor code
    * Improve encapsulation
    
    ---------
    
    Co-authored-by: Pirulax <patrikjankovics7@gmail.com>

--- source/game_sa/Interior/FurnitureSubGroup_c.h
+++ source/game_sa/Interior/FurnitureSubGroup_c.h
@@ -8,13 +8,6 @@ class Furniture_c;
 
 class FurnitureSubGroup_c : public ListItem_c<FurnitureSubGroup_c> {
 public:
-    int32         m_nSubgroupId;
-    TList_c<void> m_Furnitures;
-    bool          m_bCanPlaceInFrontOfWindow;
-    bool          m_bIsTall;
-    bool          m_bCanSteal;
-
-public:
     static void InjectHooks();
 
     FurnitureSubGroup_c() = default;  // 0x590E20
@@ -23,5 +16,13 @@ public:
     bool AddFurniture(uint16 modelId, int16 id, uint8 wealthMin, uint8 wealthMax, uint8 maxAng);
     Furniture_c* GetFurniture(int16 id, uint8 wealth);
     int32 GetRandomId(uint8 a2);
+    void Exit();
+
+public:
+    int32                m_SubGroupId;
+    TList_c<Furniture_c> m_Furnitures;
+    bool                 m_bCanPlaceInFrontOfWindow;
+    bool                 m_bIsTall;
+    bool                 m_bCanSteal;
 };
 VALIDATE_SIZE(FurnitureSubGroup_c, 0x1C);
