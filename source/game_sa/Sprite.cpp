094fcf7a215638b480c1a4a91245f5799cee94c0
yukani <yukani@zohomail.eu>
2024-03-03 17:10:29 +0300

    sprite further

--- source/game_sa/Sprite.cpp
+++ source/game_sa/Sprite.cpp
@@ -2,6 +2,7 @@
 #include "Sprite.h"
 
 static inline auto& nSpriteBufferIndex = StaticRef<int32>(0xC6A158);
+static inline auto& s_XLUSpriteVertices = StaticRef<std::array<RwIm2DVertex, 4>>(0xC4B8E0);
 
 void CSprite::InjectHooks() {
     RH_ScopedClass(CSprite);
@@ -12,12 +13,12 @@ void CSprite::InjectHooks() {
     RH_ScopedInstall(FlushSpriteBuffer, 0x70CF20);
     RH_ScopedInstall(CalcScreenCoors, 0x70CE30);
     RH_ScopedInstall(CalcHorizonCoors, 0x70E3E0);
-    // RH_ScopedOverloadedInstall(Set4Vertices2D, "0", 0x70E1C0, void (*)(RwD3D9Vertex*, const CRect&, const CRGBA&, const CRGBA&, const CRGBA&, const CRGBA&));
+    RH_ScopedOverloadedInstall(Set4Vertices2D, "CRect", 0x70E1C0, void (*)(RwIm2DVertex*, const CRect&, const CRGBA&, const CRGBA&, const CRGBA&, const CRGBA&));
     // RH_ScopedOverloadedInstall(Set4Vertices2D, "1", 0x70E2D0, void (*)(RwD3D9Vertex*, float, float, float, float, float, float, float, float, const CRGBA&, const CRGBA&, const CRGBA&, const CRGBA&));
     RH_ScopedInstall(RenderOneXLUSprite, 0x70D000, { .reversed = false });
     RH_ScopedInstall(RenderOneXLUSprite_Triangle, 0x70D320, { .reversed = false });
     RH_ScopedInstall(RenderOneXLUSprite_Rotate_Aspect, 0x70D490, { .reversed = false });
-    RH_ScopedInstall(RenderOneXLUSprite2D, 0x70F540, { .reversed = false });
+    RH_ScopedInstall(RenderOneXLUSprite2D, 0x70F540);
     RH_ScopedInstall(RenderBufferedOneXLUSprite, 0x70E4A0, { .reversed = false });
     RH_ScopedInstall(RenderBufferedOneXLUSprite_Rotate_Aspect, 0x70E780, { .reversed = false });
     RH_ScopedInstall(RenderBufferedOneXLUSprite_Rotate_Dimension, 0x70EAB0, { .reversed = false });
@@ -109,8 +110,32 @@ float CSprite::CalcHorizonCoors() {
 }
 
 // 0x70E1C0
-void CSprite::Set4Vertices2D(RwD3D9Vertex*, const CRect&, const CRGBA&, const CRGBA&, const CRGBA&, const CRGBA&) {
-    assert(false);
+void CSprite::Set4Vertices2D(RwIm2DVertex* verts, const CRect& rt, const CRGBA& topLeftColor, const CRGBA& topRightColor, const CRGBA& bottomLeftColor, const CRGBA& bottomRightColor) {
+    for (auto i = 0u; i < 4u; i++) {
+        auto& vert = verts[i];
+
+        vert.x = (i == 0 || i == 3) ? rt.left : rt.right;
+        vert.y = (i == 0 || i == 1) ? rt.bottom : rt.top;
+        vert.z = m_f2DNearScreenZ;
+        vert.u = (i == 0 || i == 3) ? 0.0f : 1.0f;
+        vert.v = (i == 0 || i == 1) ? 0.0f : 1.0f;
+        vert.rhw = m_fRecipNearClipPlane;
+
+        vert.emissiveColor = [&] {
+            switch (i) {
+            case 0:
+                return bottomLeftColor.ToInt();
+            case 1:
+                return bottomRightColor.ToInt();
+            case 2:
+                return topRightColor.ToInt();
+            case 3:
+                return topLeftColor.ToInt();
+            default:
+                NOTSA_UNREACHABLE();
+            }
+        }();
+    }
 }
 
 // unused
@@ -147,8 +172,24 @@ void CSprite::RenderOneXLUSprite_Rotate_2Colours(float, float, float, float, flo
 }
 
 // 0x70F540
-void CSprite::RenderOneXLUSprite2D(float, float, float, float, const RwRGBA&, int16, uint8) {
-    assert(false);
+void CSprite::RenderOneXLUSprite2D(CVector2D screen, CVector2D size, const CRGBA& color, int16 intensity, uint8 alpha) {
+    CRGBA vertsColor{};
+    for (auto i = 0; i < 4; i++) {
+        vertsColor[i] = static_cast<uint8>((intensity * color[i]) >> 8);
+    }
+
+    Set4Vertices2D(
+        s_XLUSpriteVertices.data(),
+        { screen.x - size.x, screen.y - size.y, screen.x + size.x, screen.y + size.y },
+        vertsColor,
+        vertsColor,
+        vertsColor,
+        vertsColor
+    );
+
+    RwRenderStateSet(rwRENDERSTATEZTESTENABLE, RWRSTATE(false));
+    RwIm2DRenderPrimitive(rwPRIMTYPETRIFAN, s_XLUSpriteVertices.data(), 4);
+    RwRenderStateSet(rwRENDERSTATEZTESTENABLE, RWRSTATE(true));
 }
 
 // unused
--- source/game_sa/Sprite.h
+++ source/game_sa/Sprite.h
@@ -23,15 +23,15 @@ public:
     static bool  CalcScreenCoors(const RwV3d& posn, RwV3d* out, float* w, float* h, bool checkMaxVisible, bool checkMinVisible);
     static float CalcHorizonCoors();
 
-    static void Set4Vertices2D(RwD3D9Vertex*, const CRect&, const CRGBA&, const CRGBA&, const CRGBA&, const CRGBA&);
-    static void Set4Vertices2D(RwD3D9Vertex*, float, float, float, float, float, float, float, float, const CRGBA&, const CRGBA&, const CRGBA&, const CRGBA&);
+    static void Set4Vertices2D(RwIm2DVertex* verts, const CRect& rt, const CRGBA& topLeftColor, const CRGBA& topRightColor, const CRGBA& bottomLeftColor, const CRGBA& bottomRightColor);
+    static void Set4Vertices2D(RwIm2DVertex* verts, float, float, float, float, float, float, float, float, const CRGBA&, const CRGBA&, const CRGBA&, const CRGBA&);
 
     static void RenderOneXLUSprite(CVector pos, CVector2D halfSize, uint8 r, uint8 g, uint8 b, int16 intensity, float rhw, uint8 a, uint8 udir, uint8 vdir);
     static void RenderOneXLUSprite_Triangle(float, float, float, float, float, float, float, uint8, uint8, uint8, int16, float, uint8);
     static void RenderOneXLUSprite_Rotate_Aspect(CVector pos, CVector2D size, uint8 r, uint8 g, uint8 b, int16 intensity, float rz, float rotation, uint8 alpha);
     static void RenderOneXLUSprite_Rotate_Dimension(float, float, float, float, float, uint8, uint8, uint8, int16, float, float, uint8);
     static void RenderOneXLUSprite_Rotate_2Colours(float, float, float, float, float, uint8, uint8, uint8, uint8, uint8, uint8, float, float, float, float, uint8);
-    static void RenderOneXLUSprite2D(float, float, float, float, const RwRGBA&, int16, uint8);
+    static void RenderOneXLUSprite2D(CVector2D screen, CVector2D size, const CRGBA& color, int16 intensity, uint8 alpha);
     static void RenderOneXLUSprite2D_Rotate_Dimension(float, float, float, float, const RwRGBA&, int16, float, uint8);
 
     static void RenderBufferedOneXLUSprite(CVector pos, CVector2D size, uint8 r, uint8 g, uint8 b, int16 intensity, float recipNearZ, uint8 a11);
