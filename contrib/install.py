26ab0967994cb3106c7bffe2654c6837bf29f356
Vladik01-11 <denjidananaa@bk.ru>
2024-06-18 23:41:48 +0300

    Added comments in `install.py `

--- contrib/install.py
+++ contrib/install.py
@@ -17,7 +17,7 @@ def modern():
     git_repo_root = Path.cwd()
     if git_repo_root.parts[-1] == 'contrib':
         git_repo_root = git_repo_root.parent
-    
+
     gta = tkFileDialog.askopenfilename(
         title='Select the executable file from the GTA:SA installation directory'
     )
@@ -28,12 +28,12 @@ def modern():
         return modern()
 
     gta_scripts_dir = gta_root_dir / 'scripts'
-    
+
     # Unpack zip into gta dir
     print("Unpacking `plugins.zip` into GTA:SA root directory...")
     with zipfile.ZipFile(git_repo_root / 'contrib' / 'plugins.zip') as plugins_zip:
         plugins_zip.extractall(gta_root_dir)
-    
+
     # Create symlinks
     config_name = input("Choose configuration to link (debug/release, default: debug): ") or 'debug'
     print(f"Creating the symlinks for `{config_name}` configuration...")
@@ -43,20 +43,22 @@ def modern():
         dst = gta_scripts_dir / filename
         dst.unlink(missing_ok=True) # Delete symlink if it already exists
         # This fails [WinError 1314] if the script isn't run with admin rights [softlinks require it]
-        # To run as admin just open an admin cmd, and type `python ./install.py` in the repo root dir
         os.symlink(config_bin_dir / filename, dst)
 
     setting_file = git_repo_root / 'build' / 'gta_reversed.vcxproj.user'
+    # We check the availability of the project through the availability of parameters for gta_reversed
     if os.path.exists(setting_file):
         try:
             with open(setting_file) as file:
                 content = file.read()
-        
+
+            # We substitute the current parameters for debugging, namely: "Command" and "Working directory"
             content = re.sub(r'<LocalDebuggerCommand>(.*?)</LocalDebuggerCommand>', f'<LocalDebuggerCommand>{gta_sa_file.as_posix()}</LocalDebuggerCommand>', content)
             content = re.sub(r'<LocalDebuggerWorkingDirectory>(.*?)</LocalDebuggerWorkingDirectory>', f'<LocalDebuggerWorkingDirectory>{gta_root_dir.as_posix()}</LocalDebuggerWorkingDirectory>', content)
             with open(setting_file, 'w') as file:
                 file.write(content)
-            print("The debugging settings have been changed. It's better to re-enter the project")
+            print("The debugger settings have been changed. It is better to re-open the project.")
+        # For some reason, it was not possible to change
         except Except:
             print(f"The changes in {setting_file} could not be applied.")
     input('Done! Press Enter...')
