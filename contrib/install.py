5f5a8ee1f4d5e474653a1a266e9c0442883e251f
Vladik01-11 <denjidananaa@bk.ru>
2024-06-24 00:45:57 +0300

    Update install.py

--- contrib/install.py
+++ contrib/install.py
@@ -5,31 +5,28 @@ import zipfile
 import ctypes
 import sys
 
-def main():
-    if ctypes.windll.shell32.IsUserAnAdmin():
-        modern()
-    else:
-        print('Allow the launch as an administrator!')
-        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-
 def set_env_var(name, value):
     os.system(f"powershell [Environment]::SetEnvironmentVariable('{name}', '{value}', [EnvironmentVariableTarget]::User)")
 
-def modern():
+def main():
+    if not ctypes.windll.shell32.IsUserAnAdmin():
+        print('Allow the launch as an administrator!')
+        return ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+
     git_repo_root = Path.cwd()
     if git_repo_root.parts[-1] == 'contrib':
         git_repo_root = git_repo_root.parent
 
-    gta_sa_file = Path(tkFileDialog.askopenfilename(
+    initialdir = os.environ['GTA_SA_DIR'] if 'GTA_SA_DIR' in os.environ else ''
+    select = tkFileDialog.askopenfilename(
         title='Select the executable file from the GTA:SA installation directory',
-        filetypes=[("GTA:SA", ".exe")]
-    ))
-    gta_root_dir = gta_sa_file.parent.resolve()
-    if gta_root_dir.is_relative_to(git_repo_root):
-        print("GTA root directory can't be relative to this project's git root path")
-        return modern()
+        filetypes=[("GTA:SA", ".exe")], initialdir=initialdir
+    )
+    if not select:
+        return input('You have not selected a file, we are leaving. Press Enter...')
 
-    gta_scripts_dir = gta_root_dir / 'scripts'
+    gta_sa_file = Path(select)
+    gta_root_dir = gta_sa_file.parent.resolve()
 
     # Unpack zip into gta dir
     print("Unpacking `plugins.zip` into GTA:SA root directory...")
@@ -42,14 +39,14 @@ def modern():
     config_bin_dir = git_repo_root / 'bin' / Path(config_name)
     print(f'{config_bin_dir=}')
     for filename in ('gta_reversed.pdb', 'gta_reversed.asi'):
-        dst = gta_scripts_dir / filename
+        dst = gta_root_dir / 'scripts' / filename
         dst.unlink(missing_ok=True) # Delete symlink if it already exists
         # This fails [WinError 1314] if the script isn't run with admin rights [softlinks require it]
         os.symlink(config_bin_dir / filename, dst)
 
-    print("The Env variables are changing. If you had a project open, reopen it.")
-    set_env_var('GTA_SA_EXE', gta_sa_file.as_posix())
-    set_env_var('GTA_SA_DIR', gta_root_dir.as_posix())
+    print("The Env variables are changing... If VS is open with the Modern solution, restart VS")
+    set_env_var('GTA_SA_EXE', gta_sa_file)
+    set_env_var('GTA_SA_DIR', gta_root_dir)
     input('Done! Press Enter...')
 
 if __name__ == "__main__":
