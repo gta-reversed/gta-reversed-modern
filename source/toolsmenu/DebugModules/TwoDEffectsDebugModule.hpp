8f8ce352e1c8192ee250c794e6fdc24e1b4add22
Pirulax <patrikjankovics7@gmail.com>
2024-07-07 01:12:08 +0200

    Incomplete debug module implementation addendum

--- source/toolsmenu/DebugModules/TwoDEffectsDebugModule.hpp
+++ source/toolsmenu/DebugModules/TwoDEffectsDebugModule.hpp
@@ -5,7 +5,7 @@
 namespace notsa { 
 namespace debugmodules {
 class TwoDEffectsDebugModule final : public DebugModule {
-    struct TwoDFxInRange {
+    struct InRange2DFx {
         CEntity*   Entity; //!< The entity this fx belongs to
         C2dEffect* Fx;     //!< The 2D effect
         CVector    Pos;    //!< The world position of the fx (Calculated using the entity's matrix)
@@ -23,13 +23,13 @@ private:
     void RenderNearbyEffectsTable();
 
 private:
-    bool                                        m_IsOpen{};
-    int                                         m_MaxEntities{ 512 };
-    float                                       m_Range{ 100.f };
-    std::vector<CEntity*>                       m_EntitiesInRange{};
-    std::vector<std::pair<CEntity*, C2dEffect>> m_FxInRange{};           //!< All effects of the entitites in range
-    C2dEffect*                                  m_SelectedFx{};          //!< The selected fx. Only valid iff `m_IsSelectedFxInRange`!
-    bool                                        m_IsSelectedFxInRange{}; //!< If the selected fx is in range
+    bool                     m_IsOpen{};
+    int                      m_MaxEntities{ 512 };
+    float                    m_Range{ 100.f };
+    std::vector<CEntity*>    m_EntitiesInRange{};
+    std::vector<InRange2DFx> m_FxInRange{};           //!< All effects of the entitites in range
+    C2dEffect*               m_SelectedFx{};          //!< The selected fx. Only valid iff `m_IsSelectedFxInRange`!
+    bool                     m_IsSelectedFxInRange{}; //!< If the selected fx is in range
 };
 }; // namespace debugmodules
 }; // namespace notsa
