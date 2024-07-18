2a1691a2fe1991eaf90c68b2edfda8a3218e7763
Pirulax <patrikjankovics7@gmail.com>
2024-07-18 11:21:12 +0200

    Refactor `CAEAudioEnvironment::GetPositionRelativeToCamera`

--- source/game_sa/Audio/AEAudioEnvironment.h
+++ source/game_sa/Audio/AEAudioEnvironment.h
@@ -17,8 +17,12 @@ public:
     static float GetDistanceAttenuation(float dist);
     static float GetDirectionalMikeAttenuation(const CVector& soundDir);
     static void  GetReverbEnvironmentAndDepth(int8* reverbEnv, int32* depth);
-    static void  GetPositionRelativeToCamera(CVector* vecOut, const CVector* vecPos);
-    static void  GetPositionRelativeToCamera(CVector* vecOut, CPlaceable* placeable);
+
+    static void GetPositionRelativeToCamera(CVector& out, const CVector* vecPos);
+    static CVector GetPositionRelativeToCamera(const CVector& pos) { CVector o; GetPositionRelativeToCamera(o, &pos); return o; }
+
+    static void GetPositionRelativeToCamera(CVector& out, CPlaceable* placeable);
+    static CVector GetPositionRelativeToCamera(CPlaceable* placeable) { CVector o; GetPositionRelativeToCamera(o, placeable); return o; }
 };
 
 static constexpr int32 NUM_AUDIO_ENVIRONMENTS = 68;
